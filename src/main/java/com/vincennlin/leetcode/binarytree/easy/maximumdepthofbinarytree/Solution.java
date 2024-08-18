cbb0bd0f4a78ab6aa0f15ef2b2e77eb3bde32ff9
vincennlin <vincentagwa@gmail.com>
2024-08-18 12:01:09 +0800

    Binary Tree - Maximum Depth of Binary Tree

---
+++ src/main/java/com/vincennlin/leetcode/binarytree/easy/maximumdepthofbinarytree/Solution.java
@@ -0,0 +1,40 @@
+package com.vincennlin.leetcode.binarytree.easy.maximumdepthofbinarytree;
+//104
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        return 0;
+    }
+}
