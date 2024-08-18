09e1be2383c2c06dd942597ffa03162879329f79
vincennlin <vincentagwa@gmail.com>
2024-08-18 11:57:21 +0800

    LL - Merge Two Sorted Lists - my & sol

--- src/main/java/com/vincennlin/leetcode/linkedlist/easy/mergetwosortedlists/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/easy/mergetwosortedlists/Solution.java
@@ -20,6 +20,29 @@ class Solution {
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return null;
+        ListNode dummyNode = new ListNode();
+        ListNode currentNode = dummyNode;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                currentNode.next = list1;
+                list1 = list1.next;
+            } else {
+                currentNode.next = list2;
+                list2 = list2.next;
+            }
+            currentNode = currentNode.next;
+        }
+        while (list1 != null) {
+            currentNode.next = list1;
+            list1 = list1.next;
+            currentNode = currentNode.next;
+        }
+        while (list2 != null) {
+            currentNode.next = list2;
+            list2 = list2.next;
+            currentNode = currentNode.next;
+        }
+        return dummyNode.next;
     }
 }
