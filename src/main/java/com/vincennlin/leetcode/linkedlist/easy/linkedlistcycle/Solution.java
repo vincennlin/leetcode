ab20fb4c57d4e958a885fd7e8d71e2c5280d00f6
vincennlin <vincentagwa@gmail.com>
2024-08-18 11:25:46 +0800

    LL - Linked List Cycle - my & ans

---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/easy/linkedlistcycle/Solution.java
@@ -0,0 +1,38 @@
+package com.vincennlin.leetcode.linkedlist.easy.linkedlistcycle;
+
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slowNode = head;
+        ListNode fastNode = head;
+
+        while (fastNode != null && fastNode.next != null) {
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next;
+            if (fastNode == slowNode) return true;
+        }
+        return false;
+    }
+}
