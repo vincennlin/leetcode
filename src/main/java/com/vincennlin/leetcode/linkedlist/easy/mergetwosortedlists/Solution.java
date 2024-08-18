25484f4f8c3f3b430850c90e2e66370eeab22b5f
vincennlin <vincentagwa@gmail.com>
2024-08-18 11:48:17 +0800

    LL - Merge Two Sorted Lists

---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/easy/mergetwosortedlists/Solution.java
@@ -0,0 +1,25 @@
+package com.vincennlin.leetcode.linkedlist.easy.mergetwosortedlists;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return null;
+    }
+}
