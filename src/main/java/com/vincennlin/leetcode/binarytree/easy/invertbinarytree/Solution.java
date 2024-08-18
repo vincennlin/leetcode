4aa37cd80b19baafc4abef9d0d8e5321199dbbd8
vincennlin <vincentagwa@gmail.com>
2024-08-18 14:05:06 +0800

    Binary Tree - Convert Binary Tree

---
+++ src/main/java/com/vincennlin/leetcode/binarytree/easy/invertbinarytree/Solution.java
@@ -0,0 +1,40 @@
+package com.vincennlin.leetcode.binarytree.easy.invertbinarytree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        return null;
+    }
+}
