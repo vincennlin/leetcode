078af0f99333762cf8dd3a9d4e14effaed0c3a3c
vincennlin <vincentagwa@gmail.com>
2024-08-19 10:09:43 +0800

    Binary Tree - Path Sum - my

--- src/main/java/com/vincennlin/leetcode/binarytree/easy/pathsum/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytree/easy/pathsum/Solution.java
@@ -19,8 +19,18 @@ class Solution {
         }
     }
 
+    public boolean traverse(TreeNode currentNode, int currentSum, int targetSum) {
+        if (currentNode.left == null && currentNode.right == null) {
+            return currentSum + currentNode.val == targetSum;
+        }
+        if (currentNode.right == null) return traverse(currentNode.left, currentSum + currentNode.val, targetSum);
+        if (currentNode.left == null) return traverse(currentNode.right, currentSum + currentNode.val, targetSum);
+        return traverse(currentNode.left, currentSum + currentNode.val, targetSum)
+                || traverse(currentNode.right, currentSum + currentNode.val, targetSum);
+    }
 
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return false;
+        if (root == null) return false;
+        return traverse(root, 0, targetSum);
     }
 }
