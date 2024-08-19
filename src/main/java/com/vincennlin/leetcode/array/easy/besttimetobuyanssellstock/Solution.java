e4fc129f492b26c9a33bbdb43253a7e9664517c1
vincennlin <vincentagwa@gmail.com>
2024-08-19 10:46:40 +0800

    Array - Best Time to Buy and Sell Stock - my & ans

--- src/main/java/com/vincennlin/leetcode/array/easy/besttimetobuyanssellstock/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/easy/besttimetobuyanssellstock/Solution.java
@@ -2,6 +2,15 @@ package com.vincennlin.leetcode.array.easy.besttimetobuyanssellstock;
 //121
 class Solution {
     public int maxProfit(int[] prices) {
-        return 0;
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            } else if (minPrice > prices[i]) {
+                minPrice = prices[i];
+            }
+        }
+        return maxProfit;
     }
 }
