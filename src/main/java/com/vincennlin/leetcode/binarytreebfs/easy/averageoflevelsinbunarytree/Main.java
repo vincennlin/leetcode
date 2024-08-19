deece7883a7a0e40613ac4a6b368c1003ca2e4b2
vincennlin <vincentagwa@gmail.com>
2024-08-19 12:06:25 +0800

    Binary Tree BFS - Average of Levels in Binary Tree

---
+++ src/main/java/com/vincennlin/leetcode/binarytreebfs/easy/averageoflevelsinbunarytree/Main.java
@@ -0,0 +1,29 @@
+package com.vincennlin.leetcode.binarytreebfs.easy.averageoflevelsinbunarytree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example 1: root = [3,9,20,null,null,15,7]
+        Solution.TreeNode root1 = new Solution.TreeNode(3);
+        root1.left = new Solution.TreeNode(9);
+        root1.right = new Solution.TreeNode(20);
+        root1.right.left = new Solution.TreeNode(15);
+        root1.right.right = new Solution.TreeNode(7);
+
+        System.out.println(solution.averageOfLevels(root1));
+        // 應該輸出 [3.00000, 14.50000, 11.00000]
+
+        // Example 2: root = [3,9,20,15,7]
+        Solution.TreeNode root2 = new Solution.TreeNode(3);
+        root2.left = new Solution.TreeNode(9);
+        root2.right = new Solution.TreeNode(20);
+        root2.right.left = new Solution.TreeNode(15);
+        root2.right.right = new Solution.TreeNode(7);
+
+        System.out.println(solution.averageOfLevels(root2));
+        // 應該輸出 [3.00000, 14.50000, 11.00000]
+    }
+}
+
---
+++ src/main/java/com/vincennlin/leetcode/binarytreebfs/easy/averageoflevelsinbunarytree/Solution.java
@@ -0,0 +1,26 @@
+package com.vincennlin.leetcode.binarytreebfs.easy.averageoflevelsinbunarytree;
+
+import java.util.List;
+//637
+class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public List<Double> averageOfLevels(TreeNode root) {
+        return null;
+    }
+}
