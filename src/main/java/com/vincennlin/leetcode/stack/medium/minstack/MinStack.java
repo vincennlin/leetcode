d2df3b6d22b32fb19b59971ad535c0e23811a523
vincennlin <vincentagwa@gmail.com>
2024-08-19 21:19:12 +0800

    Stack - Min Stack - my

--- src/main/java/com/vincennlin/leetcode/stack/medium/minstack/MinStack.java
+++ src/main/java/com/vincennlin/leetcode/stack/medium/minstack/MinStack.java
@@ -1,24 +1,35 @@
 package com.vincennlin.leetcode.stack.medium.minstack;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //155
 class MinStack {
 
-    public MinStack() {
+    private List<int[]> stack;
 
+    public MinStack() {
+        stack = new ArrayList<>();
     }
 
     public void push(int val) {
-
+        if (stack.isEmpty()) {
+            stack.add(new int[]{val, val});
+        } else {
+            int minVal = Math.min(val, getMin());
+            stack.add(new int[]{val, minVal});
+        }
     }
 
     public void pop() {
-
+        stack.remove(stack.size() - 1);
     }
 
     public int top() {
-        return 0;
+        return stack.get(stack.size() - 1)[0];
     }
 
     public int getMin() {
-        return 0;
+        return stack.get(stack.size() - 1)[1];
     }
 }
