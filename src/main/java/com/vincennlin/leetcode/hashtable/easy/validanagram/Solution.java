711bd0cf86c684617c687f9a06b78aaa685d9666
vincennlin <vincentagwa@gmail.com>
2024-08-19 20:53:49 +0800

    Hash Table - Valid Anagram - my

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/validanagram/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/validanagram/Solution.java
@@ -1,7 +1,24 @@
 package com.vincennlin.leetcode.hashtable.easy.validanagram;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //242
 class Solution {
     public boolean isAnagram(String s, String t) {
-        return false;
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            int count = map.getOrDefault(c, 0);
+            if (count == 0) {
+                return false;
+            } else {
+                map.put(c, count - 1);
+            }
+        }
+        return true;
     }
 }
