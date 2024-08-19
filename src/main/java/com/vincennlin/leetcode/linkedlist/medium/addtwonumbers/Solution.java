fce674f630072eb50c210941767d3d32e0e1057e
vincennlin <vincentagwa@gmail.com>
2024-08-19 11:24:26 +0800

    LL - Add Two Numbers - my

--- src/main/java/com/vincennlin/leetcode/linkedlist/medium/addtwonumbers/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/addtwonumbers/Solution.java
@@ -10,6 +10,40 @@ class Solution {
         }
     }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        ListNode dummyNode = new ListNode(0);
+        ListNode currentNode = dummyNode;
+        int extra = 0;
+
+        while (l1 != null && l2 != null) {
+            int newVal = (l1.val + l2.val + extra);
+            extra = newVal / 10 > 0 ? 1 : 0;
+            currentNode.next = new ListNode(newVal % 10);
+            currentNode = currentNode.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            int newVal = (l1.val + extra);
+            extra = newVal / 10 > 0 ? 1 : 0;
+            currentNode.next = new ListNode(newVal % 10);
+            currentNode = currentNode.next;
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            int newVal = (l2.val + extra);
+            extra = newVal / 10 > 0 ? 1 : 0;
+            currentNode.next = new ListNode(newVal % 10);
+            currentNode = currentNode.next;
+            l2 = l2.next;
+        }
+
+        if (extra > 0) {
+            currentNode.next = new ListNode(extra);
+            currentNode = currentNode.next;
+        }
+
+        return dummyNode.next;
     }
 }
