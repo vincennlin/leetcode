f25b6e7095c069cb7d1620c3c0c61d58d71ce3f7
vincennlin <vincentagwa@gmail.com>
2024-08-19 11:48:55 +0800

    Heap - Kth Largest Element in an Array

--- src/main/java/com/vincennlin/leetcode/heap/medium/kthlargestelementinanarray/Solution.java
+++ src/main/java/com/vincennlin/leetcode/heap/medium/kthlargestelementinanarray/Solution.java
@@ -1,82 +1,18 @@
 package com.vincennlin.leetcode.heap.medium.kthlargestelementinanarray;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.PriorityQueue;
 //215
 class Solution {
 
-    private List<Integer> heap;
-
-    private int leftChild(int index) {
-        return 2 * index + 1;
-    }
-
-    private int rightChild(int index) {
-        return 2 * index + 2;
-    }
-
-    private int parent (int index) {
-        return (index - 1) / 2;
-    }
-
-    private void swap (int index1, int index2) {
-        if (index1 > heap.size() - 1 || index2 > heap.size() - 1) return;
-        int temp = heap.get(index1);
-        heap.set(index1, heap.get(index2));
-        heap.set(index2, temp);
-    }
-
-    private void insert(int value) {
-        heap.add(value);
-        int currentIndex = heap.size() - 1;
-        while (currentIndex > 0 && heap.get(currentIndex) > heap.get(parent(currentIndex))) {
-            swap(currentIndex, parent(currentIndex));
-            currentIndex = parent(currentIndex);
-        }
-    }
-
-    private void sinkDown(int index) {
-        int maxIndex = index;
-        int leftIndex = leftChild(index);
-        int rightIndex= rightChild(index);
-
-        if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
-            maxIndex = leftIndex;
-        }
-        if (rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
-            maxIndex = rightIndex;
-        }
-
-        if (maxIndex != index) {
-            swap(index, maxIndex);
-            sinkDown(maxIndex);
-        }
-    }
-
-    private Integer remove() {
-        if (heap.size() == 0) return null;
-        if (heap.size() == 1) return heap.remove(0);
-
-        int maxValue = heap.get(0);
-        heap.set(0, heap.remove(heap.size() - 1));
-        sinkDown(0);
-
-        return maxValue;
-    }
-
     public int findKthLargest(int[] nums, int k) {
 
-        heap = new ArrayList<>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
         for (int num : nums) {
-            insert(num);
+            minHeap.add(num);
+            if (minHeap.size() > k) minHeap.poll();
         }
 
-        while (k > 1) {
-            remove();
-            k--;
-        }
-
-        return remove();
+        return minHeap.peek();
     }
 }
