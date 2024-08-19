624886c38213e778d259b5d4fcfc70bbc2f9d72e
vincennlin <vincentagwa@gmail.com>
2024-08-19 10:15:05 +0800

    Binary Tree - Count Complete Tree Nodes

---
+++ src/main/java/com/vincennlin/leetcode/binarytree/easy/countcompletetreenodes/Solution.java
@@ -0,0 +1,25 @@
+package com.vincennlin.leetcode.binarytree.easy.countcompletetreenodes;
+//222
+class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int countNodes(TreeNode root) {
+        return 0;
+    }
+}
