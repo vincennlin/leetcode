4eb56ff2ce927d091ddb86401980ecbbd927ad4c
vincennlin <vincentagwa@gmail.com>
2024-08-19 21:37:05 +0800

    Binary Tree - Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/main/java/com/vincennlin/leetcode/binarytreegeneral/medium/constructbinarytreefrompreorderandinordertraversal/Solution.java
@@ -0,0 +1,25 @@
+package com.vincennlin.leetcode.binarytreegeneral.medium.constructbinarytreefrompreorderandinordertraversal;
+//105
+class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return null;
+    }
+}
