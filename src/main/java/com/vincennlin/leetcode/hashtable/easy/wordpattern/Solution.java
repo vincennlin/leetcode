9f5cdd4a60fbb1e1a46bee5739f1ebdc5972af00
vincennlin <vincentagwa@gmail.com>
2024-08-19 20:45:10 +0800

    Hash Table - Word Pattern - ans

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/wordpattern/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/wordpattern/Solution.java
@@ -8,19 +8,22 @@ class Solution {
     public boolean wordPattern(String pattern, String s) {
         char[] patternChar = pattern.toCharArray();
         String[] words = s.split(" ");
+
         if (patternChar.length == 0 || words.length == 0 || patternChar.length != words.length) {
             return false;
         }
 
-        Map<Character, String> map = new HashMap<>();
+        Map<Character, String> charToWordMap = new HashMap<>();
+        Map<String, Character> wordToCharMap = new HashMap<>();
+
         for (int i = 0; i < patternChar.length; i++) {
-            if (map.containsKey(patternChar[i]) && (!map.get(patternChar[i]).equals(words[i]))) {
+            if (charToWordMap.containsKey(patternChar[i]) && (!charToWordMap.get(patternChar[i]).equals(words[i]))) {
                 return false;
-            } else if (!map.containsKey(patternChar[i]) && map.containsValue(words[i])){
+            } else if (wordToCharMap.containsKey(words[i]) && (!wordToCharMap.get(words[i]).equals(patternChar[i]))) {
                 return false;
-            } else {
-                map.put(patternChar[i], words[i]);
             }
+            charToWordMap.put(patternChar[i], words[i]);
+            wordToCharMap.put(words[i], patternChar[i]);
         }
 
         return true;
