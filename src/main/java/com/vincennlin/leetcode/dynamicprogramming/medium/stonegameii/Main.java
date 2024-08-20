dfcb68da35a9b526216ece043ed64216146fc641
vincennlin <vincentagwa@gmail.com>
2024-08-20 09:44:47 +0800

    Dynamic Programming - Stone Game II

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/medium/stonegameii/Main.java
@@ -0,0 +1,13 @@
+package com.vincennlin.leetcode.dynamicprogramming.medium.stonegameii;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] piles1 = new int[]{2, 7, 9, 4, 4};
+        System.out.println(solution.stoneGameII(piles1));//10
+
+        int[] piles2 = new int[]{1, 2, 3, 4, 5, 100};
+        System.out.println(solution.stoneGameII(piles2));//104
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/medium/stonegameii/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dynamicprogramming.medium.stonegameii;
+//1140
+class Solution {
+    public int stoneGameII(int[] piles) {
+        return 0;
+    }
+}
