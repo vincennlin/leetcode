1443197f36ac18d2d50971e833fce2588a3a6c41
vincennlin <vincentagwa@gmail.com>
2024-08-20 10:34:04 +0800

    Dynamic Programming - Climbing Stairs - my

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/easy/climbingstairs/Main.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/easy/climbingstairs/Main.java
@@ -4,12 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        // 測試例子1
-        int n1 = 2;
-        System.out.println("n = " + n1 + " 的方法數: " + solution.climbStairs(n1));
+        int n1 = 4;
+        System.out.println(solution.climbStairs(n1));
 
-        // 測試例子2
-        int n2 = 3;
-        System.out.println("n = " + n2 + " 的方法數: " + solution.climbStairs(n2));
+        int n2 = 5;
+        System.out.println(solution.climbStairs(n2));
     }
 }
