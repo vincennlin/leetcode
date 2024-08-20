6fb4f8299b0bfe4cd124b650bc9ed95162574563
vincennlin <vincentagwa@gmail.com>
2024-08-20 10:36:35 +0800

    Dynamic Programming - Climbing Stairs - ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/easy/climbingstairs/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/easy/climbingstairs/Solution.java
@@ -5,14 +5,17 @@ class Solution {
         if (n < 0) return 0;
         if (n == 0) return 1;
         if (n == 1) return 1;
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+
+        int prev1 = 1;
+        int prev2 = 1;
+        int current = 0;
 
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
         }
 
-        return dp[n];
+        return current;
     }
 }
