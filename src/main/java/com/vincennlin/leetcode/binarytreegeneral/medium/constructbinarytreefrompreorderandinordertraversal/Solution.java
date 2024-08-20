7cc07caafba6ec631ac9ec54202e979fb8264809
vincennlin <vincentagwa@gmail.com>
2024-08-20 08:31:16 +0800

    Binary Tree - Construct Binary Tree from Preorder and Inorder Traversal - my - wrong

--- src/main/java/com/vincennlin/leetcode/binarytreegeneral/medium/constructbinarytreefrompreorderandinordertraversal/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytreegeneral/medium/constructbinarytreefrompreorderandinordertraversal/Solution.java
@@ -1,4 +1,7 @@
 package com.vincennlin.leetcode.binarytreegeneral.medium.constructbinarytreefrompreorderandinordertraversal;
+
+import java.util.Arrays;
+
 //105
 class Solution {
     public static class TreeNode {
@@ -19,7 +22,27 @@ class Solution {
         }
     }
 
+    private int firstIndexOf(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) return i;
+        }
+        return -1;
+    }
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
+        if (preorder.length == 0 || inorder.length == 0) return null;
+        TreeNode currentNode = new TreeNode(preorder[0]);
+        if (preorder.length == 1 && inorder.length == 1) return currentNode;
+        int currentIndexInInOrder = firstIndexOf(inorder, preorder[0]);
+        int lastLeftIndexInPreOrder = firstIndexOf(inorder, inorder[currentIndexInInOrder - 1]);
+        currentNode.left = buildTree(
+                Arrays.copyOfRange(preorder, 1, lastLeftIndexInPreOrder + 1),
+                Arrays.copyOfRange(inorder, 0, currentIndexInInOrder)
+        );
+        currentNode.right = buildTree(
+                Arrays.copyOfRange(preorder, lastLeftIndexInPreOrder + 1, preorder.length - 1),
+                Arrays.copyOfRange(inorder, currentIndexInInOrder + 1, inorder.length - 1)
+        );
+        return currentNode;
     }
 }
