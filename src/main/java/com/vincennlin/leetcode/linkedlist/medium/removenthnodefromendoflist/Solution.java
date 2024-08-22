e123fa4a5e6ed7cff67c0805f313571171729838
vincennlin <vincentagwa@gmail.com>
2024-08-22 12:21:25 +0800

    LL - Remove Nth Node From End of List - ans

--- src/main/java/com/vincennlin/leetcode/linkedlist/medium/removenthnodefromendoflist/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/removenthnodefromendoflist/Solution.java
@@ -2,23 +2,21 @@ package com.vincennlin.leetcode.linkedlist.medium.removenthnodefromendoflist;
 //19
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) return null;
+        ListNode dummyNode = new ListNode(0, head);
+        ListNode fastNode = dummyNode;
+        ListNode slowNode = dummyNode;
 
-        ListNode frontNode = head;
         for (int i = 0; i < n; i++) {
-            frontNode = frontNode.next;
+            fastNode = fastNode.next;
         }
 
-        if (frontNode == null) return head.next;
-
-        ListNode backNode = head;
-        while (frontNode.next != null) {
-            backNode = backNode.next;
-            frontNode = frontNode.next;
+        while (fastNode.next != null) {
+            fastNode = fastNode.next;
+            slowNode = slowNode.next;
         }
 
-        backNode.next = backNode.next.next;
+        slowNode.next = slowNode.next.next;
 
-        return head;
+        return dummyNode.next;
     }
 }
