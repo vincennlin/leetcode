a7bdf9d9bf5d5eb8307a0519a02e489150a4433a
vincennlin <vincentagwa@gmail.com>
2024-08-22 11:45:04 +0800

    LL - Remove Nth Node From End of List

---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/removenthnodefromendoflist/Main.java
@@ -0,0 +1,54 @@
+package com.vincennlin.leetcode.linkedlist.medium.removenthnodefromendoflist;
+/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        head1.next.next.next.next = new ListNode(5);
+
+        ListNode result1 = solution.removeNthFromEnd(head1, 2);
+        while (result1 != null) {
+            System.out.println(result1.val);
+            result1 = result1.next;
+        }
+
+        System.out.println();
+
+        ListNode head2 = new ListNode(1);
+        ListNode result2 = solution.removeNthFromEnd(head2, 1);
+        while (result2 != null) {
+            System.out.println(result2.val);
+            result2 = result2.next;
+        }
+
+        System.out.println();
+
+        ListNode head3 = new ListNode(1);
+        head3.next = new ListNode(2);
+        ListNode result3 = solution.removeNthFromEnd(head3, 1);
+        while (result3 != null) {
+            System.out.println(result3.val);
+            result3 = result3.next;
+        }
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/removenthnodefromendoflist/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.linkedlist.medium.removenthnodefromendoflist;
+//19
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return null;
+    }
+}
