b94269ab1083d38a6944e6b0f89014c2db9980a5
vincennlin <vincentagwa@gmail.com>
2024-08-22 11:02:56 +0800

    Array - Summary Ranges - ans

--- src/main/java/com/vincennlin/leetcode/array/easy/summaryranges/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/easy/summaryranges/Solution.java
@@ -7,28 +7,18 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> rangesList = new ArrayList<>();
         if (nums.length == 0) return rangesList;
-        if (nums.length == 1) {
-            rangesList.add("" + nums[0]);
-            return rangesList;
-        }
 
         int startIndex = 0;
-        for (int currentIndex = 1; currentIndex < nums.length; currentIndex++) {
-            if (nums[currentIndex] != nums[currentIndex-1] + 1) {
+        for (int currentIndex = 1; currentIndex <= nums.length; currentIndex++) {
+            if (currentIndex == nums.length || nums[currentIndex] != nums[currentIndex-1] + 1) {
                 int endIndex = currentIndex - 1;
                 if (startIndex == endIndex) {
-                    rangesList.add("" + nums[startIndex]);
+                    rangesList.add(String.valueOf(nums[startIndex]));
                 } else {
                     rangesList.add(nums[startIndex] + "->" + nums[endIndex]);
                 }
                 startIndex = currentIndex;
             }
-        }
-
-        if (startIndex == nums.length - 1) {
-            rangesList.add("" + nums[startIndex]);
-        } else {
-            rangesList.add(nums[startIndex] + "->" + nums[nums.length-1]);
         }
 
         return rangesList;
