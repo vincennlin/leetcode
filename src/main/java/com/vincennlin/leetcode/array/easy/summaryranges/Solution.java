0f47751bdfad312e69c73a7889d08e8d3af1ff17
vincennlin <vincentagwa@gmail.com>
2024-08-22 10:33:52 +0800

    Array - Summary Ranges

---
+++ src/main/java/com/vincennlin/leetcode/array/easy/summaryranges/Solution.java
@@ -0,0 +1,9 @@
+package com.vincennlin.leetcode.array.easy.summaryranges;
+
+import java.util.List;
+//228
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        return null;
+    }
+}
