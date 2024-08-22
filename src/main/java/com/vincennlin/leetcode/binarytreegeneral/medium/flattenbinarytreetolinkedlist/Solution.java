0792e733b8f72f00b0ebdf679fa6a9d025a9cb18
vincennlin <vincentagwa@gmail.com>
2024-08-22 15:33:16 +0800

    Binary Tree - Flatten Binary Tree to Linked List - ans

--- src/main/java/com/vincennlin/leetcode/binarytreegeneral/medium/flattenbinarytreetolinkedlist/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytreegeneral/medium/flattenbinarytreetolinkedlist/Solution.java
@@ -1,27 +1,42 @@
 package com.vincennlin.leetcode.binarytreegeneral.medium.flattenbinarytreetolinkedlist;
 //114
 class Solution {
-    public TreeNode flattenNode(TreeNode currentNode) {
-        if (currentNode == null) return null;
-        if (currentNode.left == null) {
-            currentNode.right = flattenNode(currentNode.right);
-            return currentNode;
-        }
-
-        TreeNode tempNode = flattenNode(currentNode.right);
-        currentNode.right = flattenNode(currentNode.left);
-
-        TreeNode pointerNode = currentNode.right;
-
-        while (pointerNode.right != null) {
-            pointerNode = pointerNode.right;
-        }
-        pointerNode.right = tempNode;
-        currentNode.left = null;
-        return currentNode;
-    }
+//    public TreeNode flattenNode(TreeNode currentNode) {
+//        if (currentNode == null) return null;
+//        if (currentNode.left == null) {
+//            currentNode.right = flattenNode(currentNode.right);
+//            return currentNode;
+//        }
+//
+//        TreeNode tempNode = flattenNode(currentNode.right);
+//        currentNode.right = flattenNode(currentNode.left);
+//
+//        TreeNode pointerNode = currentNode.right;
+//
+//        while (pointerNode.right != null) {
+//            pointerNode = pointerNode.right;
+//        }
+//        pointerNode.right = tempNode;
+//        currentNode.left = null;
+//        return currentNode;
+//    }
 
     public void flatten(TreeNode root) {
-        flattenNode(root);
+        TreeNode currentNode = root;
+
+        while (currentNode != null) {
+            if (currentNode.left != null) {
+                TreeNode rightMost = currentNode.left;
+                while (rightMost.right != null) {
+                    rightMost = rightMost.right;
+                }
+
+                rightMost.right = currentNode.right;
+
+                currentNode.right = currentNode.left;
+                currentNode.left = null;
+            }
+            currentNode = currentNode.right;
+        }
     }
 }
