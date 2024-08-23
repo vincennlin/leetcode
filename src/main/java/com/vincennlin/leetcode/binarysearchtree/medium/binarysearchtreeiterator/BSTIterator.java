7f32444cde01f8b7578a28616bd03550c217fdc5
vincennlin <vincentagwa@gmail.com>
2024-08-23 14:17:45 +0800

    Binary Search Tree - Binary Search Tree Iterator

---
+++ src/main/java/com/vincennlin/leetcode/binarysearchtree/medium/binarysearchtreeiterator/BSTIterator.java
@@ -0,0 +1,16 @@
+package com.vincennlin.leetcode.binarysearchtree.medium.binarysearchtreeiterator;
+//173
+class BSTIterator {
+
+    public BSTIterator(TreeNode root) {
+
+    }
+
+    public int next() {
+        return 0;
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+}
