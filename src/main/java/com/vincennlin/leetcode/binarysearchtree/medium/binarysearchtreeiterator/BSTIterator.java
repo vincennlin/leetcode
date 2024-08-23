e6debb8670fc1309365c40fa624856acefb43caf
vincennlin <vincentagwa@gmail.com>
2024-08-23 14:36:55 +0800

    Binary Search Tree - Binary Search Tree Iterator - my

--- src/main/java/com/vincennlin/leetcode/binarysearchtree/medium/binarysearchtreeiterator/BSTIterator.java
+++ src/main/java/com/vincennlin/leetcode/binarysearchtree/medium/binarysearchtreeiterator/BSTIterator.java
@@ -2,15 +2,37 @@ package com.vincennlin.leetcode.binarysearchtree.medium.binarysearchtreeiterator
 //173
 class BSTIterator {
 
-    public BSTIterator(TreeNode root) {
+    private TreeNode dummyNode;
+    private TreeNode currentNode;
 
+    public BSTIterator(TreeNode root) {
+        traverse(root);
+        currentNode = dummyNode;
     }
 
     public int next() {
-        return 0;
+        currentNode = currentNode.right;
+        return currentNode.val;
     }
 
     public boolean hasNext() {
-        return false;
+        return currentNode.right != null;
+    }
+
+    private void traverse(TreeNode currentNode) {
+        if (currentNode == null) return;
+
+        if (currentNode.left != null) traverse(currentNode.left);
+
+        if (this.currentNode == null) {
+            this.currentNode = new TreeNode(currentNode.val);
+            dummyNode = new TreeNode();
+            dummyNode.right = this.currentNode;
+        } else {
+            this.currentNode.right = new TreeNode(currentNode.val);
+            this.currentNode = this.currentNode.right;
+        }
+
+        if (currentNode.right != null) traverse(currentNode.right);
     }
 }
