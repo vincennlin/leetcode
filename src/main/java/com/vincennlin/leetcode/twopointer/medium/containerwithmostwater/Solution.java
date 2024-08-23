ca184002a611798f328d376a5a982779159d2886
vincennlin <vincentagwa@gmail.com>
2024-08-23 19:24:26 +0800

    Two Pointers - Container With Most Water - my

--- src/main/java/com/vincennlin/leetcode/twopointer/medium/containerwithmostwater/Solution.java
+++ src/main/java/com/vincennlin/leetcode/twopointer/medium/containerwithmostwater/Solution.java
@@ -2,7 +2,6 @@ package com.vincennlin.leetcode.twopointer.medium.containerwithmostwater;
 //11
 class Solution {
     public int maxArea(int[] height) {
-        if (height.length < 2) return 0;
         int leftIndex = 0;
         int rightIndex = height.length - 1;
         int maxArea = 0;
