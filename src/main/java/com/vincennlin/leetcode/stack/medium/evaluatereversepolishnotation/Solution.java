172bbf8335725aea41ffb8f573ed3268cc0df817
vincennlin <vincentagwa@gmail.com>
2024-08-23 11:17:08 +0800

    Stack - Evaluate Reverse Polish Notation - my

--- src/main/java/com/vincennlin/leetcode/stack/medium/evaluatereversepolishnotation/Solution.java
+++ src/main/java/com/vincennlin/leetcode/stack/medium/evaluatereversepolishnotation/Solution.java
@@ -1,7 +1,31 @@
 package com.vincennlin.leetcode.stack.medium.evaluatereversepolishnotation;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 //150
 class Solution {
     public int evalRPN(String[] tokens) {
-        return 0;
+        Deque<Integer> tokenStack = new ArrayDeque<>();
+
+        for(String token : tokens) {
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                int num2 = tokenStack.pop();
+                int num1 = tokenStack.pop();
+                if (token.equals("+")) {
+                    tokenStack.push(num1 + num2);
+                } else if (token.equals("-")) {
+                    tokenStack.push(num1 - num2);
+                } else if (token.equals("*")) {
+                    tokenStack.push(num1 * num2);
+                } else {
+                    tokenStack.push(num1 / num2);
+                }
+            } else {
+                tokenStack.push(Integer.parseInt(token));
+            }
+        }
+
+        return tokenStack.pop();
     }
 }
