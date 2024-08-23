865904ad5951cbb22bf80be1fc6766696bdbbc14
vincennlin <vincentagwa@gmail.com>
2024-08-23 09:17:17 +0800

    Binary Search Tree - Minimum Absolute Difference in BST - my

--- src/main/java/com/vincennlin/leetcode/binarysearchtree/easy/minimumabsolutedifferenceinbst/Main.java
+++ src/main/java/com/vincennlin/leetcode/binarysearchtree/easy/minimumabsolutedifferenceinbst/Main.java
@@ -16,20 +16,42 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        TreeNode root = new TreeNode(4);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(6);
-        root.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(3);
-        int result = solution.getMinimumDifference(root);
-        System.out.println(result);
+        TreeNode root1 = new TreeNode(4);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(6);
+        root1.left.left = new TreeNode(1);
+        root1.left.right = new TreeNode(3);
+        int result1 = solution.getMinimumDifference(root1);
 
-        root = new TreeNode(1);
-        root.left = new TreeNode(0);
-        root.right = new TreeNode(48);
-        root.right.left = new TreeNode(12);
-        root.right.right = new TreeNode(49);
-        result = solution.getMinimumDifference(root);
-        System.out.println(result);
+        System.out.println(result1); // 1
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(0);
+        root2.right = new TreeNode(48);
+        root2.right.left = new TreeNode(12);
+        root2.right.right = new TreeNode(49);
+        int result2 = solution.getMinimumDifference(root2);
+
+        System.out.println(result2); // 1
+
+        // [236,104,701,null,227,null,911]
+        TreeNode root3 = new TreeNode(236);
+        root3.left = new TreeNode(104);
+        root3.right = new TreeNode(701);
+        root3.left.right = new TreeNode(227);
+        root3.right.right = new TreeNode(911);
+        int result3 = solution.getMinimumDifference(root3);
+
+        System.out.println(result3); // 9
+
+        // [1564,1434,3048,1,null,null,3184]
+        TreeNode root4 = new TreeNode(1564);
+        root4.left = new TreeNode(1434);
+        root4.left.left = new TreeNode(1);
+        root4.right = new TreeNode(3048);
+        root4.right.right = new TreeNode(3184);
+        int result4 = solution.getMinimumDifference(root4);
+
+        System.out.println(result4); // 130
     }
 }
--- src/main/java/com/vincennlin/leetcode/binarysearchtree/easy/minimumabsolutedifferenceinbst/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarysearchtree/easy/minimumabsolutedifferenceinbst/Solution.java
@@ -1,7 +1,40 @@
 package com.vincennlin.leetcode.binarysearchtree.easy.minimumabsolutedifferenceinbst;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 //530
 class Solution {
+    private Queue<TreeNode> nodeQueue;
+    private int minDifference;
+    private int currentNum;
+
+    private void traverse() {
+        TreeNode currentNode = nodeQueue.poll();
+        if (currentNode.left != null) {
+            nodeQueue.add(currentNode.left);
+            traverse();
+        }
+        if (currentNum >= 0) {
+            int diff = currentNode.val - currentNum;
+            if (diff < minDifference) {
+                minDifference = diff;
+            }
+        }
+        currentNum = currentNode.val;
+        if (currentNode.right != null) {
+            nodeQueue.add(currentNode.right);
+            traverse();
+        }
+    }
+
     public int getMinimumDifference(TreeNode root) {
-        return 0;
+        if (root == null) return 0;
+        minDifference = Integer.MAX_VALUE;
+        currentNum = -1;
+        nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        traverse();
+        return minDifference;
     }
 }
