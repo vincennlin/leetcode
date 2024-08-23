14328e3e894363ebee64b2f1a52b53338d2d38f6
vincennlin <vincentagwa@gmail.com>
2024-08-23 10:51:28 +0800

    Stack - Simplify Path - my

--- src/main/java/com/vincennlin/leetcode/stack/medium/simplifypath/Solution.java
+++ src/main/java/com/vincennlin/leetcode/stack/medium/simplifypath/Solution.java
@@ -1,7 +1,32 @@
 package com.vincennlin.leetcode.stack.medium.simplifypath;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 //71
 class Solution {
     public String simplifyPath(String path) {
-        return "";
+        String[] pathArray = path.split("/");
+        Stack<String> pathStack = new Stack<>();
+
+        for (String currentStr : pathArray) {
+            if (currentStr.equals("..")) {
+                if (!pathStack.isEmpty()) pathStack.pop();
+            } else if (!currentStr.equals(".") && !currentStr.isEmpty()) {
+                pathStack.push(currentStr);
+            }
+        }
+
+        List<String> pathList = new LinkedList<>(pathStack);
+        StringBuilder sb = new StringBuilder();
+
+        while (!pathList.isEmpty()) {
+            String str = pathList.remove(0);
+            sb.append("/").append(str);
+        }
+        if (sb.isEmpty()) sb.append("/");
+
+        return sb.toString();
     }
 }
