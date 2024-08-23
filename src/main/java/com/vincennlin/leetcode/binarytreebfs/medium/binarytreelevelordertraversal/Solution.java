4d96b1ed895f2cbcca821e2f1ee67f30e3adab83
vincennlin <vincentagwa@gmail.com>
2024-08-23 14:59:20 +0800

    Binary Search Tree - Binary Tree Level Order Traversal - my & ans

--- src/main/java/com/vincennlin/leetcode/binarytreebfs/medium/binarytreelevelordertraversal/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytreebfs/medium/binarytreelevelordertraversal/Solution.java
@@ -1,9 +1,32 @@
 package com.vincennlin.leetcode.binarytreebfs.medium.binarytreelevelordertraversal;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
 //102
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        return null;
+        List<List<Integer>> levelNodesList = new ArrayList<>();
+        if (root == null) return levelNodesList;
+
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        while (!nodeQueue.isEmpty()) {
+            List<Integer> levelNodes = new ArrayList<>();
+            int levelNodesCount = nodeQueue.size();
+
+            for (int i = 0; i < levelNodesCount; i++) {
+                TreeNode currentNode = nodeQueue.poll();
+                if (currentNode.left != null) nodeQueue.add(currentNode.left);
+                if (currentNode.right != null) nodeQueue.add(currentNode.right);
+                levelNodes.add(currentNode.val);
+            }
+            levelNodesList.add(levelNodes);
+        }
+
+        return levelNodesList;
     }
 }
