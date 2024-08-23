086008524b6d6d271edc68d5f4ef06b223f7976b
vincennlin <vincentagwa@gmail.com>
2024-08-23 10:08:27 +0800

    Binary Search Tree - Validate Binary Search Tree - my

--- src/main/java/com/vincennlin/leetcode/binarysearchtree/medium/validatebinarysearchtree/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarysearchtree/medium/validatebinarysearchtree/Solution.java
@@ -1,7 +1,25 @@
 package com.vincennlin.leetcode.binarysearchtree.medium.validatebinarysearchtree;
 //98
 class Solution {
+    private Integer previousVal;
+    private boolean isValid;
+
+    private void inorderTraversal(TreeNode currentNode) {
+        if (currentNode == null) return;
+
+        inorderTraversal(currentNode.left);
+        if (!isValid) return;
+
+        if (previousVal != null) isValid = previousVal < currentNode.val;
+        previousVal = currentNode.val;
+
+        inorderTraversal(currentNode.right);
+    }
+
     public boolean isValidBST(TreeNode root) {
-        return false;
+        previousVal = null;
+        isValid = true;
+        inorderTraversal(root);
+        return isValid;
     }
 }
