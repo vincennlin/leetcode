b3ebdd6c1baec16c1631f4745669befb9dc85490
vincennlin <vincentagwa@gmail.com>
2024-08-23 14:50:53 +0800

    Binary Search Tree - Binary Tree Level Order Traversal

---
+++ src/main/java/com/vincennlin/leetcode/binarytreebfs/medium/binarytreelevelordertraversal/Solution.java
@@ -0,0 +1,9 @@
+package com.vincennlin.leetcode.binarytreebfs.medium.binarytreelevelordertraversal;
+
+import java.util.List;
+//102
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+}
