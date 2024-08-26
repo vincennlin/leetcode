5d15cd8a497685a596258c6efc78b5cf30ead041
vincennlin <vincentagwa@gmail.com>
2024-08-26 15:49:26 +0800

    Array - Longest Common Prefix - ans

--- src/main/java/com/vincennlin/leetcode/array/easy/longestcommonprefix/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/easy/longestcommonprefix/Solution.java
@@ -6,19 +6,15 @@ import java.util.Arrays;
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) return "";
-        StringBuilder prefix = new StringBuilder();
-        Arrays.sort(strs, (o1, o2) -> o2.length() - o1.length());
-        if (strs[0].isEmpty()) return "";
 
-        for (int i = 0; i < strs[0].length(); i++) {
-            for (int j = 1; j < strs.length; j++) {
-                if (strs[0].charAt(i) != strs[j].charAt(i)) {
-                    return prefix.toString();
-                }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
             }
-            prefix.append(strs[0].charAt(i));
         }
 
-        return prefix.toString();
+        return prefix;
     }
 }
