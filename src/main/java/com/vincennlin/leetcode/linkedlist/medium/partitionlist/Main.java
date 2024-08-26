c0d4d9a71a19f47964e7874cab19db26dc709f24
vincennlin <vincentagwa@gmail.com>
2024-08-26 09:30:07 +0800

    Linked List - Partition List

---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/partitionlist/Main.java
@@ -0,0 +1,51 @@
+package com.vincennlin.leetcode.linkedlist.medium.partitionlist;
+/*
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example 1:
+
+Input: head = [1,4,3,2,5,2], x = 3
+Output: [1,2,2,4,3,5]
+
+Example 2:
+
+Input: head = [2,1], x = 2
+Output: [1,2]
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 200].
+-100 <= Node.val <= 100
+-200 <= x <= 200
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(4);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(2);
+        head1.next.next.next.next = new ListNode(5);
+        head1.next.next.next.next.next = new ListNode(2);
+
+        ListNode result1 = solution.partition(head1, 3);
+        while (result1 != null) {
+            System.out.println(result1.val);
+            result1 = result1.next;
+        }
+
+        System.out.println();
+
+        ListNode head2 = new ListNode(2);
+        head2.next = new ListNode(1);
+
+        ListNode result2 = solution.partition(head2, 2);
+        while (result2 != null) {
+            System.out.println(result2.val);
+            result2 = result2.next;
+        }
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/partitionlist/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.linkedlist.medium.partitionlist;
+//86
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        return null;
+    }
+}
