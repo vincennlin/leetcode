f2ef5964dc2a3f120e3a82b1bff1adecc404094a
vincennlin <vincentagwa@gmail.com>
2024-08-26 09:18:43 +0800

    Linked List - Rotate List - my

--- src/main/java/com/vincennlin/leetcode/linkedlist/medium/rotatelist/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/rotatelist/Solution.java
@@ -2,6 +2,35 @@ package com.vincennlin.leetcode.linkedlist.medium.rotatelist;
 //61
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        return null;
+        if (head == null) return null;
+        if (k == 0) return head;
+
+        ListNode dummyNode = new ListNode();
+        dummyNode.next = head;
+        ListNode currentNode = head;
+        int length = 0;
+
+        while (currentNode != null) {
+            currentNode = currentNode.next;
+            length++;
+        }
+
+        k = k % length;
+        ListNode tailNode = head;
+        for (int i = 0; i < k; i++) {
+            tailNode = tailNode.next;
+        }
+
+        currentNode = head;
+        while (tailNode.next != null) {
+            tailNode = tailNode.next;
+            currentNode = currentNode.next;
+        }
+
+        tailNode.next = dummyNode.next;
+        dummyNode.next = currentNode.next;
+        currentNode.next = null;
+
+        return dummyNode.next;
     }
 }
