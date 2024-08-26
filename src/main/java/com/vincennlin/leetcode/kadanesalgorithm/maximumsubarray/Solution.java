6b2c8b5688d54580295619b739203832d0468967
vincennlin <vincentagwa@gmail.com>
2024-08-26 14:30:54 +0800

    Kadane's Algorithm - Maximum Subarray - ans

--- src/main/java/com/vincennlin/leetcode/kadanesalgorithm/maximumsubarray/Solution.java
+++ src/main/java/com/vincennlin/leetcode/kadanesalgorithm/maximumsubarray/Solution.java
@@ -2,6 +2,16 @@ package com.vincennlin.leetcode.kadanesalgorithm.maximumsubarray;
 //53
 class Solution {
     public int maxSubArray(int[] nums) {
-        return 0;
+        if (nums.length == 0) return 0;
+
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
     }
 }
