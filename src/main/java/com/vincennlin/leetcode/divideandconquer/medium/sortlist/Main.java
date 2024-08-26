da8cf0071f361ce11bc51f1a78c3d8c1add53802
vincennlin <vincentagwa@gmail.com>
2024-08-26 12:22:12 +0800

    Divide and Conquer - Sort List

---
+++ src/main/java/com/vincennlin/leetcode/divideandconquer/medium/sortlist/Main.java
@@ -0,0 +1,54 @@
+package com.vincennlin.leetcode.divideandconquer.medium.sortlist;
+/*
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Example 1:
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Example 2:
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+
+Example 3:
+
+Input: head = []
+Output: []
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode head1 = new ListNode(4);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(1);
+        head1.next.next.next = new ListNode(3);
+
+        System.out.println(solution.sortList(head1));
+
+        System.out.println();
+
+        ListNode head2 = new ListNode(-1);
+        head2.next = new ListNode(5);
+        head2.next.next = new ListNode(3);
+        head2.next.next.next = new ListNode(4);
+        head2.next.next.next.next = new ListNode(0);
+
+        System.out.println(solution.sortList(head2));
+
+        System.out.println();
+
+        ListNode head3 = null;
+
+        System.out.println(solution.sortList(head3));
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/divideandconquer/medium/sortlist/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.divideandconquer.medium.sortlist;
+//148
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return null;
+    }
+}
