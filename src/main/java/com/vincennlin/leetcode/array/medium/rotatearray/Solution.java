f3147e48c867c60900bed713c7c4890975cdbf9c
vincennlin <vincentagwa@gmail.com>
2024-08-26 21:14:39 +0800

    Array - Rotate Array - ans

--- src/main/java/com/vincennlin/leetcode/array/medium/rotatearray/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/rotatearray/Solution.java
@@ -1,25 +1,21 @@
 package com.vincennlin.leetcode.array.medium.rotatearray;
 //189
 class Solution {
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
     public void rotate(int[] nums, int k) {
-        if (nums.length == 0 || nums.length == 1) return;
-        int n = nums.length;
-        k = k % n;
+        k = k % nums.length;
         if (k == 0) return;
-        for (int i = 0; i < n / 2; i++) {
-            int temp = nums[i];
-            nums[i] = nums[n - 1 - i];
-            nums[n - 1 - i] = temp;
-        }
-        for (int i = 0; i < k / 2; i ++) {
-            int temp = nums[i];
-            nums[i] = nums[k - 1 - i];
-            nums[k - 1 - i] = temp;
-        }
-        for (int i = k; i <= (n - k); i++) {
-            int temp = nums[i];
-            nums[i] = nums[n + k - i - 1];
-            nums[n + k - i - 1] = temp;
-        }
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
 }
