5cb337b417f85da6562bea107a1f7bd7144639d4
vincennlin <vincentagwa@gmail.com>
2024-08-26 08:19:01 +0800

    Hash Table - Isomorphic Strings - my

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/isomorphicstrings/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/isomorphicstrings/Solution.java
@@ -1,7 +1,26 @@
 package com.vincennlin.leetcode.hashtable.easy.isomorphicstrings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        return false;
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Character> sToTMap = new HashMap<>();
+        Map<Character, Character> tToSMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (sToTMap.containsKey(s.charAt(i)) && sToTMap.get(s.charAt(i)) != t.charAt(i)) {
+                return false;
+            }
+            if (tToSMap.containsKey(t.charAt(i)) && tToSMap.get(t.charAt(i)) != s.charAt(i)) {
+                return false;
+            }
+            sToTMap.put(s.charAt(i), t.charAt(i));
+            tToSMap.put(t.charAt(i), s.charAt(i));
+        }
+
+        return true;
     }
 }
