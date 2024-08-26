8545cefb5c76b1181d7c87aa17bbe84db75eebc4
vincennlin <vincentagwa@gmail.com>
2024-08-26 21:32:12 +0800

    Array - Jump Game - ans

--- src/main/java/com/vincennlin/leetcode/array/medium/jumpgame/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/jumpgame/Solution.java
@@ -1,12 +1,20 @@
 package com.vincennlin.leetcode.array.medium.jumpgame;
 //55
 class Solution {
+//    public boolean canJump(int[] nums) {
+//        if (nums.length == 1) return true;
+//        int currentMax = 0;
+//        for (int i = 0; i < nums.length - 1; i++) {
+//            currentMax = Math.max(nums[i], --currentMax);
+//            if (currentMax <= 0) return false;
+//        }
+//        return true;
+//    }
     public boolean canJump(int[] nums) {
-        if (nums.length == 1) return true;
-        int currentMax = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            currentMax = Math.max(nums[i], --currentMax);
-            if (currentMax <= 0) return false;
+        int farthest = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) return false;
+            farthest = Math.max(farthest, i + nums[i]);
         }
         return true;
     }
