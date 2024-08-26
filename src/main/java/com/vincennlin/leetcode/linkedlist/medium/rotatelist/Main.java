24d909cb5767c785c702d5d713198a920299fab5
vincennlin <vincentagwa@gmail.com>
2024-08-26 08:28:23 +0800

    Linked List - Rotate List

---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/rotatelist/Main.java
@@ -0,0 +1,39 @@
+package com.vincennlin.leetcode.linkedlist.medium.rotatelist;
+/*
+Given the head of a linked list, rotate the list to the right by k places.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+
+Example 2:
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int k = 2;
+        ListNode result = solution.rotateRight(head, k);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/rotatelist/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.linkedlist.medium.rotatelist;
+//61
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        return null;
+    }
+}
