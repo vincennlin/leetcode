03c4ff2805e0a28934fb3dca326c627ee60f9dfc
vincennlin <vincentagwa@gmail.com>
2024-08-26 21:23:19 +0800

    Array - Jump Game

---
+++ src/main/java/com/vincennlin/leetcode/array/medium/jumpgame/Main.java
@@ -0,0 +1,34 @@
+package com.vincennlin.leetcode.array.medium.jumpgame;
+/*
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums1 = {2,3,1,1,4};
+        System.out.println(solution.canJump(nums1));// true
+
+        int[] nums2 = {3,2,1,0,4};
+        System.out.println(solution.canJump(nums2));// false
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/array/medium/jumpgame/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.array.medium.jumpgame;
+//55
+class Solution {
+    public boolean canJump(int[] nums) {
+        return false;
+    }
+}
