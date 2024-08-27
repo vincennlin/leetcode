0f0a5e1c14acb5770136cd81c28e2f54120a5a70
vincennlin <vincentagwa@gmail.com>
2024-08-27 11:48:55 +0800

    Binary Tree - Lowest Common Ancestor of a Binary Tree - ans

--- src/main/java/com/vincennlin/leetcode/binarytreegeneral/medium/lowestcommonancestorofabinarytree/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytreegeneral/medium/lowestcommonancestorofabinarytree/Solution.java
@@ -2,6 +2,17 @@ package com.vincennlin.leetcode.binarytreegeneral.medium.lowestcommonancestorofa
 //236
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return null;
+        if (root == null) return null;
+        if (root.val == p.val || root.val == q.val) return root;
+
+        TreeNode leftLca = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightLca = lowestCommonAncestor(root.right, p, q);
+
+        if (leftLca != null && rightLca != null) return root;
+        if (leftLca != null) {
+            return leftLca;
+        } else {
+            return rightLca;
+        }
     }
 }
