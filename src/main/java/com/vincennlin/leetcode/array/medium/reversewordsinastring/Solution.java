cc008683f30adfaf00f78db65c8acfb341794179
vincennlin <vincentagwa@gmail.com>
2024-08-27 10:34:28 +0800

    Array - Reverse Words in a String - my

--- src/main/java/com/vincennlin/leetcode/array/medium/reversewordsinastring/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/reversewordsinastring/Solution.java
@@ -2,6 +2,16 @@ package com.vincennlin.leetcode.array.medium.reversewordsinastring;
 //151
 class Solution {
     public String reverseWords(String s) {
-        return "";
+        s = s.trim();
+        String[] strArray = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = strArray.length - 1; i >= 0; i--) {
+            if (strArray[i].isEmpty() || strArray[i].equals(" ")) continue;
+            sb.append(strArray[i]);
+            if (i != 0){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
     }
 }
