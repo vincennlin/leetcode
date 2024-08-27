03f60b86b83fdf0b32f8d165d52b4b1fd200849a
vincennlin <vincentagwa@gmail.com>
2024-08-27 13:30:21 +0800

    Heap - Find K Pairs with Smallest Sums - ans

--- src/main/java/com/vincennlin/leetcode/heap/medium/findkpairswithsmallestsums/Solution.java
+++ src/main/java/com/vincennlin/leetcode/heap/medium/findkpairswithsmallestsums/Solution.java
@@ -1,28 +1,33 @@
 package com.vincennlin.leetcode.heap.medium.findkpairswithsmallestsums;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 
 //373
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<List<Integer>> pairQueue = new PriorityQueue<>(
-                Comparator.comparingInt(o -> (o.get(0) + o.get(1)))
-        );
-        List<List<Integer>> pairList = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        for (int num1 : nums1) {
-            for (int num2 : nums2) {
-                pairQueue.add(List.of(num1, num2));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) ->
+                Integer.compare(nums1[a[0]] + nums2[a[1]], nums1[b[0]] + nums2[b[1]])
+        );
+
+        for (int i = 0; i < Math.min(nums1.length, k); i++) {
+            minHeap.add(new int[] {i, 0});
+        }
+
+        while (k-- > 0 && !minHeap.isEmpty()) {
+            int[] curr = minHeap.poll();
+            int i = curr[0], j = curr[1];
+            result.add(Arrays.asList(nums1[i], nums2[j]));
+
+            if (j + 1 < nums2.length) {
+                minHeap.add(new int[] {i, j + 1});
             }
         }
 
-        for (int i = 0; i < k; i++) {
-            pairList.add(pairQueue.poll());
-        }
-
-        return pairList;
+        return result;
     }
 }
