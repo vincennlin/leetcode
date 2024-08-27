85374bc33ae8a78c61d583fabd10c73ce9a3f721
vincennlin <vincentagwa@gmail.com>
2024-08-27 12:00:18 +0800

    Heap - Find K Pairs with Smallest Sums

---
+++ src/main/java/com/vincennlin/leetcode/heap/medium/findkpairswithsmallestsums/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.heap.medium.findkpairswithsmallestsums;
+
+import java.util.List;
+
+//373
+class Solution {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        return null;
+    }
+}
