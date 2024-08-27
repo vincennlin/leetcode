d2fda6d1e09120844b0e08c06d6461f8ab32c680
vincennlin <vincentagwa@gmail.com>
2024-08-27 13:46:12 +0800

    Bit - Add Binary - my

--- src/main/java/com/vincennlin/leetcode/bit/easy/addbinary/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/easy/addbinary/Solution.java
@@ -2,6 +2,25 @@ package com.vincennlin.leetcode.bit.easy.addbinary;
 //67
 class Solution {
     public String addBinary(String a, String b) {
-        return "";
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        int sum = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (i >= 0 || j >= 0) {
+            sum = ((i >= 0 ? Integer.parseInt(String.valueOf(a.charAt(i))) : 0) + (j >= 0 ? Integer.parseInt(String.valueOf(b.charAt(j))) : 0) + carry);
+            carry = sum / 2;
+            sum = sum % 2;
+
+            sb.append(sum);
+            i--;
+            j--;
+        }
+
+        if (carry > 0) sb.append(carry);
+
+        return sb.reverse().toString();
     }
 }
