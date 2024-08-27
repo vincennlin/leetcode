5328671611fc6d2b42cfa21f841f9a1d5dcdf5ab
vincennlin <vincentagwa@gmail.com>
2024-08-27 09:50:55 +0800

    Hash Table - Roman to Integer - ans

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/romantointeger/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/romantointeger/Solution.java
@@ -1,7 +1,32 @@
 package com.vincennlin.leetcode.hashtable.easy.romantointeger;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //13
 class Solution {
     public int romanToInt(String s) {
-        return 0;
+        Map<Character, Integer> romanToIntMap = new HashMap<>();
+        romanToIntMap.put('I', 1);
+        romanToIntMap.put('V', 5);
+        romanToIntMap.put('X', 10);
+        romanToIntMap.put('L', 50);
+        romanToIntMap.put('C', 100);
+        romanToIntMap.put('D', 500);
+        romanToIntMap.put('M', 1000);
+
+        int n = s.length();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int value = romanToIntMap.get(s.charAt(i));
+            if (i < n - 1 && value < romanToIntMap.get(s.charAt(i + 1))) {
+                sum -= value;
+            } else {
+                sum += value;
+            }
+        }
+
+        return sum;
     }
 }
