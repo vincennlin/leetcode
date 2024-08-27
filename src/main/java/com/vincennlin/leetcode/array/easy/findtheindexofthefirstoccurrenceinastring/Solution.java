6e5393de2defddfe6bf65e679ef65bdd7ee60ac4
vincennlin <vincentagwa@gmail.com>
2024-08-27 09:57:42 +0800

    Array - Find the Index of the First Occurrence in a String

---
+++ src/main/java/com/vincennlin/leetcode/array/easy/findtheindexofthefirstoccurrenceinastring/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.array.easy.findtheindexofthefirstoccurrenceinastring;
+//28
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return 0;
+    }
+}
