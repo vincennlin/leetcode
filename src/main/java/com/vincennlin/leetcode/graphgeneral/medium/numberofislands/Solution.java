ee27835b2b757b123396ac07394390c6e065c8b5
vincennlin <vincentagwa@gmail.com>
2024-08-28 19:29:57 +0800

    Graph - Number of Islands - my

--- src/main/java/com/vincennlin/leetcode/graphgeneral/medium/numberofislands/Solution.java
+++ src/main/java/com/vincennlin/leetcode/graphgeneral/medium/numberofislands/Solution.java
@@ -1,7 +1,31 @@
 package com.vincennlin.leetcode.graphgeneral.medium.numberofislands;
 //200
 class Solution {
+    private int m;
+    private int n;
+
+    private void dfs(char[][] grid, int i, int j) {
+        grid[i][j] = '0';
+        if (i > 0 && grid[i - 1][j] == '1') dfs(grid, i - 1, j);
+        if (i < m - 1 && grid[i + 1][j] == '1') dfs(grid, i + 1, j);
+        if (j > 0 && grid[i][j - 1] == '1') dfs(grid, i, j - 1);
+        if (j < n - 1 && grid[i][j + 1] == '1') dfs(grid, i, j + 1);
+    }
+
     public int numIslands(char[][] grid) {
-        return 0;
+        m = grid.length;
+        n = grid[0].length;
+        int islandNum = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    islandNum++;
+                }
+            }
+        }
+
+        return islandNum;
     }
 }
