05be9aa942be24fbf2d9bf9e7a6d621a21f7cca6
vincennlin <vincentagwa@gmail.com>
2024-08-28 22:28:56 +0800

    Hash Table - Happy Number - my

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/happynumber/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/happynumber/Solution.java
@@ -1,7 +1,28 @@
 package com.vincennlin.leetcode.hashtable.easy.happynumber;
+
+import java.util.HashSet;
+import java.util.Set;
+
 //202
 class Solution {
     public boolean isHappy(int n) {
-        return false;
+        Set<Integer> numSet = new HashSet<>();
+
+        while (true) {
+            n = sumOfSquares(n);
+            if (numSet.contains(n)) return false;
+            if (n == 1) return true;
+            numSet.add(n);
+        }
+    }
+
+    private int sumOfSquares(int n) {
+        int result = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            result += digit * digit;
+            n /= 10;
+        }
+        return result;
     }
 }
