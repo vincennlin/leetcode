b81a91be180174115c060029418a2ea2fa4b87cd
vincennlin <vincentagwa@gmail.com>
2024-08-28 21:27:50 +0800

    Graph - Surrounded Regions - ans

--- src/main/java/com/vincennlin/leetcode/graphgeneral/medium/surroundedregions/Solution.java
+++ src/main/java/com/vincennlin/leetcode/graphgeneral/medium/surroundedregions/Solution.java
@@ -1,27 +1,20 @@
 package com.vincennlin.leetcode.graphgeneral.medium.surroundedregions;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 //130
 class Solution {
     private int m;
     private int n;
-    private Set<List<Integer>> marked;
 
     private void dfs(int i, int j, char[][] board) {
-        marked.add(List.of(i, j));
-        if (i > 0 && board[i - 1][j] == 'O' && !marked.contains(List.of(i - 1, j))) dfs(i - 1, j, board);
-        if (i < m - 1 && board[i + 1][j] == 'O' && !marked.contains(List.of(i + 1, j))) dfs(i + 1, j, board);
-        if (j > 0 && board[i][j - 1] == 'O' && !marked.contains(List.of(i, j - 1))) dfs(i, j - 1, board);
-        if (j < n - 1 && board[i][j + 1] == 'O' && !marked.contains(List.of(i, j + 1))) dfs(i, j + 1, board);
+        board[i][j] = '*';
+        if (i > 0 && board[i - 1][j] == 'O') dfs(i - 1, j, board);
+        if (i < m - 1 && board[i + 1][j] == 'O') dfs(i + 1, j, board);
+        if (j > 0 && board[i][j - 1] == 'O') dfs(i, j - 1, board);
+        if (j < n - 1 && board[i][j + 1] == 'O') dfs(i, j + 1, board);
     }
 
     public void solve(char[][] board) {
         m = board.length;
         n = board[0].length;
-        marked = new HashSet<>();
 
         for (int i = 0; i < m; i++) {
             if (board[i][0] == 'O') dfs(i, 0, board);
@@ -34,8 +27,10 @@ class Solution {
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O' && !marked.contains(List.of(i, j))) {
+                if (board[i][j] == 'O') {
                     board[i][j] = 'X';
+                } else if (board[i][j] == '*') {
+                    board[i][j] = 'O';
                 }
             }
         }
