5754a77cde64da0f7ad518b19100456cf9aa2d65
vincennlin <vincentagwa@gmail.com>
2024-08-28 09:57:04 +0800

    Array - Product of Array Except Self

---
+++ src/main/java/com/vincennlin/leetcode/array/medium/productofarrayexceptself/Main.java
@@ -0,0 +1,36 @@
+package com.vincennlin.leetcode.array.medium.productofarrayexceptself;
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums1 = {1, 2, 3, 4};
+        System.out.println(solution.productExceptSelf(nums1)); // [24,12,8,6]
+
+        int[] nums2 = {-1, 1, 0, -3, 3};
+        System.out.println(solution.productExceptSelf(nums2)); // [0,0,9,0,0]
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/array/medium/productofarrayexceptself/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.array.medium.productofarrayexceptself;
+//238
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        return new int[]{};
+    }
+}
