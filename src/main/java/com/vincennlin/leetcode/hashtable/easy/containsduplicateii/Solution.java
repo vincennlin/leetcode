3b9ef5d34d3176eb8debea0ece9622bcb7fa28cf
vincennlin <vincentagwa@gmail.com>
2024-08-29 07:45:08 +0800

    Hash Table - Contains Duplicates II - my

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/containsduplicateii/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/containsduplicateii/Solution.java
@@ -1,7 +1,20 @@
 package com.vincennlin.leetcode.hashtable.easy.containsduplicateii;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //219
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> numIndexMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (numIndexMap.containsKey(nums[i]) && i - numIndexMap.get(nums[i]) <= k) {
+                return true;
+            }
+            numIndexMap.put(nums[i], i);
+        }
+
         return false;
     }
 }
