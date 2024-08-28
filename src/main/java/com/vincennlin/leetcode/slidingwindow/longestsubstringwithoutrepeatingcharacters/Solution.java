b503feaae23a77271353ed2788d93cda510b08f3
vincennlin <vincentagwa@gmail.com>
2024-08-28 11:54:39 +0800

    Sliding Window - Longest Substring Without Repeating Characters - my

--- src/main/java/com/vincennlin/leetcode/slidingwindow/longestsubstringwithoutrepeatingcharacters/Solution.java
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/longestsubstringwithoutrepeatingcharacters/Solution.java
@@ -1,7 +1,24 @@
 package com.vincennlin.leetcode.slidingwindow.longestsubstringwithoutrepeatingcharacters;
+
+import java.util.HashSet;
+import java.util.Set;
+
 //3
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        int leftIndex = 0, rightIndex = 0;
+        Set<Character> charSet = new HashSet<>();
+        int longest = 0;
+
+        while (rightIndex < s.length()) {
+            while (charSet.contains(s.charAt(rightIndex))) {
+                charSet.remove(s.charAt(leftIndex));
+                leftIndex++;
+            }
+            charSet.add(s.charAt(rightIndex));
+            rightIndex++;
+            longest = Math.max(rightIndex - leftIndex, longest);
+        }
+        return longest;
     }
 }
