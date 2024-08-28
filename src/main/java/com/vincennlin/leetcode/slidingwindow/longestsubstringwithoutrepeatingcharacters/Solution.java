a8ed351352c3399bd7e6cdbccbb6e270d4fb7494
vincennlin <vincentagwa@gmail.com>
2024-08-28 12:02:14 +0800

    Sliding Window - Longest Substring Without Repeating Characters - ans

--- src/main/java/com/vincennlin/leetcode/slidingwindow/longestsubstringwithoutrepeatingcharacters/Solution.java
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/longestsubstringwithoutrepeatingcharacters/Solution.java
@@ -1,24 +1,27 @@
 package com.vincennlin.leetcode.slidingwindow.longestsubstringwithoutrepeatingcharacters;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 //3
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int leftIndex = 0, rightIndex = 0;
-        Set<Character> charSet = new HashSet<>();
+        Map<Character, Integer> charIndexMap = new HashMap<>();
+        int leftIndex = 0;
         int longest = 0;
 
-        while (rightIndex < s.length()) {
-            while (charSet.contains(s.charAt(rightIndex))) {
-                charSet.remove(s.charAt(leftIndex));
-                leftIndex++;
+        for (int rightIndex = 0; rightIndex < s.length(); rightIndex++) {
+            char c = s.charAt(rightIndex);
+
+            if (charIndexMap.containsKey(c) && charIndexMap.get(c) >= leftIndex) {
+                leftIndex = charIndexMap.get(c) + 1;
             }
-            charSet.add(s.charAt(rightIndex));
-            rightIndex++;
-            longest = Math.max(rightIndex - leftIndex, longest);
+
+            charIndexMap.put(c, rightIndex);
+
+            longest = Math.max(longest, rightIndex - leftIndex + 1);
         }
+
         return longest;
     }
 }
