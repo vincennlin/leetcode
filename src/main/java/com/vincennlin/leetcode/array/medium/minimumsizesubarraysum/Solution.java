8d62a0c3fd705f23c8edef1fc6f0de477002eb2d
vincennlin <vincentagwa@gmail.com>
2024-08-28 11:35:13 +0800

    Sliding Window - Minimum Size Subarray Sum - Binary Search

--- src/main/java/com/vincennlin/leetcode/array/medium/minimumsizesubarraysum/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/minimumsizesubarraysum/Solution.java
@@ -17,4 +17,38 @@ class Solution {
         }
         return minSize == Integer.MAX_VALUE ? 0 : minSize;
     }
+//    Binary Search
+//
+//    public int minSubArrayLen(int target, int[] nums) {
+//        int[] prefixSum = new int[nums.length + 1];
+//        prefixSum[0] = 0;
+//
+//        for(int i = 1; i < prefixSum.length; i++) {
+//            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+//        }
+//
+//        int minSize = Integer.MAX_VALUE;
+//        for (int i = 1; i <= nums.length; i++) {
+//            int toFind = prefixSum[i] - target;
+//            int j = binarySearch(prefixSum, toFind);
+//            if (j != -1) {
+//                minSize = Math.min(i - j, minSize);
+//            }
+//        }
+//
+//        return minSize == Integer.MAX_VALUE ? 0 : minSize;
+//    }
+//
+//    private int binarySearch(int[] prefixSum, int target) {
+//        int left = 0, right = prefixSum.length - 1;
+//        while (left <= right) {
+//            int mid = left + (right - left) / 2;
+//            if (prefixSum[mid] <= target) {
+//                left = mid + 1;
+//            } else {
+//                right = mid - 1;
+//            }
+//        }
+//        return right;
+//    }
 }
