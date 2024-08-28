551ad814e53bac5f139c17a02cb5e81143a46227
vincennlin <vincentagwa@gmail.com>
2024-08-28 21:57:38 +0800

    Graph - Clone Graph - my

--- src/main/java/com/vincennlin/leetcode/graphgeneral/medium/clonegraph/Solution.java
+++ src/main/java/com/vincennlin/leetcode/graphgeneral/medium/clonegraph/Solution.java
@@ -1,7 +1,27 @@
 package com.vincennlin.leetcode.graphgeneral.medium.clonegraph;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //133
 class Solution {
+    Map<Node, Node> nodeMap;
+
     public Node cloneGraph(Node node) {
-        return null;
+        if (node == null) return null;
+        nodeMap = new HashMap<>();
+        dfs(node);
+        return nodeMap.get(node);
+    }
+
+    private void dfs(Node node) {
+        if (!nodeMap.containsKey(node)) {
+            Node clonedNode = new Node(node.val);
+            nodeMap.put(node, clonedNode);
+            for (Node neighbor : node.neighbors) {
+                dfs(neighbor);
+                clonedNode.neighbors.add(nodeMap.get(neighbor));
+            }
+        }
     }
 }
