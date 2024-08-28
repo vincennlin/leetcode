27c11eee0511dd949fd110e5bb50f06308a00efa
vincennlin <vincentagwa@gmail.com>
2024-08-28 12:49:30 +0800

    Matrix - Spiral Matrix

---
+++ src/main/java/com/vincennlin/leetcode/matrix/medium/spiralmatrix/Main.java
@@ -0,0 +1,40 @@
+package com.vincennlin.leetcode.matrix.medium.spiralmatrix;
+/*
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Example 2:
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println(solution.spiralOrder(matrix1)); // [1,2,3,6,9,8,7,4,5]
+
+        int[][] matrix2 = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+        };
+        System.out.println(solution.spiralOrder(matrix2)); // [1,2,3,4,8,12,11,10,9,5,6,7]
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/matrix/medium/spiralmatrix/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.matrix.medium.spiralmatrix;
+
+import java.util.List;
+
+//54
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        return null;
+    }
+}
