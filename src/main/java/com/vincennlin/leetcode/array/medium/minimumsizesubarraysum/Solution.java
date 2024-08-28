5033d0ac16959a825b526585e5564779de7840f7
vincennlin <vincentagwa@gmail.com>
2024-08-28 11:03:10 +0800

    Sliding Window - Minimum Size Subarray Sum - ans

--- src/main/java/com/vincennlin/leetcode/array/medium/minimumsizesubarraysum/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/minimumsizesubarraysum/Solution.java
@@ -3,21 +3,18 @@ package com.vincennlin.leetcode.array.medium.minimumsizesubarraysum;
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int leftIndex = 0, rightIndex = 0;
-        int sum = nums[0];
+        int sum = 0;
         int minSize = Integer.MAX_VALUE;
         while (rightIndex < nums.length) {
-            if (sum < target) {
-                rightIndex++;
-                if (rightIndex == nums.length) return minSize != Integer.MAX_VALUE ? minSize : 0;
-                sum += nums[rightIndex];
-            } else {
-                if (rightIndex - leftIndex + 1 < minSize) {
-                    minSize = rightIndex - leftIndex + 1;
-                }
-                sum -= nums[leftIndex];
-                leftIndex++;
+            sum += nums[rightIndex];
+
+            while (sum >= target) {
+                minSize = Math.min(rightIndex - leftIndex + 1, minSize);
+                sum -= nums[leftIndex++];
             }
+
+            rightIndex++;
         }
-        return minSize;
+        return minSize == Integer.MAX_VALUE ? 0 : minSize;
     }
 }
