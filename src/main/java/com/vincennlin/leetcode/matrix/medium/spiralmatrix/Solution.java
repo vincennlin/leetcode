62b5fbf464aa5e82a23d353beb535faa311b5168
vincennlin <vincentagwa@gmail.com>
2024-08-28 13:12:20 +0800

    Matrix - Spiral Matrix - ans

--- src/main/java/com/vincennlin/leetcode/matrix/medium/spiralmatrix/Solution.java
+++ src/main/java/com/vincennlin/leetcode/matrix/medium/spiralmatrix/Solution.java
@@ -1,10 +1,42 @@
 package com.vincennlin.leetcode.matrix.medium.spiralmatrix;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //54
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+
+        int top = 0;
+        int right = matrix[0].length - 1;
+        int bottom = matrix.length - 1;
+        int left = 0;
+
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; j++) {
+                result.add(matrix[top][j]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top > bottom || left > right) break;
+
+            for (int j = right; j >= left; j--) {
+                result.add(matrix[bottom][j]);
+            }
+            bottom--;
+
+            for (int i = bottom; i >= top; i--) {
+                result.add(matrix[i][left]);
+            }
+            left++;
+        }
+
+        return result;
     }
 }
