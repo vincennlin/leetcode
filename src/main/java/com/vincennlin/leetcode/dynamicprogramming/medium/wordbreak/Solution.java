85ab94cf3faa33e4875da2a1d3a45f874f26ed05
vincennlin <vincentagwa@gmail.com>
2024-08-29 14:04:40 +0800

    Dynamic Programming - Word Break

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/medium/wordbreak/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.dynamicprogramming.medium.wordbreak;
+
+import java.util.List;
+
+//139
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return false;
+    }
+}
