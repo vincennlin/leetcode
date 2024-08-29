5185c7d73b34cfd9fea8112f228ac9fcaae8b9b1
vincennlin <vincentagwa@gmail.com>
2024-08-29 20:22:39 +0800

    Dynamic Programming - Coin Change - ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/medium/coinchange/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/medium/coinchange/Solution.java
@@ -1,21 +1,24 @@
 package com.vincennlin.leetcode.dynamicprogramming.medium.coinchange;
+
+import java.util.Arrays;
+
 //322
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) return 0;
+
         int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0;
 
         for (int i = 1; i <= amount; i++) {
-            dp[i] = Integer.MAX_VALUE;
             for (int coin : coins) {
-                if ((coin < i && dp[i - coin] != 0) || coin == i) {
+                if (coin <= i) {
                     dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
                 }
             }
-            if (dp[i] == Integer.MAX_VALUE) dp[i] = 0;
         }
 
-        return dp[amount] == 0 ? -1 : dp[amount];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
