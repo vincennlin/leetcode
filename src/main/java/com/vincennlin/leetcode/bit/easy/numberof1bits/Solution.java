c810721813c30a32813322624eec617bac3feb0f
vincennlin <vincentagwa@gmail.com>
2024-08-29 09:10:24 +0800

    Bit - Number of 1 Bits - ans

--- src/main/java/com/vincennlin/leetcode/bit/easy/numberof1bits/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/easy/numberof1bits/Solution.java
@@ -2,6 +2,11 @@ package com.vincennlin.leetcode.bit.easy.numberof1bits;
 //191
 class Solution {
     public int hammingWeight(int n) {
-        return 0;
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
     }
 }
