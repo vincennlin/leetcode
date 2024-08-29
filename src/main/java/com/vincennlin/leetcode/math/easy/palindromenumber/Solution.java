a96f08cf3c3f752be43be83cc77d718b48baa0b4
vincennlin <vincentagwa@gmail.com>
2024-08-29 08:03:26 +0800

    Math - Palindrome Number - ans

--- src/main/java/com/vincennlin/leetcode/math/easy/palindromenumber/Solution.java
+++ src/main/java/com/vincennlin/leetcode/math/easy/palindromenumber/Solution.java
@@ -2,6 +2,15 @@ package com.vincennlin.leetcode.math.easy.palindromenumber;
 //9
 class Solution {
     public boolean isPalindrome(int x) {
-        return true;
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedNumber = 0;
+
+        while (x > reversedNumber) {
+            reversedNumber = reversedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversedNumber || x == reversedNumber / 10;
     }
 }
