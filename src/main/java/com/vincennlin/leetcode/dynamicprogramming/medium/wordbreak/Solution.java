a5ca4a9dd57659c089a725d387935b7aad3edd0b
vincennlin <vincentagwa@gmail.com>
2024-08-29 15:02:28 +0800

    Dynamic Programming - Word Break - ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/medium/wordbreak/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/medium/wordbreak/Solution.java
@@ -1,10 +1,26 @@
 package com.vincennlin.leetcode.dynamicprogramming.medium.wordbreak;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 //139
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return false;
+        int n = s.length();
+
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (String word : wordDict) {
+                if (s.startsWith(word, i - word.length()) && dp[i - word.length()]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
     }
 }
