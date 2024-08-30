9abd8f73ba7b127e4d84742cc8afb9579eb165e7
vincennlin <vincentagwa@gmail.com>
2024-08-30 18:58:03 +0800

    Backtracking - Combinations - ans - bad

--- src/main/java/com/vincennlin/leetcode/backtracking/medium/combinations/Solution.java
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/combinations/Solution.java
@@ -1,10 +1,26 @@
 package com.vincennlin.leetcode.backtracking.medium.combinations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // 77
 class Solution {
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int start, int n, int k) {
+        if (tempList.size() == k) {
+            result.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = start; i <= n; i++) {
+                if (tempList.contains(i)) continue;
+                tempList.add(i);
+                backtrack(result, tempList, i + 1, n, k);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
     public List<List<Integer>> combine(int n, int k) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), 1, n, k);
+        return result;
     }
 }
