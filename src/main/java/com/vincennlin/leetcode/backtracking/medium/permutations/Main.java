b3bb382beb274ac9d63e222ef5977b5b4ad84b91
vincennlin <vincentagwa@gmail.com>
2024-08-30 18:42:00 +0800

    Backtracking - Permutations

---
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/permutations/Main.java
@@ -0,0 +1,36 @@
+package com.vincennlin.leetcode.backtracking.medium.permutations;
+/*
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+
+Constraints:
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.permute(new int[]{1, 2, 3})); // [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+
+        System.out.println(solution.permute(new int[]{0, 1})); // [[0, 1], [1, 0]]
+
+        System.out.println(solution.permute(new int[]{1})); // [[1]]
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/permutations/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.backtracking.medium.permutations;
+
+import java.util.List;
+
+// 46
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        return null;
+    }
+}
