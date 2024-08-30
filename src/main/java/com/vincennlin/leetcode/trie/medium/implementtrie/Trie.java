4efe0c68c6d724819540e15f1dc7dc36aa67be2e
vincennlin <vincentagwa@gmail.com>
2024-08-30 16:51:23 +0800

    Trie - Implement Trie (Prefix Tree)

---
+++ src/main/java/com/vincennlin/leetcode/trie/medium/implementtrie/Trie.java
@@ -0,0 +1,20 @@
+package com.vincennlin.leetcode.trie.medium.implementtrie;
+// 208
+class Trie {
+
+    public Trie() {
+
+    }
+
+    public void insert(String word) {
+
+    }
+
+    public boolean search(String word) {
+        return false;
+    }
+
+    public boolean startsWith(String prefix) {
+        return false;
+    }
+}
