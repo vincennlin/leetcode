f6ed72f530de84474e7d5d41f7ee4305c8491141
vincennlin <vincentagwa@gmail.com>
2024-08-30 14:24:14 +0800

    Multidimensional DP - Longest Palindromic Substring - ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/longestpalindromicsubstring/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/longestpalindromicsubstring/Solution.java
@@ -12,15 +12,19 @@ class Solution {
             dp[i][i] = true;
         }
 
-        for (int interval = 1; interval < n; interval++) {
-            for (int i = 0; i < n; i++) {
-                int j = i + 1;
-                while (j - i <= interval && j < n) {
-                    if (s.charAt(i) == s.charAt(j) && (j - i == 1 || dp[i + 1][j - 1])) {
-                        dp[i][j] = true;
-                        ans = s.substring(i, j + 1);
-                    }
-                    j++;
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                ans = s.substring(i, i + 2);
+            }
+        }
+
+        for (int length = 3; length <= n; length++) {
+            for (int i = 0; i <= n - length; i++) {
+                int j = i + length - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ans = s.substring(i, j + 1);
                 }
             }
         }
