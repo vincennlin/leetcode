bdf940be86a7c690b246bfe147f2e36d55abd784
vincennlin <vincentagwa@gmail.com>
2024-08-30 13:36:45 +0800

    Multidimensional DP - Unique Paths II - my & ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/uniquepathsii/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/uniquepathsii/Solution.java
@@ -2,6 +2,24 @@ package com.vincennlin.leetcode.dynamicprogramming.multidimensional.medium.uniqu
 // 63
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1) return 0;
+        obstacleGrid[0][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+            obstacleGrid[i][0] = obstacleGrid[i][0] == 1 ? 0 : obstacleGrid[i - 1][0];
+        }
+        for (int j = 1; j < n; j++) {
+            obstacleGrid[0][j] = obstacleGrid[0][j] == 1 ? 0 : obstacleGrid[0][j - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                obstacleGrid[i][j] = obstacleGrid[i][j] == 1 ? 0 : obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+            }
+        }
+
+        return obstacleGrid[m - 1][n - 1];
     }
 }
