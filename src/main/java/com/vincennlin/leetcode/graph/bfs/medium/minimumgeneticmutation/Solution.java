ee94f12e3fd050743f2af06cc5d850dacb291a67
vincennlin <vincentagwa@gmail.com>
2024-08-30 16:47:01 +0800

    Graph BFS - Minimum Genetic Mutation - ans

--- src/main/java/com/vincennlin/leetcode/graph/bfs/medium/minimumgeneticmutation/Solution.java
+++ src/main/java/com/vincennlin/leetcode/graph/bfs/medium/minimumgeneticmutation/Solution.java
@@ -1,7 +1,53 @@
 package com.vincennlin.leetcode.graph.bfs.medium.minimumgeneticmutation;
+
+import java.util.*;
+
 // 433
 class Solution {
     public int minMutation(String startGene, String endGene, String[] bank) {
-        return 0;
+        Set<String> bankSet = new HashSet<>(List.of(bank));
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+
+        int mutations = 0;
+
+        queue.add(startGene);
+        visited.add(startGene);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                String current = queue.poll();
+
+                if (current.equals(endGene)) {
+                    return mutations;
+                }
+
+                char[] currentArray = current.toCharArray();
+                for (int j = 0; j < currentArray.length; j++) {
+                    char originalChar = currentArray[j];
+
+                    for (char c : new char[]{'A', 'C', 'G', 'T'}) {
+                        if (originalChar == c) continue;
+
+                        currentArray[j] = c;
+
+                        String next = new String(currentArray);
+
+                        if (bankSet.contains(next) && !visited.contains(next)) {
+                            queue.add(next);
+                            visited.add(next);
+                        }
+                    }
+
+                    currentArray[j] = originalChar;
+                }
+            }
+
+            mutations++;
+        }
+
+        return -1;
     }
 }
