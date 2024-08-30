26b42a28d75a055668a671cdf84f879d32de40ae
vincennlin <vincentagwa@gmail.com>
2024-08-30 16:03:28 +0800

    Multidimensional DP - Interleaving String - ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/interleavingstring/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/interleavingstring/Solution.java
@@ -4,10 +4,7 @@ class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         int m = s1.length();
         int n = s2.length();
-        int k = s3.length();
-        if (m == 0 || n == 0) {
-            return s3.equals(s1) || s3.equals(s2);
-        } else if (k != m + n) {
+        if (s3.length() != m + n) {
             return false;
         }
 
@@ -15,10 +12,10 @@ class Solution {
         dp[0][0] = true;
 
         for (int i = 1; i <= m; i++) {
-            dp[i][0] = s1.substring(0, i).equals(s3.substring(0, i));
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
         }
         for (int j = 1; j <= n; j++) {
-            dp[0][j] = s2.substring(0, j).equals(s3.substring(0, j));
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
         }
 
         for (int i = 1; i <= m; i++) {
