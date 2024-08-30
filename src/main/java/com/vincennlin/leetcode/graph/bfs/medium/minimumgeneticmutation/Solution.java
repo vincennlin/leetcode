0fd451457fbcf32fa45629bdbbc1a0f7df6d1ef2
vincennlin <vincentagwa@gmail.com>
2024-08-30 16:21:03 +0800

    Graph BFS - Minimum Genetic Mutation

---
+++ src/main/java/com/vincennlin/leetcode/graph/bfs/medium/minimumgeneticmutation/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.graph.bfs.medium.minimumgeneticmutation;
+// 433
+class Solution {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        return 0;
+    }
+}
