b870ad1e9e1e8b474fd4f4c1c75b389db668c48d
vincennlin <vincentagwa@gmail.com>
2024-08-30 18:50:40 +0800

    Backtracking - Combinations

---
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/combinations/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.backtracking.medium.combinations;
+
+import java.util.List;
+
+// 77
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        return null;
+    }
+}
