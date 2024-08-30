5455b5fee87d596fed87421bc8afab47b7f62093
vincennlin <vincentagwa@gmail.com>
2024-08-31 07:55:14 +0800

    Backtracking - Combinations - ans

--- src/main/java/com/vincennlin/leetcode/backtracking/medium/combinations/Solution.java
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/combinations/Solution.java
@@ -5,22 +5,22 @@ import java.util.List;
 
 // 77
 class Solution {
-    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int start, int n, int k) {
-        if (tempList.size() == k) {
-            result.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = start; i <= n; i++) {
-                if (tempList.contains(i)) continue;
-                tempList.add(i);
-                backtrack(result, tempList, i + 1, n, k);
-                tempList.remove(tempList.size() - 1);
-            }
-        }
-    }
-
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), 1, n, k);
+        combineHelper(result, new ArrayList<>(k), 1, n, k);
         return result;
+    }
+
+    private void combineHelper(List<List<Integer>> result, List<Integer> tempList, int start, int n, int k) {
+        if (tempList.size() == k) {
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            tempList.add(i);
+            combineHelper(result, tempList, i + 1, n, k);
+            tempList.remove(tempList.size() - 1);
+        }
     }
 }
