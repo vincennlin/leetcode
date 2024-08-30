01d8e4bb1457be2668f72f1b4656e9a3f6972c6e
vincennlin <vincentagwa@gmail.com>
2024-08-30 18:48:48 +0800

    Backtracking - Permutations - ans

--- src/main/java/com/vincennlin/leetcode/backtracking/medium/permutations/Solution.java
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/permutations/Solution.java
@@ -1,10 +1,26 @@
 package com.vincennlin.leetcode.backtracking.medium.permutations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // 46
 class Solution {
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+        } else {
+            for (int num : nums) {
+                if (tempList.contains(num)) continue;
+                tempList.add(num);
+                backtrack(result, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
     public List<List<Integer>> permute(int[] nums) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums);
+        return result;
     }
 }
