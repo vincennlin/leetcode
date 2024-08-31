853d6702811f777aa81391eb33623c2a971cb7df
vincennlin <vincentagwa@gmail.com>
2024-08-31 18:15:45 +0800

    Backtracking - Generate Parentheses - ans

--- src/main/java/com/vincennlin/leetcode/backtracking/medium/generateparentheses/Solution.java
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/generateparentheses/Solution.java
@@ -1,10 +1,28 @@
 package com.vincennlin.leetcode.backtracking.medium.generateparentheses;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // 22
 class Solution {
     public List<String> generateParenthesis(int n) {
-        return null;
+        List<String> result = new ArrayList<>();
+        backtracking(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtracking(List<String> result, String tempString, int open, int close, int max) {
+        if (tempString.length() == max * 2) {
+            result.add(tempString);
+            return;
+        }
+
+        if (open < max) {
+            backtracking(result, tempString + "(", open + 1, close, max);
+        }
+
+        if (close < open) {
+            backtracking(result, tempString + ")", open, close + 1, max);
+        }
     }
 }
