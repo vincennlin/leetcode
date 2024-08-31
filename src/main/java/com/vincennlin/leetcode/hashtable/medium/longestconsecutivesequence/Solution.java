d713598fce37508ccf00fbdbf07b27c5817cf7ca
vincennlin <vincentagwa@gmail.com>
2024-08-31 17:49:59 +0800

    Hash Table - Longest Consecutive Sequence - ans

--- src/main/java/com/vincennlin/leetcode/hashtable/medium/longestconsecutivesequence/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/longestconsecutivesequence/Solution.java
@@ -1,7 +1,33 @@
 package com.vincennlin.leetcode.hashtable.medium.longestconsecutivesequence;
+
+import java.util.HashSet;
+import java.util.Set;
+
 // 128
 class Solution {
     public int longestConsecutive(int[] nums) {
-        return 0;
+        Set<Integer> numSet = new HashSet<>();
+
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : nums) {
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(currentStreak, longestStreak);
+            }
+        }
+
+        return longestStreak;
     }
 }
