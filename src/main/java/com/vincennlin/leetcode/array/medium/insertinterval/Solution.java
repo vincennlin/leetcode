306833ccc3ae83426f8ca5ea68b07ab706f6c236
vincennlin <vincentagwa@gmail.com>
2024-08-31 20:40:04 +0800

    Intervals - Insert Interval

---
+++ src/main/java/com/vincennlin/leetcode/array/medium/insertinterval/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.array.medium.insertinterval;
+// 57
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        return new int[][]{};
+    }
+}
