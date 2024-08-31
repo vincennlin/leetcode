9d1073dc42b0ad6411d364a30a26e6cc561f5676
vincennlin <vincentagwa@gmail.com>
2024-08-31 21:32:14 +0800

    Intervals - Minimum Number of Arrows to Burst Balloons - ans

--- src/main/java/com/vincennlin/leetcode/intervals/medium/minimumnumberofarrowstoburstballons/Solution.java
+++ src/main/java/com/vincennlin/leetcode/intervals/medium/minimumnumberofarrowstoburstballons/Solution.java
@@ -1,7 +1,22 @@
 package com.vincennlin.leetcode.intervals.medium.minimumnumberofarrowstoburstballons;
+
+import java.util.Arrays;
+
 // 452
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        return 0;
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int arrows = 1;
+        int lastArrow = points[0][1];
+
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > lastArrow) {
+                lastArrow = points[i][1];
+                arrows++;
+            }
+        }
+
+        return arrows;
     }
 }
