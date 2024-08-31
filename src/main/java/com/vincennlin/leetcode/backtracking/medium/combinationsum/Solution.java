c3a5463d6cccccfd65931c9964de19b7d16e01a8
vincennlin <vincentagwa@gmail.com>
2024-08-31 08:21:32 +0800

    Backtracking - Combination Sum - my & ans

--- src/main/java/com/vincennlin/leetcode/backtracking/medium/combinationsum/Solution.java
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/combinationsum/Solution.java
@@ -1,10 +1,27 @@
 package com.vincennlin.leetcode.backtracking.medium.combinationsum;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // 39
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), 0, 0, target, candidates);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int start, int currentSum, int target, int[] candidates) {
+        if (currentSum == target) {
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (currentSum + candidates[i] > target) continue;
+            tempList.add(candidates[i]);
+            backtrack(result, tempList, i, currentSum + candidates[i], target, candidates);
+            tempList.remove(tempList.size() - 1);
+        }
     }
 }
