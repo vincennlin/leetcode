0a70cea037374b4e40da46612179a47ea9ebde3b
vincennlin <vincentagwa@gmail.com>
2024-08-31 17:28:16 +0800

    Hash Table - Group Anagrams - my

--- src/main/java/com/vincennlin/leetcode/hashtable/medium/groupanagrams/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/groupanagrams/Solution.java
@@ -1,10 +1,22 @@
 package com.vincennlin.leetcode.hashtable.medium.groupanagrams;
 
-import java.util.List;
+import java.util.*;
 
 // 49
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+
+            String sortedStr = String.valueOf(charArray);
+            List<String> anagramList = anagramMap.getOrDefault(sortedStr, new ArrayList<>());
+            anagramList.add(str);
+            anagramMap.put(sortedStr, anagramList);
+        }
+
+        return new ArrayList<>(anagramMap.values());
     }
 }
