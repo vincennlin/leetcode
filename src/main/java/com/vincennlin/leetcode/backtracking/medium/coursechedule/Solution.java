d1ddffab526d30d913adb721c3b67ff278649f2c
vincennlin <vincentagwa@gmail.com>
2024-08-31 12:30:23 +0800

    Backtracking - Course Schedule

---
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/coursechedule/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.backtracking.medium.coursechedule;
+// 207
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        return false;
+    }
+}
