b25649660eeaa58737688e04166d9049a3479179
vincennlin <vincentagwa@gmail.com>
2024-08-31 16:35:38 +0800

    Multidimensional DP - Maximal Square

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/maximalsquare/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dynamicprogramming.multidimensional.medium.maximalsquare;
+// 221
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        return 0;
+    }
+}
