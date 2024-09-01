9d171293bbb232169d8ce39d1bf5b8b39130b942
vincennlin <vincentagwa@gmail.com>
2024-09-01 19:49:08 +0800

    Stack - Asteroid Collision - ans

--- src/main/java/com/vincennlin/leetcode/stack/medium/asteroidcollision/Solution.java
+++ src/main/java/com/vincennlin/leetcode/stack/medium/asteroidcollision/Solution.java
@@ -1,7 +1,41 @@
 package com.vincennlin.leetcode.stack.medium.asteroidcollision;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
 // 735
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        return new int[]{};
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int asteroid : asteroids) {
+            boolean exploded = false;
+
+            while (!deque.isEmpty() && asteroid < 0 && deque.peekLast() > 0) {
+                int top = deque.peekLast();
+
+                if (top < -asteroid) {
+                    deque.removeLast();
+                } else if (top > -asteroid) {
+                    exploded = true;
+                    break;
+                } else {
+                    deque.removeLast();
+                    exploded = true;
+                    break;
+                }
+            }
+
+            if (!exploded) {
+                deque.addLast(asteroid);
+            }
+        }
+
+        int[] result = new int[deque.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = deque.removeFirst();
+        }
+
+        return result;
     }
 }
