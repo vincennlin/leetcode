eaa7f60ebfc7f1fc9a496c8ce60781eecebc820b
vincennlin <vincentagwa@gmail.com>
2024-09-01 10:24:02 +0800

    Linked List - Delete the Middle Node of a Linked List - my & ans

--- src/main/java/com/vincennlin/leetcode/linkedlist/medium/deletethemiddlenodeofalinkedlist/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/deletethemiddlenodeofalinkedlist/Solution.java
@@ -2,6 +2,24 @@ package com.vincennlin.leetcode.linkedlist.medium.deletethemiddlenodeofalinkedli
 // 2095
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        return null;
+        if (head == null) return null;
+
+        ListNode beforeNode = null;
+        ListNode slowNode = head;
+        ListNode fastNode = head;
+
+        while (fastNode != null && fastNode.next != null) {
+            beforeNode = slowNode;
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next;
+        }
+
+        if (beforeNode != null) {
+            beforeNode.next = slowNode.next;
+        } else {
+            head = null;
+        }
+
+        return head;
     }
 }
