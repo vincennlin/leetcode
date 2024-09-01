6ab39e0e8a7550349ffccfc53c1a1d3446b7b7ca
vincennlin <vincentagwa@gmail.com>
2024-09-01 10:09:52 +0800

    Linked List - Reverse Linked List - my & ans

--- src/main/java/com/vincennlin/leetcode/linkedlist/easy/reverselinkedlist/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/easy/reverselinkedlist/Solution.java
@@ -2,6 +2,21 @@ package com.vincennlin.leetcode.linkedlist.easy.reverselinkedlist;
 // 206
 class Solution {
     public ListNode reverseList(ListNode head) {
-        return null;
+        if (head == null) return null;
+
+        ListNode before = null;
+        ListNode current = head;
+        ListNode after;
+
+        while (current != null) {
+            after = current.next;
+            current.next = before;
+            before = current;
+            current = after;
+        }
+
+        head = before;
+
+        return head;
     }
 }
