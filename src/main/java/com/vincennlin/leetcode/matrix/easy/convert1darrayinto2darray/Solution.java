40e638e09d2455e1e9cec69463dcfeeb07c6521b
vincennlin <vincentagwa@gmail.com>
2024-09-02 06:53:20 +0800

    Matrix - Convert 1D Array Into 2D Array - my

--- src/main/java/com/vincennlin/leetcode/matrix/easy/convert1darrayinto2darray/Solution.java
+++ src/main/java/com/vincennlin/leetcode/matrix/easy/convert1darrayinto2darray/Solution.java
@@ -2,6 +2,16 @@ package com.vincennlin.leetcode.matrix.easy.convert1darrayinto2darray;
 // 2022
 class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
-        return new int[][]{};
+        if (original.length != m * n) return new int[][]{};
+
+        int[][] result = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = original[i * n + j];
+            }
+        }
+
+        return result;
     }
 }
