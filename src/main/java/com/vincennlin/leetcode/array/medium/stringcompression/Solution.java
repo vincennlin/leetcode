6c6e3e9ddb86314ea7651d4ac91dd8b8f9a998e2
vincennlin <vincentagwa@gmail.com>
2024-09-01 15:58:41 +0800

    Array - String Compression - ans

--- src/main/java/com/vincennlin/leetcode/array/medium/stringcompression/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/stringcompression/Solution.java
@@ -2,6 +2,29 @@ package com.vincennlin.leetcode.array.medium.stringcompression;
 // 443
 class Solution {
     public int compress(char[] chars) {
-        return 0;
+        if (chars.length == 1) return 1;
+
+        int write = 0, start = 0;
+        while (start < chars.length) {
+            char currentChar = chars[start];
+            int end = start;
+
+            while (end < chars.length && chars[end] == currentChar) {
+                end++;
+            }
+
+            int length = end - start;
+
+            chars[write++] = currentChar;
+            if (length > 1) {
+                for (char c : String.valueOf(length).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+
+            start = end;
+        }
+
+        return write;
     }
 }
