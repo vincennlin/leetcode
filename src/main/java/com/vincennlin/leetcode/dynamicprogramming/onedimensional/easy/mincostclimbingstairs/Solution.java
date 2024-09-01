685149894ec411c0249f56bf02982a3c5e184f95
vincennlin <vincentagwa@gmail.com>
2024-09-01 14:02:36 +0800

    Dynamic Programming - Min Cost Climbing Stairs

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/easy/mincostclimbingstairs/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dynamicprogramming.onedimensional.easy.mincostclimbingstairs;
+// 746
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        return 0;
+    }
+}
