1f609de9a07dd26d49d0a7aeddd69f8dafe0cc34
vincennlin <vincentagwa@gmail.com>
2024-09-01 08:49:00 +0800

    Array - Greatest Common Divisor of Strings

---
+++ src/main/java/com/vincennlin/leetcode/array/easy/greatestcommondivisorofstrings/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.array.easy.greatestcommondivisorofstrings;
+// 1071
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        return null;
+    }
+}
