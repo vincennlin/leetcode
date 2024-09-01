df04f23e05880f2fadfc8f76251e0b73ffeceee5
vincennlin <vincentagwa@gmail.com>
2024-09-01 18:20:06 +0800

    Stack - Removing Stars From a String - my & ans

--- src/main/java/com/vincennlin/leetcode/stack/medium/removingstarsfromastring/Solution.java
+++ src/main/java/com/vincennlin/leetcode/stack/medium/removingstarsfromastring/Solution.java
@@ -1,7 +1,27 @@
 package com.vincennlin.leetcode.stack.medium.removingstarsfromastring;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 // 2390
 class Solution {
     public String removeStars(String s) {
-        return "";
+        Deque<Character> charDeque = new LinkedList<>();
+
+        for (char c : s.toCharArray()) {
+            if (c != '*') {
+                charDeque.addLast(c);
+            } else {
+                charDeque.removeLast();
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (char c : charDeque) {
+            result.append(c);
+        }
+
+        return result.toString();
     }
 }
