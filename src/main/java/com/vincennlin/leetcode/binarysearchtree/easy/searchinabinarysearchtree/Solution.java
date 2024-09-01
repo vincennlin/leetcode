a8fe58e6863b139b3f1ea851ea177e4e34a4912d
vincennlin <vincentagwa@gmail.com>
2024-09-01 11:02:11 +0800

    Binary Tree - Search in a Binary Search Tree - my & ans

--- src/main/java/com/vincennlin/leetcode/binarysearchtree/easy/searchinabinarysearchtree/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarysearchtree/easy/searchinabinarysearchtree/Solution.java
@@ -2,6 +2,16 @@ package com.vincennlin.leetcode.binarysearchtree.easy.searchinabinarysearchtree;
 // 700
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        return null;
+        return traverse(root, val);
+    }
+
+    private TreeNode traverse(TreeNode currentNode, int val) {
+        if (currentNode == null) return null;
+        if (currentNode.val == val) return currentNode;
+        if (val < currentNode.val) {
+            return traverse(currentNode.left, val);
+        } else {
+            return traverse(currentNode.right, val);
+        }
     }
 }
