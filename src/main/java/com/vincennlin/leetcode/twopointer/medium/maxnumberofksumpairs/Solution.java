c8ce49e1a4f3170fe91321bdad81fbfd7842e1ec
vincennlin <vincentagwa@gmail.com>
2024-09-01 09:22:13 +0800

    Two Pointers - Max Number of K-Sum Pairs - my & ans

--- src/main/java/com/vincennlin/leetcode/twopointer/medium/maxnumberofksumpairs/Solution.java
+++ src/main/java/com/vincennlin/leetcode/twopointer/medium/maxnumberofksumpairs/Solution.java
@@ -1,7 +1,29 @@
 package com.vincennlin.leetcode.twopointer.medium.maxnumberofksumpairs;
+
+import java.util.Arrays;
+
 // 1679
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        return 0;
+        if (nums.length < 2) return 0;
+
+        Arrays.sort(nums);
+
+        int left = 0, right = nums.length - 1;
+        int count = 0;
+
+        while (left < right) {
+            if (nums[left] + nums[right] == k) {
+                count++;
+                left++;
+                right--;
+            } else if (nums[left] + nums[right] > k) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return count;
     }
 }
