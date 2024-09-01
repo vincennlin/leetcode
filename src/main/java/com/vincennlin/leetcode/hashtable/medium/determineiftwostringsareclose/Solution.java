9b9b2ad3cc8d283fde840e0cf7dae1a5124d32cc
vincennlin <vincentagwa@gmail.com>
2024-09-01 17:08:58 +0800

    Hash Table - Determine if Two Strings Are Close

---
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/determineiftwostringsareclose/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.hashtable.medium.determineiftwostringsareclose;
+// 1657
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        return false;
+    }
+}
