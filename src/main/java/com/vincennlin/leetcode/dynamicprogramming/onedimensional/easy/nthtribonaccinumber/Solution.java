3c7a57dd4ac6dff1c296ff0bc8f439d957466aa3
vincennlin <vincentagwa@gmail.com>
2024-09-01 13:53:20 +0800

    Dynamic Programming - N-th Tribonacci Number - my

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/easy/nthtribonaccinumber/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/easy/nthtribonaccinumber/Solution.java
@@ -2,6 +2,23 @@ package com.vincennlin.leetcode.dynamicprogramming.onedimensional.easy.nthtribon
 // 1137
 class Solution {
     public int tribonacci(int n) {
-        return 0;
+        if (n < 1) {
+            return 0;
+        } else if (n < 3) {
+            return 1;
+        }
+        int first = 0;
+        int second = 1;
+        int third = 1;
+        int current = first + second + third;
+
+        for (int i = 0; i < n - 3; i++) {
+            first = second;
+            second = third;
+            third = current;
+            current = first + second + third;
+        }
+
+        return current;
     }
 }
