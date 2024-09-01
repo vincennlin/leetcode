beb4a0338ddd338ba9233d17d808b1e3cc45b717
vincennlin <vincentagwa@gmail.com>
2024-09-01 18:00:51 +0800

    Hash Table - Equal Row and Column Pairs - my - wrong

--- src/main/java/com/vincennlin/leetcode/hashtable/medium/equalrowandcolumnpairs/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/equalrowandcolumnpairs/Solution.java
@@ -1,7 +1,36 @@
 package com.vincennlin.leetcode.hashtable.medium.equalrowandcolumnpairs;
+
+import java.util.*;
+
 // 2352
 class Solution {
     public int equalPairs(int[][] grid) {
-        return 0;
+        int n = grid.length;
+
+        Set<List<Integer>> rowSet = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> row = new ArrayList<>();
+            for(int j = 0; j < n; j++) {
+                row.add(grid[i][j]);
+            }
+            rowSet.add(row);
+        }
+
+        int pairs = n - rowSet.size();
+
+        for (int j = 0; j < n; j++) {
+            List<Integer> col = new ArrayList<>();
+            for (int[] ints : grid) {
+                col.add(ints[j]);
+            }
+            if (rowSet.contains(col)) {
+                pairs++;
+            } else {
+                rowSet.add(col);
+            }
+        }
+
+        return pairs;
     }
 }
