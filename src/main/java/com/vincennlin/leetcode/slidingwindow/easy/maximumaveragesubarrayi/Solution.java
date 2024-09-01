d1703b22e1b94d5d0ad5ac1a3f6efd7c3b2dae85
vincennlin <vincentagwa@gmail.com>
2024-09-01 09:49:47 +0800

    Sliding Window - Maximum Average Subarray I - ans

--- src/main/java/com/vincennlin/leetcode/slidingwindow/easy/maximumaveragesubarrayi/Solution.java
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/easy/maximumaveragesubarrayi/Solution.java
@@ -1,7 +1,20 @@
 package com.vincennlin.leetcode.slidingwindow.easy.maximumaveragesubarrayi;
+
 // 643
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        return 0;
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        double maxSum = sum;
+
+        for (int end = k; end < nums.length; end++) {
+            sum = sum + nums[end] - nums[end - k];
+            maxSum = Math.max(sum, maxSum);
+        }
+
+        return maxSum / k;
     }
 }
