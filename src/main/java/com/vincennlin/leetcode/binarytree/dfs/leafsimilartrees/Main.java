543be486ac96e1781c29d0f3865686a51f7dfb41
vincennlin <vincentagwa@gmail.com>
2024-09-01 10:34:55 +0800

    Binary Tree - Leaf-Similar Trees

---
+++ src/main/java/com/vincennlin/leetcode/binarytree/dfs/leafsimilartrees/Main.java
@@ -0,0 +1,63 @@
+package com.vincennlin.leetcode.binarytree.dfs.leafsimilartrees;
+/*
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+Example 1:
+
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+
+Example 2:
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
+
+Constraints:
+
+The number of nodes in each tree will be in the range [1, 200].
+Both of the given trees will have values in the range [0, 200].
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        TreeNode root1 = new TreeNode(3);
+        root1.left = new TreeNode(5);
+        root1.right = new TreeNode(1);
+        root1.left.left = new TreeNode(6);
+        root1.left.right = new TreeNode(2);
+        root1.left.right.left = new TreeNode(7);
+        root1.left.right.right = new TreeNode(4);
+        root1.right.left = new TreeNode(9);
+        root1.right.right = new TreeNode(8);
+
+        TreeNode root2 = new TreeNode(3);
+        root2.left = new TreeNode(5);
+        root2.right = new TreeNode(1);
+        root2.left.left = new TreeNode(6);
+        root2.left.right = new TreeNode(7);
+        root2.left.right.left = new TreeNode(4);
+        root2.left.right.right = new TreeNode(2);
+        root2.right.left = new TreeNode(9);
+        root2.right.right = new TreeNode(8);
+
+        System.out.println(solution.leafSimilar(root1, root2)); // true
+
+        TreeNode root3 = new TreeNode(1);
+        root3.left = new TreeNode(2);
+        root3.right = new TreeNode(3);
+
+        TreeNode root4 = new TreeNode(1);
+        root4.left = new TreeNode(3);
+        root4.right = new TreeNode(2);
+
+        System.out.println(solution.leafSimilar(root3, root4)); // false
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/binarytree/dfs/leafsimilartrees/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.binarytree.dfs.leafsimilartrees;
+// 872
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        return false;
+    }
+}
