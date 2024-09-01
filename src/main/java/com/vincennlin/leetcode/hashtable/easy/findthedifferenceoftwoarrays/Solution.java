9a1f95335b4e6d70867193979818509a9529a83f
vincennlin <vincentagwa@gmail.com>
2024-09-01 12:54:44 +0800

    Hash Table - Find the Difference of Two Arrays - my

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/findthedifferenceoftwoarrays/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/findthedifferenceoftwoarrays/Solution.java
@@ -1,10 +1,41 @@
 package com.vincennlin.leetcode.hashtable.easy.findthedifferenceoftwoarrays;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 // 2215
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        return null;
+        Set<Integer> nums1Set = new HashSet<>();
+        Set<Integer> nums2Set = new HashSet<>();
+
+        for (int num : nums1) {
+            nums1Set.add(num);
+        }
+
+        for (int num : nums2) {
+            nums2Set.add(num);
+        }
+
+        List<Integer> nums1List = new ArrayList<>();
+        List<Integer> nums2List = new ArrayList<>();
+
+        for (int num : nums1) {
+            if (!nums2Set.contains(num)) {
+                nums1List.add(num);
+                nums2Set.add(num);
+            }
+        }
+
+        for (int num : nums2) {
+            if (!nums1Set.contains(num)) {
+                nums2List.add(num);
+                nums1Set.add(num);
+            }
+        }
+
+        return List.of(nums1List, nums2List);
     }
 }
