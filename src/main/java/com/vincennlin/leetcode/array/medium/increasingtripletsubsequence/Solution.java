8ad0f01a2c5a252d06a04f3d7cae77ab231f0c5b
vincennlin <vincentagwa@gmail.com>
2024-09-01 09:09:05 +0800

    Array - Increasing Triplet Subsequence - ans

--- src/main/java/com/vincennlin/leetcode/array/medium/increasingtripletsubsequence/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/increasingtripletsubsequence/Solution.java
@@ -2,6 +2,21 @@ package com.vincennlin.leetcode.array.medium.increasingtripletsubsequence;
 // 334
 class Solution {
     public boolean increasingTriplet(int[] nums) {
+        if (nums.length < 3) return false;
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+
         return false;
     }
 }
