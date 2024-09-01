987ff08c561388eafe7edd2e0062910abc177af4
vincennlin <vincentagwa@gmail.com>
2024-09-01 11:43:10 +0800

    Prefix Sum - Find Pivot Index - my & ans

--- src/main/java/com/vincennlin/leetcode/prefixsum/easy/findpivotindex/Solution.java
+++ src/main/java/com/vincennlin/leetcode/prefixsum/easy/findpivotindex/Solution.java
@@ -2,6 +2,26 @@ package com.vincennlin.leetcode.prefixsum.easy.findpivotindex;
 // 724
 class Solution {
     public int pivotIndex(int[] nums) {
-        return 0;
+        int sum = 0;
+        int[] sumLeft = new int[nums.length];
+        int[] sumRight = new int[nums.length];
+
+        for (int i = 0 ; i < nums.length; i++) {
+            sumLeft[i] = sum;
+            sum += nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            sum -= nums[i];
+            sumRight[i] = sum;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (sumLeft[i] == sumRight[i]) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
