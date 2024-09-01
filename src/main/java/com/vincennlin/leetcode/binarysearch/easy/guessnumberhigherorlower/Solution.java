400f1e40345aa23d2c4476ce68bf4eae98718562
vincennlin <vincentagwa@gmail.com>
2024-09-01 13:39:57 +0800

    Binary Search - Guess Number Higher or Lower - my & ans

--- src/main/java/com/vincennlin/leetcode/binarysearch/easy/guessnumberhigherorlower/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarysearch/easy/guessnumberhigherorlower/Solution.java
@@ -14,6 +14,24 @@ public class Solution extends GuessGame {
     }
 
     public int guessNumber(int n) {
+        int left = 0, right = n;
+
+        while (left <= right) {
+            if (left == right) return left;
+
+            int mid = left + (right - left) / 2;
+
+            int result = guess(mid);
+
+            if (result == 0) {
+                return mid;
+            } else if (result == 1) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
         return 0;
     }
 }
