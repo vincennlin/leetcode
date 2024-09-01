b8bac587f1bdc1a6c8a8773768cd7dff5ef685af
vincennlin <vincentagwa@gmail.com>
2024-09-01 20:36:40 +0800

    Linked List - Odd Even Linked List - my

--- src/main/java/com/vincennlin/leetcode/linkedlist/medium/oddevenlinkedlist/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/oddevenlinkedlist/Solution.java
@@ -2,6 +2,30 @@ package com.vincennlin.leetcode.linkedlist.medium.oddevenlinkedlist;
 // 328
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        return null;
+        if (head == null) return null;
+        if (head.next == null || head.next.next == null) return head;
+
+        ListNode oddHeadNode = new ListNode();
+        ListNode oddCurrentNode = oddHeadNode;
+        ListNode evenHeadNode = new ListNode();
+        ListNode evenCurrentNode = evenHeadNode;
+
+        boolean isOdd = true;
+        while (head != null) {
+            if (isOdd) {
+                oddCurrentNode.next = head;
+                oddCurrentNode = oddCurrentNode.next;
+            } else {
+                evenCurrentNode.next = head;
+                evenCurrentNode = evenCurrentNode.next;
+            }
+            head = head.next;
+            isOdd = !isOdd;
+        }
+
+        oddCurrentNode.next = evenHeadNode.next;
+        evenCurrentNode.next = null;
+
+        return oddHeadNode.next;
     }
 }
