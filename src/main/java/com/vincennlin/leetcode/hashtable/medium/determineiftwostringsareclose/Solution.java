0ce763dee5caba6edaee521644f2a9b733199312
vincennlin <vincentagwa@gmail.com>
2024-09-01 17:30:30 +0800

    Hash Table - Determine if Two Strings Are Close - ans

--- src/main/java/com/vincennlin/leetcode/hashtable/medium/determineiftwostringsareclose/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/determineiftwostringsareclose/Solution.java
@@ -5,25 +5,26 @@ import java.util.*;
 // 1657
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        Map<Character, Integer> word1Map = new HashMap<>();
-        Map<Character, Integer> word2Map = new HashMap<>();
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
 
         for (char c : word1.toCharArray()) {
-            word1Map.put(c, word1Map.getOrDefault(c, 0) + 1);
+            freq1[c - 'a'] += 1;
         }
+
         for (char c : word2.toCharArray()) {
-            word2Map.put(c, word2Map.getOrDefault(c, 0) + 1);
+            freq2[c - 'a'] += 1;
         }
 
-        if (!word1Map.keySet().equals(word2Map.keySet())) {
-            return false;
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
         }
 
-        List<Integer> word1Freq = new ArrayList<>(word1Map.values());
-        List<Integer> word2Freq = new ArrayList<>(word2Map.values());
-        Collections.sort(word1Freq);
-        Collections.sort(word2Freq);
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
 
-        return word1Freq.equals(word2Freq);
+        return Arrays.equals(freq1, freq2);
     }
 }
