9eace2f397933ffb2b6f310a9d45c6054d499171
vincennlin <vincentagwa@gmail.com>
2024-09-01 13:07:10 +0800

    Hash Table - Unique Number of Occurrences - my & ans

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/uniquenumberofoccurrences/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/uniquenumberofoccurrences/Solution.java
@@ -1,7 +1,21 @@
 package com.vincennlin.leetcode.hashtable.easy.uniquenumberofoccurrences;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 // 1207
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        return false;
+        Map<Integer, Integer> numOccurrenceMap = new HashMap<>();
+
+        for (int num : arr) {
+            numOccurrenceMap.put(num, numOccurrenceMap.getOrDefault(num, 0) + 1);
+        }
+
+        Set<Integer> occurrenceSet = new HashSet<>(numOccurrenceMap.values());
+
+        return occurrenceSet.size() == numOccurrenceMap.size();
     }
 }
