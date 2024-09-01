45cc6458944147f2b39069e4c02064eadd5f671c
vincennlin <vincentagwa@gmail.com>
2024-09-01 14:09:53 +0800

    Dynamic Programming - Min Cost Climbing Stairs - ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/easy/mincostclimbingstairs/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/easy/mincostclimbingstairs/Solution.java
@@ -2,6 +2,15 @@ package com.vincennlin.leetcode.dynamicprogramming.onedimensional.easy.mincostcl
 // 746
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        return 0;
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+        }
+
+        return dp[n];
     }
 }
