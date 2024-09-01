7ebfe2129aea94f1509c98995c51fcdc1099b21c
vincennlin <vincentagwa@gmail.com>
2024-09-01 13:25:45 +0800

    Queue - Number of Recent Calls

---
+++ src/main/java/com/vincennlin/leetcode/queue/easy/numberofrecentcalls/RecentCounter.java
@@ -0,0 +1,12 @@
+package com.vincennlin.leetcode.queue.easy.numberofrecentcalls;
+// 933
+class RecentCounter {
+
+    public RecentCounter() {
+
+    }
+
+    public int ping(int t) {
+        return 0;
+    }
+}
