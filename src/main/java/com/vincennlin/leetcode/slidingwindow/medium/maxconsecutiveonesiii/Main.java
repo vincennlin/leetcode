848db3db8df1df759416f016e631f0e98f5f0e85
vincennlin <vincentagwa@gmail.com>
2024-09-01 16:32:28 +0800

    Sliding Window - Max Consecutive Ones III

---
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/maxconsecutiveonesiii/Main.java
@@ -0,0 +1,33 @@
+package com.vincennlin.leetcode.slidingwindow.medium.maxconsecutiveonesiii;
+/*
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Example 1:
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+Example 2:
+
+Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+Output: 10
+Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+0 <= k <= nums.length
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.longestOnes(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2)); // 6
+
+        System.out.println(solution.longestOnes(new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3)); // 10
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/maxconsecutiveonesiii/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.slidingwindow.medium.maxconsecutiveonesiii;
+// 1004
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        return 0;
+    }
+}
