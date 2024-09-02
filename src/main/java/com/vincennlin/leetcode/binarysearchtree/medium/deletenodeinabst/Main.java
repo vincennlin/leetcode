772296eb258d568c2600e1948a44c1f3c3e94e4f
vincennlin <vincentagwa@gmail.com>
2024-09-02 16:57:32 +0800

    Binary Search Tree - Delete Node in a BST

---
+++ src/main/java/com/vincennlin/leetcode/binarysearchtree/medium/deletenodeinabst/Main.java
@@ -0,0 +1,64 @@
+package com.vincennlin.leetcode.binarysearchtree.medium.deletenodeinabst;
+/*
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+Example 2:
+
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
+
+Example 3:
+
+Input: root = [], key = 0
+Output: []
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-105 <= Node.val <= 105
+Each node has a unique value.
+root is a valid binary search tree.
+-105 <= key <= 105
+
+
+Follow up: Could you solve it with time complexity O(height of tree)?
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        TreeNode root1 = new TreeNode(5);
+        root1.left = new TreeNode(3);
+        root1.right = new TreeNode(6);
+        root1.left.left = new TreeNode(2);
+        root1.left.right = new TreeNode(4);
+        root1.right.right = new TreeNode(7);
+        System.out.println(solution.deleteNode(root1, 3)); // [5,4,6,2,null,null,7]
+
+        TreeNode root2 = new TreeNode(5);
+        root2.left = new TreeNode(3);
+        root2.right = new TreeNode(6);
+        root2.left.left = new TreeNode(2);
+        root2.left.right = new TreeNode(4);
+        root2.right.right = new TreeNode(7);
+        System.out.println(solution.deleteNode(root2, 0)); // [5,3,6,2,4,null,7]
+
+        TreeNode root3 = null;
+        System.out.println(solution.deleteNode(root3, 0)); // []
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/binarysearchtree/medium/deletenodeinabst/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.binarysearchtree.medium.deletenodeinabst;
+// 450
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return null;
+    }
+}
