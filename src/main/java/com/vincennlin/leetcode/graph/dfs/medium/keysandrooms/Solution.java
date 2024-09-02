b242a20c7c720353d3bb05761c49ee239f352c83
vincennlin <vincentagwa@gmail.com>
2024-09-02 18:06:28 +0800

    Graph DFS - Keys and Rooms - my

--- src/main/java/com/vincennlin/leetcode/graph/dfs/medium/keysandrooms/Solution.java
+++ src/main/java/com/vincennlin/leetcode/graph/dfs/medium/keysandrooms/Solution.java
@@ -1,10 +1,26 @@
 package com.vincennlin.leetcode.graph.dfs.medium.keysandrooms;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 //841
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        return false;
+        int n = rooms.size();
+        boolean[] isVisited = new boolean[n];
+        Deque<Integer> deque = new LinkedList<>(rooms.get(0));
+        isVisited[0] = true;
+        int visited = 1;
+
+        while (!deque.isEmpty()) {
+            int roomIndex = deque.poll();
+            if (isVisited[roomIndex]) continue;
+            visited++;
+            deque.addAll(rooms.get(roomIndex));
+            isVisited[roomIndex] = true;
+        }
+
+        return visited == n;
     }
 }
