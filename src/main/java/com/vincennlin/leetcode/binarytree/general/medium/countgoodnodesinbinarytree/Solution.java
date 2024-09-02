5ac863f921b3fe59efbee5d6020c309c56ac24c1
vincennlin <vincentagwa@gmail.com>
2024-09-02 15:38:45 +0800

    Binary Tree - Count Good Nodes in Binary Tree - my & ans

--- src/main/java/com/vincennlin/leetcode/binarytree/general/medium/countgoodnodesinbinarytree/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytree/general/medium/countgoodnodesinbinarytree/Solution.java
@@ -2,6 +2,12 @@ package com.vincennlin.leetcode.binarytree.general.medium.countgoodnodesinbinary
 
 class Solution {
     public int goodNodes(TreeNode root) {
-        return 0;
+        return traverse(root, Integer.MIN_VALUE);
+    }
+
+    private int traverse(TreeNode currentNode, int max) {
+        if (currentNode == null) return 0;
+        max = Math.max(currentNode.val, max);
+        return (currentNode.val >= max ? 1 : 0) + traverse(currentNode.left, max) + traverse(currentNode.right, max);
     }
 }
