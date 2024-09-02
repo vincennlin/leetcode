87ba924375dcbefc9dfb953c491b6a09b228715d
vincennlin <vincentagwa@gmail.com>
2024-09-02 19:44:56 +0800

    Graph BFS - Nearest Exit from Entrance in Maze

---
+++ src/main/java/com/vincennlin/leetcode/graph/bfs/medium/nearestexitfromentranceinmaze/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.graph.bfs.medium.nearestexitfromentranceinmaze;
+// 1926
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        return 0;
+    }
+}
