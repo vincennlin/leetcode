1b709bffc7dd9217ae6c98762f3192f7e3e62393
vincennlin <vincentagwa@gmail.com>
2024-09-02 20:37:51 +0800

    Graph DFS - Number of Provinces - my - wrong

--- src/main/java/com/vincennlin/leetcode/graph/dfs/medium/numberofprovinces/Solution.java
+++ src/main/java/com/vincennlin/leetcode/graph/dfs/medium/numberofprovinces/Solution.java
@@ -1,7 +1,20 @@
 package com.vincennlin.leetcode.graph.dfs.medium.numberofprovinces;
 // 547
 class Solution {
+    int provinces;
     public int findCircleNum(int[][] isConnected) {
-        return 0;
+        provinces = isConnected.length;
+        dfs(isConnected, 0);
+        return provinces;
+    }
+
+    private void dfs(int[][] isConnected, int current) {
+        for (int i = current + 1; i < isConnected.length; i++) {
+            if (isConnected[current][i] == 1) {
+                isConnected[i][current] = 0;
+                dfs(isConnected, current + 1);
+                provinces--;
+            }
+        }
     }
 }
