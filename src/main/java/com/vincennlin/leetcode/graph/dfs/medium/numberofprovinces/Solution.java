7d88a5d8c99236489c4524b292ab732aab86cc95
vincennlin <vincentagwa@gmail.com>
2024-09-02 20:59:42 +0800

    Graph DFS - Number of Provinces - ans

--- src/main/java/com/vincennlin/leetcode/graph/dfs/medium/numberofprovinces/Solution.java
+++ src/main/java/com/vincennlin/leetcode/graph/dfs/medium/numberofprovinces/Solution.java
@@ -15,12 +15,11 @@ class Solution {
     }
 
     private void dfs(int[][] isConnected, boolean[] isVisited, int i) {
+        isVisited[i] = true;
         for (int j = 0; j < isConnected.length; j++) {
             if (isConnected[i][j] == 1 && !isVisited[j]) {
-                isVisited[j] = true;
                 dfs(isConnected, isVisited, j);
             }
         }
-
     }
 }
