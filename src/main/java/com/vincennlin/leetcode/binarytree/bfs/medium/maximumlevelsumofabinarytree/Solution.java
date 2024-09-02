7cd5478ce9a56c251b552bfb61ca97d2a940db51
vincennlin <vincentagwa@gmail.com>
2024-09-02 16:53:20 +0800

    Binary Tree BFS - Maximum Level Sum of a Binary Tree - my & ans

--- src/main/java/com/vincennlin/leetcode/binarytree/bfs/medium/maximumlevelsumofabinarytree/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytree/bfs/medium/maximumlevelsumofabinarytree/Solution.java
@@ -1,7 +1,36 @@
 package com.vincennlin.leetcode.binarytree.bfs.medium.maximumlevelsumofabinarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 // 1161
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        return 0;
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int currentLevel = 0;
+        int maxLevel = 1;
+        int maxLevelSum = Integer.MIN_VALUE;
+
+        while (!nodeQueue.isEmpty()) {
+            currentLevel++;
+            int levelSize = nodeQueue.size();
+            int levelSum = 0;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = nodeQueue.poll();
+                if (currentNode.left != null) nodeQueue.add(currentNode.left);
+                if (currentNode.right != null) nodeQueue.add(currentNode.right);
+                levelSum += currentNode.val;
+            }
+
+            if (levelSum > maxLevelSum) {
+                maxLevelSum = levelSum;
+                maxLevel = currentLevel;
+            }
+        }
+
+        return maxLevel;
     }
 }
