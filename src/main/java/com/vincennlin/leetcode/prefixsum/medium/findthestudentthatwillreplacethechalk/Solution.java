707a7a77ea93ae35a30e3819b519ca6c4975b5e0
vincennlin <vincentagwa@gmail.com>
2024-09-03 06:46:39 +0800

    Prefix Sum - Find the Student that Will Replace the Chalk - my & ans

--- src/main/java/com/vincennlin/leetcode/prefixsum/medium/findthestudentthatwillreplacethechalk/Solution.java
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/findthestudentthatwillreplacethechalk/Solution.java
@@ -2,6 +2,29 @@ package com.vincennlin.leetcode.prefixsum.medium.findthestudentthatwillreplaceth
 // 1894
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        return 0;
+        int n = chalk.length;
+        long[] prefixSum = new long[n];
+        prefixSum[0] = chalk[0];
+        long sum = chalk[0];
+
+        for (int i = 1; i < n ; i++) {
+            prefixSum[i] = prefixSum[i - 1] + chalk[i];
+            sum += chalk[i];
+        }
+
+        k = (int)(k % sum);
+
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (k < prefixSum[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
     }
 }
