e08ab612cfecb2fd07d4eb83a74e75e0d213c67f
vincennlin <vincentagwa@gmail.com>
2024-09-02 17:47:48 +0800

    Graph DFS - Keys and Rooms

---
+++ src/main/java/com/vincennlin/leetcode/graph/dfs/medium/keysandrooms/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.graph.dfs.medium.keysandrooms;
+
+import java.util.List;
+
+//841
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        return false;
+    }
+}
