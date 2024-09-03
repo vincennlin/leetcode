0362a227343c15c2a9650498647d2cdf4c5b2e50
vincennlin <vincentagwa@gmail.com>
2024-09-04 07:30:48 +0800

    Simulation - Sum of Digits of String After Convert - ans

--- src/main/java/com/vincennlin/leetcode/simulation/easy/sumofdigitsofstringafterconvert/Solution.java
+++ src/main/java/com/vincennlin/leetcode/simulation/easy/sumofdigitsofstringafterconvert/Solution.java
@@ -2,6 +2,23 @@ package com.vincennlin.leetcode.simulation.easy.sumofdigitsofstringafterconvert;
 // 1945
 class Solution {
     public int getLucky(String s, int k) {
-        return 0;
+
+        StringBuilder number = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            number.append(c - 'a' + 1);
+        }
+
+        String digits = number.toString();
+
+        for (int i = 0; i < k; i++){
+            long newSum = 0;
+            for (char c : digits.toCharArray()) {
+                newSum += c - '0';
+            }
+            digits = Long.toString(newSum);
+        }
+
+        return Integer.parseInt(digits);
     }
 }
