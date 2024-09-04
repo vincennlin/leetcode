d4d8bdb890eeed73c989b60d0f1d8be650933624
vincennlin <vincentagwa@gmail.com>
2024-09-05 06:52:18 +0800

    Simulation - Walking Robot Simulation - ans

--- src/main/java/com/vincennlin/leetcode/simulation/medium/walkingrobotsimulation/Solution.java
+++ src/main/java/com/vincennlin/leetcode/simulation/medium/walkingrobotsimulation/Solution.java
@@ -5,10 +5,13 @@ import java.util.Set;
 
 // 874
 class Solution {
+
+    private static final int HASH_MULTIPLIER = 60001;
+
     public int robotSim(int[] commands, int[][] obstacles) {
-        Set<String> obstacleSet = new HashSet<>();
+        Set<Integer> obstacleSet = new HashSet<>();
         for (int[] obstacle : obstacles) {
-            obstacleSet.add(obstacle[0] + "," + obstacle[1]);
+            obstacleSet.add(hashCoordinates(obstacle[0], obstacle[1]));
         }
 
         int[][] directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
@@ -19,19 +22,20 @@ class Solution {
 
         for (int command : commands) {
             if (command < 0) {
-                dir_index += command == -1 ? 1 : -1;
-                if (dir_index < 0) {
-                    dir_index = 3;
-                } else if (dir_index > 3) {
-                    dir_index = 0;
+                if (command == - 1) {
+                    dir_index = (dir_index + 1) % 4;
+                } else {
+                    dir_index = (dir_index + 3) % 4;
                 }
             } else {
                 for (int i = 0; i < command; i++) {
-                    int[] nextPosition = new int[]{position[0] + directions[dir_index][0], position[1] + directions[dir_index][1]};
-                    if (obstacleSet.contains(nextPosition[0] + "," + nextPosition[1])) {
+                    int nextX = position[0] + directions[dir_index][0];
+                    int nextY = position[1] + directions[dir_index][1];
+                    if (obstacleSet.contains(hashCoordinates(nextX, nextY))) {
                         break;
                     } else {
-                        position = nextPosition;
+                        position[0] = nextX;
+                        position[1] = nextY;
                         maxDistanceSquare = Math.max(position[0] * position[0] + position[1] * position[1], maxDistanceSquare);
                     }
                 }
@@ -39,5 +43,9 @@ class Solution {
         }
 
         return maxDistanceSquare;
+    }
+
+    private int hashCoordinates(int x, int y) {
+        return x + HASH_MULTIPLIER * y;
     }
 }
