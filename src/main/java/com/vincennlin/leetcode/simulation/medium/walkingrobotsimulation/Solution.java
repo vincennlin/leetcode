d33465cb71f939fa7f51b4f113ffbc7844c8fdb8
vincennlin <vincentagwa@gmail.com>
2024-09-05 06:47:09 +0800

    Simulation - Walking Robot Simulation - my

--- src/main/java/com/vincennlin/leetcode/simulation/medium/walkingrobotsimulation/Solution.java
+++ src/main/java/com/vincennlin/leetcode/simulation/medium/walkingrobotsimulation/Solution.java
@@ -1,7 +1,43 @@
 package com.vincennlin.leetcode.simulation.medium.walkingrobotsimulation;
+
+import java.util.HashSet;
+import java.util.Set;
+
 // 874
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
-        return 0;
+        Set<String> obstacleSet = new HashSet<>();
+        for (int[] obstacle : obstacles) {
+            obstacleSet.add(obstacle[0] + "," + obstacle[1]);
+        }
+
+        int[][] directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int dir_index = 0;
+
+        int[] position = new int[]{0, 0};
+        int maxDistanceSquare = 0;
+
+        for (int command : commands) {
+            if (command < 0) {
+                dir_index += command == -1 ? 1 : -1;
+                if (dir_index < 0) {
+                    dir_index = 3;
+                } else if (dir_index > 3) {
+                    dir_index = 0;
+                }
+            } else {
+                for (int i = 0; i < command; i++) {
+                    int[] nextPosition = new int[]{position[0] + directions[dir_index][0], position[1] + directions[dir_index][1]};
+                    if (obstacleSet.contains(nextPosition[0] + "," + nextPosition[1])) {
+                        break;
+                    } else {
+                        position = nextPosition;
+                        maxDistanceSquare = Math.max(position[0] * position[0] + position[1] * position[1], maxDistanceSquare);
+                    }
+                }
+            }
+        }
+
+        return maxDistanceSquare;
     }
 }
