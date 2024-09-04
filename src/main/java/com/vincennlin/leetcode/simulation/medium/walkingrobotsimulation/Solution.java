3462424fc68d6dd3b1d8b3865987621513013f2a
vincennlin <vincentagwa@gmail.com>
2024-09-05 06:33:05 +0800

    Simulation - Walking Robot Simulation

---
+++ src/main/java/com/vincennlin/leetcode/simulation/medium/walkingrobotsimulation/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.simulation.medium.walkingrobotsimulation;
+// 874
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        return 0;
+    }
+}
