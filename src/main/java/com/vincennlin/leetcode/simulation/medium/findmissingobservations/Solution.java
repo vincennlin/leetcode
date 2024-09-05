4e6df2164346622454136594ed515a7cddb2c4d4
vincennlin <vincentagwa@gmail.com>
2024-09-06 07:49:32 +0800

    Simulation - Find Missing Observations - my & ans

--- src/main/java/com/vincennlin/leetcode/simulation/medium/findmissingobservations/Solution.java
+++ src/main/java/com/vincennlin/leetcode/simulation/medium/findmissingobservations/Solution.java
@@ -1,7 +1,30 @@
 package com.vincennlin.leetcode.simulation.medium.findmissingobservations;
+
+import java.util.Arrays;
+
 // 2028
 class Solution {
     public int[] missingRolls(int[] rolls, int mean, int n) {
-        return new int[]{};
+
+        int rollsSum = 0;
+        for (int roll : rolls) {
+            rollsSum += roll;
+        }
+
+        int missingSum = (rolls.length + n) * mean - rollsSum;
+
+        if (missingSum < n || missingSum > n * 6) {
+            return new int[]{};
+        }
+
+        int[] result = new int[n];
+        Arrays.fill(result, missingSum / n);
+        missingSum = missingSum % n;
+
+        for (int i = 0; i < missingSum; i++) {
+            result[i] ++;
+        }
+
+        return result;
     }
 }
