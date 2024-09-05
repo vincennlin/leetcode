0e748d651441666f9689ccb0ca1457e76e601087
vincennlin <vincentagwa@gmail.com>
2024-09-06 07:38:54 +0800

    Simulation - Find Missing Observations

---
+++ src/main/java/com/vincennlin/leetcode/simulation/medium/findmissingobservations/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.simulation.medium.findmissingobservations;
+// 2028
+class Solution {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        return new int[]{};
+    }
+}
