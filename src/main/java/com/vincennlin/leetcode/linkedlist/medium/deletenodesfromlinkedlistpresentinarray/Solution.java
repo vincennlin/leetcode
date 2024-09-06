14ac879a3e72bf23cc13d5e71f3dac72c30e270e
vincennlin <vincentagwa@gmail.com>
2024-09-07 07:39:38 +0800

    LinkedList - Delete Nodes From Linked List Present in Array - my

--- src/main/java/com/vincennlin/leetcode/linkedlist/medium/deletenodesfromlinkedlistpresentinarray/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/deletenodesfromlinkedlistpresentinarray/Solution.java
@@ -1,7 +1,31 @@
 package com.vincennlin.leetcode.linkedlist.medium.deletenodesfromlinkedlistpresentinarray;
+
+import java.util.HashSet;
+import java.util.Set;
+
 // 3217
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        return null;
+
+        Set<Integer> numsSet = new HashSet<>();
+        for (int num : nums) {
+            numsSet.add(num);
+        }
+
+        ListNode dummyNode = new ListNode();
+        ListNode currentNode = head;
+        dummyNode.next = currentNode;
+        ListNode previousNode = dummyNode;
+
+        while (currentNode != null) {
+            if (numsSet.contains(currentNode.val)) {
+                previousNode.next = currentNode.next;
+            } else {
+                previousNode = previousNode.next;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return dummyNode.next;
     }
 }
