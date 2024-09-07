cbe80ea7db054d82ec798aa631c7f4829f2b04fa
vincennlin <vincentagwa@gmail.com>
2024-09-07 23:37:54 +0800

    Binary Tree - Linked List in Binary Tree - my - ans

--- src/main/java/com/vincennlin/leetcode/binarytree/general/medium/linkedlistinbinarytree/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytree/general/medium/linkedlistinbinarytree/Solution.java
@@ -2,19 +2,14 @@ package com.vincennlin.leetcode.binarytree.general.medium.linkedlistinbinarytree
 // 1367
 class Solution {
     public boolean isSubPath(ListNode head, TreeNode root) {
-        return traverse(head, head, root);
+        if (root == null) return false;
+        return traverse(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
     }
 
-    private boolean traverse(ListNode head, ListNode currentListNode, TreeNode currentTreeNode) {
-        if (currentTreeNode == null) return false;
-        if (currentListNode.val == currentTreeNode.val) {
-            if (currentListNode.next == null) {
-                return true;
-            } else {
-                return traverse(head, currentListNode.next, currentTreeNode.left) || traverse(head, currentListNode.next, currentTreeNode.right);
-            }
-        } else {
-            return traverse(head, head, currentTreeNode.left) || traverse(head, head, currentTreeNode.right);
-        }
+    private boolean traverse(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null) return false;
+        if (head.val != root.val) return false;
+        return traverse(head.next, root.left) || traverse(head.next, root.right);
     }
 }
