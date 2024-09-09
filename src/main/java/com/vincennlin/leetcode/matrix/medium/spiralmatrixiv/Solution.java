8bb9c944fa9d41af131063859163fb1d79f541ee
vincennlin <vincentagwa@gmail.com>
2024-09-09 23:12:17 +0800

    Matrix - Spiral Matrix IV - my & ans

--- src/main/java/com/vincennlin/leetcode/matrix/medium/spiralmatrixiv/Solution.java
+++ src/main/java/com/vincennlin/leetcode/matrix/medium/spiralmatrixiv/Solution.java
@@ -1,7 +1,50 @@
 package com.vincennlin.leetcode.matrix.medium.spiralmatrixiv;
+
+import java.util.Arrays;
+
 // 2326
 class Solution {
     public int[][] spiralMatrix(int m, int n, ListNode head) {
-        return new int[][]{};
+        int[][] result = new int[m][];
+        for (int i = 0; i < m; i++) {
+            int[] row = new int[n];
+            Arrays.fill(row, -1);
+            result[i] = row;
+        }
+
+        int top = 0, left = 0;
+        int bottom = m - 1, right = n - 1;
+
+        while (head != null) {
+            for (int j = left; j <= right; j++) {
+                result[top][j] = head.val;
+                head = head.next;
+                if (head == null) return result;
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                result[i][right] = head.val;
+                head = head.next;
+                if (head == null) return result;
+            }
+            right--;
+
+            for (int j = right; j >= left; j--) {
+                result[bottom][j] = head.val;
+                head = head.next;
+                if (head == null) return result;
+            }
+            bottom--;
+
+            for (int i = bottom; i >= top; i--) {
+                result[i][left] = head.val;
+                head = head.next;
+                if (head == null) return result;
+            }
+            left++;
+        }
+
+        return result;
     }
 }
