73c926ffdcab15d89a8d0a777dcb2099b71e60a4
vincennlin <vincentagwa@gmail.com>
2024-09-10 08:20:15 +0800

    Linked List - Insert Greatest Common Divisors in Linked List - my & ans

--- src/main/java/com/vincennlin/leetcode/linkedlist/medium/insertgreatestcommondivisorsinlinkedlist/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/insertgreatestcommondivisorsinlinkedlist/Solution.java
@@ -2,6 +2,23 @@ package com.vincennlin.leetcode.linkedlist.medium.insertgreatestcommondivisorsin
 // 2807
 class Solution {
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        return null;
+        ListNode currentNode = head;
+
+        while (currentNode.next != null) {
+            ListNode gcdNode = new ListNode(gcd(currentNode.val, currentNode.next.val));
+            gcdNode.next = currentNode.next;
+            currentNode.next = gcdNode;
+            currentNode = gcdNode.next;
+        }
+
+        return head;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
     }
 }
