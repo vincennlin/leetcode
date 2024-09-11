5f7393d3e22eba02c3e7281933d916724cd14435
vincennlin <vincentagwa@gmail.com>
2024-09-11 19:09:48 +0800

    Bit Manipulation - Minimum Bit Flips to Convert Number - my

--- src/main/java/com/vincennlin/leetcode/bit/easy/minimumbitflipstoconvertnumber/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/easy/minimumbitflipstoconvertnumber/Solution.java
@@ -2,6 +2,14 @@ package com.vincennlin.leetcode.bit.easy.minimumbitflipstoconvertnumber;
 // 2220
 class Solution {
     public int minBitFlips(int start, int goal) {
-        return 0;
+        int count = 0;
+        while (start > 0 || goal > 0) {
+            if ((goal & 1) != (start & 1)) {
+                count++;
+            }
+            start >>>= 1;
+            goal >>>= 1;
+        }
+        return count;
     }
 }
