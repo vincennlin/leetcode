51f977c13e9431928195c24b90583154f7ccfc1c
vincennlin <vincentagwa@gmail.com>
2024-09-12 08:34:12 +0800

    Dynamic Programming - Domino and Tromino Tiling

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/medium/dominoandtrominotiling/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dynamicprogramming.onedimensional.medium.dominoandtrominotiling;
+// 790
+class Solution {
+    public int numTilings(int n) {
+        return 0;
+    }
+}
