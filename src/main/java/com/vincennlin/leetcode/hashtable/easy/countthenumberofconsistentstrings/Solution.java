e24257eacecb89e4695825983f186bb663d53d15
vincennlin <vincentagwa@gmail.com>
2024-09-12 08:29:56 +0800

    Hash Table - Count the NUmber of Consistent Strings - my

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/countthenumberofconsistentstrings/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/countthenumberofconsistentstrings/Solution.java
@@ -2,6 +2,22 @@ package com.vincennlin.leetcode.hashtable.easy.countthenumberofconsistentstrings
 // 1684
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
-        return 0;
+        boolean[] ht = new boolean[26];
+        for (char c : allowed.toCharArray()) {
+            ht[c - 'a'] = true;
+        }
+
+        int count = 0;
+        for(String word : words) {
+            count++;
+            for (char c : word.toCharArray()) {
+                if (!ht[c - 'a']) {
+                    count--;
+                    break;
+                }
+            }
+        }
+
+        return count;
     }
 }
