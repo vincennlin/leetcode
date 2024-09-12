89a79c245d9ba44aea61354923b5373dc64d540a
vincennlin <vincentagwa@gmail.com>
2024-09-12 11:32:54 +0800

    Multidimensional DP - Best Time to Buy and Sell Stock With Transaction Fee - my

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/besttimetobuyandsellstockwithtransactionfee/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/besttimetobuyandsellstockwithtransactionfee/Solution.java
@@ -3,15 +3,18 @@ package com.vincennlin.leetcode.dynamicprogramming.multidimensional.medium.bestt
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         int m = prices.length;
-        int[][] dp = new int[m + 1][2];
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
+        int sold = 0;
+        int owned = -prices[0];
+        int currentSell = 0;
+        int currentOwn = 0;
 
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1] - fee);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i - 1]);
+        for (int i = 0; i < m; i++) {
+            currentSell = Math.max(sold, owned + prices[i] - fee);
+            currentOwn = Math.max(owned, sold - prices[i]);
+            sold = currentSell;
+            owned = currentOwn;
         }
 
-        return Math.max(dp[m][0], dp[m][1]);
+        return Math.max(currentSell, currentOwn);
     }
 }
