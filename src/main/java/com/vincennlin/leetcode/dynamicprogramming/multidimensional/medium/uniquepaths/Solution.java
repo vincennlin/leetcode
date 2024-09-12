38130de71437cb09e314cc27bc99b2453cd5a0cc
vincennlin <vincentagwa@gmail.com>
2024-09-12 09:39:10 +0800

    Multidimensional DP - Unique Paths - my & ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/uniquepaths/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/uniquepaths/Solution.java
@@ -2,6 +2,21 @@ package com.vincennlin.leetcode.dynamicprogramming.multidimensional.medium.uniqu
 // 62
 class Solution {
     public int uniquePaths(int m, int n) {
-        return 0;
+        int[][] dp = new int[m][];
+        for(int i = 0; i < m; i++) {
+            dp[i] = new int[n];
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 }
