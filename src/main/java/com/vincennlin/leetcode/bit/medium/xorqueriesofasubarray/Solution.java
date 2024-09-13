3a92dd33c1b10d667c8dd759f3a9cb732172f6e0
vincennlin <vincentagwa@gmail.com>
2024-09-13 10:38:53 +0800

    Bit Manipulation - XOR Queries of a Subarray

---
+++ src/main/java/com/vincennlin/leetcode/bit/medium/xorqueriesofasubarray/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.bit.medium.xorqueriesofasubarray;
+// 1310
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        return new int[]{};
+    }
+}
--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/besttimetobuyandsellstockwithtransactionfee/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/besttimetobuyandsellstockwithtransactionfee/Solution.java
@@ -13,3 +13,4 @@ class Solution {
         return sell;
     }
 }
+
