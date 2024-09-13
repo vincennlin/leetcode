7827d5b32cc05cb85a8e0ce102a333aacf21d00a
vincennlin <vincentagwa@gmail.com>
2024-09-13 10:47:45 +0800

    Bit Manipulation - XOR Queries of a Subarray - my

--- src/main/java/com/vincennlin/leetcode/bit/medium/xorqueriesofasubarray/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/xorqueriesofasubarray/Solution.java
@@ -2,6 +2,17 @@ package com.vincennlin.leetcode.bit.medium.xorqueriesofasubarray;
 // 1310
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        return new int[]{};
+        int m = queries.length;
+        int[] result = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            int xor = 0;
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                xor ^= arr[j];
+            }
+            result[i] = xor;
+        }
+
+        return result;
     }
 }
