779931f0f039c905c3de5d10c7c996ea6ce930dc
vincennlin <vincentagwa@gmail.com>
2024-09-13 11:03:40 +0800

    Bit Manipulation - XOR Queries of a Subarray - ans

--- src/main/java/com/vincennlin/leetcode/bit/medium/xorqueriesofasubarray/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/xorqueriesofasubarray/Solution.java
@@ -2,15 +2,23 @@ package com.vincennlin.leetcode.bit.medium.xorqueriesofasubarray;
 // 1310
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int m = queries.length;
-        int[] result = new int[m];
+        int m = arr.length;
+        int n = queries.length;
+        int[] xorArray = new int[m];
+        xorArray[0] = arr[0];
 
-        for (int i = 0; i < m; i++) {
-            int xor = 0;
-            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
-                xor ^= arr[j];
+        for(int i = 1; i < m; i++) {
+            xorArray[i] = xorArray[i - 1] ^ arr[i];
+        }
+
+
+        int[] result = new int[n];
+        for (int j = 0; j < n; j++) {
+            if (queries[j][0] == 0) {
+                result[j] = xorArray[queries[j][1]];
+            } else {
+                result[j] = xorArray[queries[j][1]] ^ xorArray[queries[j][0] - 1];
             }
-            result[i] = xor;
         }
 
         return result;
