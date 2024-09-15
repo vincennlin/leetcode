640e3140fa750d432974c24e0140bd145f80aa8e
vincennlin <vincentagwa@gmail.com>
2024-09-15 08:40:37 +0800

    Bit Manipulation - Find the Longest Substring Containing Vowels in Even Counts

---
+++ src/main/java/com/vincennlin/leetcode/bit/medium/findthelongestsubstringcontainingvowelsinevencounts/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.bit.medium.findthelongestsubstringcontainingvowelsinevencounts;
+// 1371
+class Solution {
+    public int findTheLongestSubstring(String s) {
+        return 0;
+    }
+}
