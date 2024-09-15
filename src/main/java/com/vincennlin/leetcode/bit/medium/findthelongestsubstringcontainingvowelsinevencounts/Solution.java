dae7e2871f757aed3bdecae296c035e046bea35a
vincennlin <vincentagwa@gmail.com>
2024-09-15 09:18:05 +0800

    Bit Manipulation - Find the Longest Substring Containing Vowels in Even Counts - ans - good

--- src/main/java/com/vincennlin/leetcode/bit/medium/findthelongestsubstringcontainingvowelsinevencounts/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/findthelongestsubstringcontainingvowelsinevencounts/Solution.java
@@ -1,33 +1,42 @@
 package com.vincennlin.leetcode.bit.medium.findthelongestsubstringcontainingvowelsinevencounts;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 // 1371
 class Solution {
     public int findTheLongestSubstring(String s) {
-        Map<Character, Integer> vowelOrderMap = new HashMap<>();
-        vowelOrderMap.put('a', 0);
-        vowelOrderMap.put('e', 1);
-        vowelOrderMap.put('i', 2);
-        vowelOrderMap.put('o', 3);
-        vowelOrderMap.put('u', 4);
-
-        Map<Integer, Integer> bitIndexMap = new HashMap<>();
-        bitIndexMap.put(0, -1);
+        int[] bitArray = new int[32];
+        Arrays.fill(bitArray, -2);
+        bitArray[0] = -1;
 
         int bit = 0;
         int maxLength = 0;
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (vowelOrderMap.containsKey(c)) {
-                bit ^= (1 << vowelOrderMap.get(c));
+
+            switch (c) {
+                case 'a':
+                    bit ^= (1);
+                    break;
+                case 'e':
+                    bit ^= (1 << 1);
+                    break;
+                case 'i':
+                    bit ^= (1 << 2);
+                    break;
+                case 'o':
+                    bit ^= (1 << 3);
+                    break;
+                case 'u':
+                    bit ^= (1 << 4);
+                    break;
             }
-            if (bitIndexMap.containsKey(bit)) {
-                maxLength = Math.max(maxLength, i - bitIndexMap.get(bit));
+
+            if (bitArray[bit] != -2) {
+                maxLength = Math.max(maxLength, i - bitArray[bit]);
             } else {
-                bitIndexMap.put(bit, i);
+                bitArray[bit] = i;
             }
         }
 
