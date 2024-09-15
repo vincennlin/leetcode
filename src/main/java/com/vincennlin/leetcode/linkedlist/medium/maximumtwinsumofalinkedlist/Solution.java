61350fc885fd6196b1d5269f0e72127a4267b08f
vincennlin <vincentagwa@gmail.com>
2024-09-15 11:33:19 +0800

    Linked List - Maximum Twin Sum of a Linked List - ans

--- src/main/java/com/vincennlin/leetcode/linkedlist/medium/maximumtwinsumofalinkedlist/Solution.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/maximumtwinsumofalinkedlist/Solution.java
@@ -1,26 +1,32 @@
 package com.vincennlin.leetcode.linkedlist.medium.maximumtwinsumofalinkedlist;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 // 2130
 class Solution {
     public int pairSum(ListNode head) {
-        Deque<Integer> valStack = new ArrayDeque<>();
         ListNode slowNode = head;
         ListNode fastNode = head;
 
         while (fastNode != null) {
-            valStack.push(slowNode.val);
             slowNode = slowNode.next;
             fastNode = fastNode.next.next;
         }
 
-        int max = Integer.MIN_VALUE;
-
+        ListNode previousNode = null;
         while (slowNode != null) {
-            max = Math.max(max, valStack.pop() + slowNode.val);
-            slowNode = slowNode.next;
+            ListNode nextNode = slowNode.next;
+            slowNode.next = previousNode;
+            previousNode = slowNode;
+            slowNode = nextNode;
+        }
+
+        int max = Integer.MIN_VALUE;
+        ListNode startNode = head;
+        ListNode endNode = previousNode;
+
+        while (endNode != null) {
+            max = Math.max(max, startNode.val + endNode.val);
+            startNode = startNode.next;
+            endNode = endNode.next;
         }
 
         return max;
