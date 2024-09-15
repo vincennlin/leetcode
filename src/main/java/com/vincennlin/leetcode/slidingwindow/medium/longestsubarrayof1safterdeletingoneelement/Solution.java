ea0d7f87181df7e6085a4da2d74f3d24df193033
vincennlin <vincentagwa@gmail.com>
2024-09-15 09:21:20 +0800

    Sliding Window - Longest Subarray of 1's After Deleting One Element

---
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/longestsubarrayof1safterdeletingoneelement/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.slidingwindow.medium.longestsubarrayof1safterdeletingoneelement;
+// 1493
+class Solution {
+    public int longestSubarray(int[] nums) {
+        return 0;
+    }
+}
