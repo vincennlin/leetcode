f05f33e1918aaa2ca84fe3387927a77a1ea615f2
vincennlin <vincentagwa@gmail.com>
2024-09-15 09:09:16 +0800

    Bit Manipulation - Find the Longest Substring Containing Vowels in Even Counts - ans

--- src/main/java/com/vincennlin/leetcode/bit/medium/findthelongestsubstringcontainingvowelsinevencounts/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/findthelongestsubstringcontainingvowelsinevencounts/Solution.java
@@ -1,7 +1,36 @@
 package com.vincennlin.leetcode.bit.medium.findthelongestsubstringcontainingvowelsinevencounts;
+
+import java.util.HashMap;
+import java.util.Map;
+
 // 1371
 class Solution {
     public int findTheLongestSubstring(String s) {
-        return 0;
+        Map<Character, Integer> vowelOrderMap = new HashMap<>();
+        vowelOrderMap.put('a', 0);
+        vowelOrderMap.put('e', 1);
+        vowelOrderMap.put('i', 2);
+        vowelOrderMap.put('o', 3);
+        vowelOrderMap.put('u', 4);
+
+        Map<Integer, Integer> bitIndexMap = new HashMap<>();
+        bitIndexMap.put(0, -1);
+
+        int bit = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (vowelOrderMap.containsKey(c)) {
+                bit ^= (1 << vowelOrderMap.get(c));
+            }
+            if (bitIndexMap.containsKey(bit)) {
+                maxLength = Math.max(maxLength, i - bitIndexMap.get(bit));
+            } else {
+                bitIndexMap.put(bit, i);
+            }
+        }
+
+        return maxLength;
     }
 }
