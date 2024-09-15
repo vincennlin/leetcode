0b6ff8522544e3b829d5263def9f6d55047a9807
vincennlin <vincentagwa@gmail.com>
2024-09-15 20:38:38 +0800

    Binary Tree - Longest ZigZag Path in a Binary Tree - ans

--- src/main/java/com/vincennlin/leetcode/binarytree/dfs/longestzigzagpathinabinarytree/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytree/dfs/longestzigzagpathinabinarytree/Solution.java
@@ -1,20 +1,25 @@
 package com.vincennlin.leetcode.binarytree.dfs.longestzigzagpathinabinarytree;
 // 1372
 class Solution {
+    private int maxLength;
+
     public int longestZigZag(TreeNode root) {
-        return Math.max(maxZigZag(root.left,0,  false), maxZigZag(root.right, 0, true));
+        maxLength = 0;
+        dfs(root, 0, true);
+        dfs(root, 0, false);
+        return maxLength;
     }
 
-    private int maxZigZag(TreeNode currentNode, int length, boolean toLeft) {
-        if (currentNode == null) return length;
+    private void dfs(TreeNode currentNode, int length, boolean toLeft) {
+        if (currentNode == null) return;
+
+        maxLength = Math.max(maxLength, length);
         if (toLeft) {
-            return Math.max(
-                    maxZigZag(currentNode.left, length + 1, false),
-                    maxZigZag(currentNode.right, 0, true));
+            dfs(currentNode.left, length + 1, false);
+            dfs(currentNode.right, 1, true);
         } else {
-            return Math.max(
-                    maxZigZag(currentNode.right, length + 1, true),
-                    maxZigZag(currentNode.left, 0, false));
+            dfs(currentNode.right, length + 1, true);
+            dfs(currentNode.left, 1, false);
         }
     }
 }
