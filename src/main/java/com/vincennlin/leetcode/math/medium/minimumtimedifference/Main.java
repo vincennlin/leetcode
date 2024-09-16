4754b4f8d5910ba59a8a93230e6b633c1dd5496f
vincennlin <vincentagwa@gmail.com>
2024-09-16 08:06:46 +0800

    Math - Minimum Time Difference

---
+++ src/main/java/com/vincennlin/leetcode/math/medium/minimumtimedifference/Main.java
@@ -0,0 +1,32 @@
+package com.vincennlin.leetcode.math.medium.minimumtimedifference;
+
+import java.util.List;
+
+/*
+Given a list of 24-hour clock time points in "HH:MM" format, return the minimum minutes difference between any two time-points in the list.
+
+
+Example 1:
+
+Input: timePoints = ["23:59","00:00"]
+Output: 1
+Example 2:
+
+Input: timePoints = ["00:00","23:59","00:00"]
+Output: 0
+
+
+Constraints:
+
+2 <= timePoints.length <= 2 * 104
+timePoints[i] is in the format "HH:MM".
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.findMinDifference(List.of("23:59", "00:00"))); // 1
+
+        System.out.println(solution.findMinDifference(List.of("00:00", "23:59", "00:00"))); // 0
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/math/medium/minimumtimedifference/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.math.medium.minimumtimedifference;
+
+import java.util.List;
+
+// 539
+class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        return 0;
+    }
+}
