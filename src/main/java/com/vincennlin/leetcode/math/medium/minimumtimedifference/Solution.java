7848b6e9e19b55e3f7ebe1e8aa3c69a16729c89d
vincennlin <vincentagwa@gmail.com>
2024-09-16 08:28:01 +0800

    Math - Minimum Time Difference - my

--- src/main/java/com/vincennlin/leetcode/math/medium/minimumtimedifference/Solution.java
+++ src/main/java/com/vincennlin/leetcode/math/medium/minimumtimedifference/Solution.java
@@ -1,10 +1,32 @@
 package com.vincennlin.leetcode.math.medium.minimumtimedifference;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 // 539
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        return 0;
+
+        PriorityQueue<Integer> minutes = new PriorityQueue<>();
+
+        for (String timePoint : timePoints) {
+            minutes.add(toMinute(timePoint));
+        }
+
+        int firstMinute = minutes.peek();
+        int minDiff = Integer.MAX_VALUE;
+
+        while (minutes.size() > 1) {
+            minDiff = Math.min(minDiff, -(minutes.poll() - minutes.peek()));
+        }
+
+        minDiff = Math.min(minDiff, firstMinute + 1440 - (minutes.peek()));
+
+        return minDiff;
+    }
+
+    private int toMinute(String timePoint) {
+        return 60 * Integer.parseInt(timePoint.substring(0, 2)) + Integer.parseInt(timePoint.substring(3, 5));
     }
 }
