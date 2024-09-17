1a26a5570f431e3fb8eb937401a2855eb1c06a66
vincennlin <vincentagwa@gmail.com>
2024-09-18 00:09:44 +0800

    Hash Table - Uncommon Words from Two Sentences - my

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/uncommonwordsfromtwosentences/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/uncommonwordsfromtwosentences/Solution.java
@@ -1,7 +1,27 @@
 package com.vincennlin.leetcode.hashtable.easy.uncommonwordsfromtwosentences;
+
+import java.util.*;
+
 // 884
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        return null;
+        Map<String, Integer> stringCountMap = new HashMap<>();
+        for (String s : s1.split(" ")) {
+            stringCountMap.put(s, stringCountMap.getOrDefault(s, 0) + 1);
+        }
+        for (String s : s2.split(" ")) {
+            stringCountMap.put(s, stringCountMap.getOrDefault(s, 0) + 1);
+        }
+        List<String> resultList = new ArrayList<>();
+        for (String str : stringCountMap.keySet()) {
+            if (stringCountMap.get(str) == 1) {
+                resultList.add(str);
+            }
+        }
+        String[] resultArray = new String[resultList.size()];
+        for (int i = 0; i < resultArray.length; i++) {
+            resultArray[i] = resultList.get(i);
+        }
+        return resultArray;
     }
 }
