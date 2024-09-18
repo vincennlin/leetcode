f48a88010852ac20e1deb3776a18031e04f27c0a
vincennlin <vincentagwa@gmail.com>
2024-09-18 17:06:30 +0800

    Greedy - Largest Number - ans

--- src/main/java/com/vincennlin/leetcode/greedy/medium/largestnumber/Solution.java
+++ src/main/java/com/vincennlin/leetcode/greedy/medium/largestnumber/Solution.java
@@ -1,7 +1,27 @@
 package com.vincennlin.leetcode.greedy.medium.largestnumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 179
 class Solution {
     public String largestNumber(int[] nums) {
-        return "";
+        List<String> numberStringList = new ArrayList<>();
+        for (int num : nums) {
+            numberStringList.add(String.valueOf(num));
+        }
+
+        numberStringList.sort((o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        StringBuilder result = new StringBuilder();
+        for (String numberString : numberStringList) {
+            result.append(numberString);
+        }
+
+        if (result.toString().startsWith("0")) {
+            return "0";
+        }
+
+        return result.toString();
     }
 }
