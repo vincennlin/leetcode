7565afae1b42f0ec35deff6a45a0196faf0b9511
vincennlin <vincentagwa@gmail.com>
2024-09-20 08:36:01 +0800

    String Matching - Shortest Palindrome

---
+++ src/main/java/com/vincennlin/leetcode/stringmatching/hard/shortestpalindrome/Main.java
@@ -0,0 +1,34 @@
+package com.vincennlin.leetcode.stringmatching.hard.shortestpalindrome;
+/*
+You are given a string s. You can convert s to a
+palindrome
+ by adding characters in front of it.
+
+Return the shortest palindrome you can find by performing this transformation.
+
+
+
+Example 1:
+
+Input: s = "aacecaaa"
+Output: "aaacecaaa"
+Example 2:
+
+Input: s = "abcd"
+Output: "dcbabcd"
+
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of lowercase English letters only.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.shortestPalindrome("aacecaaa")); // "aaacecaaa"
+
+        System.out.println(solution.shortestPalindrome("abcd")); // "dcbabcd"
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/stringmatching/hard/shortestpalindrome/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.stringmatching.hard.shortestpalindrome;
+// 214
+class Solution {
+    public String shortestPalindrome(String s) {
+        return "";
+    }
+}
