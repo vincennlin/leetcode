512139fb3eb55dac8572bee1acaa87807ebd20c1
vincennlin <vincentagwa@gmail.com>
2024-09-21 12:36:38 +0800

    DFS - Lexicographical Numbers - ans

--- src/main/java/com/vincennlin/leetcode/dfs/medium/lexicographicalnumbers/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dfs/medium/lexicographicalnumbers/Solution.java
@@ -1,10 +1,29 @@
 package com.vincennlin.leetcode.dfs.medium.lexicographicalnumbers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // 386
 class Solution {
+    private List<Integer> result;
+    private int n;
+
     public List<Integer> lexicalOrder(int n) {
-        return null;
+        this.n = n;
+        result = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            dfs(i);
+        }
+        return result;
+    }
+
+    private void dfs(int num) {
+        if (num > n) return;
+        result.add(num);
+        for (int i = 0; i <= 9; i++) {
+            int nextNum = num * 10 + i;
+            if (nextNum > n) break;
+            dfs(nextNum);
+        }
     }
 }
