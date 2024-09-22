e5b22150f26a7934f2ea95ed4a407c4533f083f0
vincennlin <vincentagwa@gmail.com>
2024-09-22 21:17:00 +0800

    DFS - K-th Smallest in Lexicographical Order - ans (Not understood)

--- src/main/java/com/vincennlin/leetcode/dfs/hard/kthsmallestinlexicographicalorder/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dfs/hard/kthsmallestinlexicographicalorder/Solution.java
@@ -2,6 +2,30 @@ package com.vincennlin.leetcode.dfs.hard.kthsmallestinlexicographicalorder;
 // 440
 class Solution {
     public int findKthNumber(int n, int k) {
-        return 0;
+        int currentNum = 1;
+        k--;
+
+        while (k > 0) {
+            int count = getCount(n, currentNum, currentNum + 1);
+            if (count <= k) {
+                currentNum++;
+                k -= count;
+            } else {
+                currentNum *= 10;
+                k--;
+            }
+        }
+
+        return currentNum;
+    }
+
+    private int getCount(int n, long prefix, long nextPrefix) {
+        int count = 0;
+        while (prefix <= n) {
+            count += (int) (Math.min(n + 1, nextPrefix) - prefix);
+            prefix *= 10;
+            nextPrefix *= 10;
+        }
+        return count;
     }
 }
