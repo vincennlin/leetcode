b5b1f4def9990c613a014e8afa6fe3b61f711b61
vincennlin <vincentagwa@gmail.com>
2024-09-23 09:14:15 +0800

    One Dimensional DP - Extra Characters in a String

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/medium/extracharactersinastring/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dynamicprogramming.onedimensional.medium.extracharactersinastring;
+// 2707
+class Solution {
+    public int minExtraChar(String s, String[] dictionary) {
+        return 0;
+    }
+}
