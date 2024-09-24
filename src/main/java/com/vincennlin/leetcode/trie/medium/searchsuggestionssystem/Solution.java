e267dec9bf2104985f094c81ac0ca7fa43dc0445
vincennlin <vincentagwa@gmail.com>
2024-09-24 18:14:41 +0800

    Trie - Search Suggestions System

---
+++ src/main/java/com/vincennlin/leetcode/trie/medium/searchsuggestionssystem/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.trie.medium.searchsuggestionssystem;
+
+import java.util.List;
+
+// 1268
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        return null;
+    }
+}
