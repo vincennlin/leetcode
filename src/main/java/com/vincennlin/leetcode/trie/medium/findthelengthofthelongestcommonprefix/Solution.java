603ff0683e9f12f21f119ae186e684b0ec75c139
vincennlin <vincentagwa@gmail.com>
2024-09-24 09:50:35 +0800

    Trie - Find the Length of the Longest Common Prefix

---
+++ src/main/java/com/vincennlin/leetcode/trie/medium/findthelengthofthelongestcommonprefix/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.trie.medium.findthelengthofthelongestcommonprefix;
+// 3043
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        return 0;
+    }
+}
