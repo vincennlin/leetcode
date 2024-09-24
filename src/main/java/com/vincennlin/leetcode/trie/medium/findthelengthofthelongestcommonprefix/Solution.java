8c6abb6bce4116813e8a071cec9e332cbd1eb0db
vincennlin <vincentagwa@gmail.com>
2024-09-24 10:03:37 +0800

    Trie - Find the Length of the Longest Common Prefix - my & ans

--- src/main/java/com/vincennlin/leetcode/trie/medium/findthelengthofthelongestcommonprefix/Solution.java
+++ src/main/java/com/vincennlin/leetcode/trie/medium/findthelengthofthelongestcommonprefix/Solution.java
@@ -2,6 +2,51 @@ package com.vincennlin.leetcode.trie.medium.findthelengthofthelongestcommonprefi
 // 3043
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        return 0;
+        Node root = new Node();
+        for (int num1 : arr1) {
+            insert(root, num1);
+        }
+
+        int longest = 0;
+        for (int num2 : arr2) {
+            longest = Math.max(longest, searchDepth(root, num2));
+        }
+
+        return longest;
+    }
+
+    private void insert(Node root, int num) {
+        String str = String.valueOf(num);
+        for (char c : str.toCharArray()) {
+            int path = c - '0';
+            if (root.children[path] == null) {
+                root.children[path] = new Node();
+            }
+            root = root.children[path];
+        }
+        root.isEnd = true;
+    }
+
+    private int searchDepth(Node root, int num) {
+        int depth = 0;
+        String str = String.valueOf(num);
+        for (char c : str.toCharArray()) {
+            int path = c - '0';
+            if (root.children[path] == null) {
+                return depth;
+            }
+            root = root.children[path];
+            depth++;
+        }
+        return depth;
+    }
+}
+
+class Node{
+    public Node[] children;
+    public boolean isEnd;
+
+    Node() {
+        children = new Node[10];
     }
 }
