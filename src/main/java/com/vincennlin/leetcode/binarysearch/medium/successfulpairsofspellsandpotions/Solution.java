d080f0feaf0147db3860c23f0c633987f5fb000d
vincennlin <vincentagwa@gmail.com>
2024-09-24 20:50:49 +0800

    Binary Search - Successful Pairs of Spells and Potions

---
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/successfulpairsofspellsandpotions/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.binarysearch.medium.successfulpairsofspellsandpotions;
+// 2300
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        return new int[]{};
+    }
+}
