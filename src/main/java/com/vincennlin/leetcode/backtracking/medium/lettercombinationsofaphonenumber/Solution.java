9d073e390f3e5a9448d93ee215bb5ec4d6a10228
vincennlin <vincentagwa@gmail.com>
2024-09-25 20:50:53 +0800

    Backtracking - Letter Combinations of a Phone Number - ans

--- src/main/java/com/vincennlin/leetcode/backtracking/medium/lettercombinationsofaphonenumber/Solution.java
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/lettercombinationsofaphonenumber/Solution.java
@@ -1,42 +1,31 @@
 package com.vincennlin.leetcode.backtracking.medium.lettercombinationsofaphonenumber;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 // 17
 class Solution {
-
-    Map<Integer, List<Character>> numberCharsMap;
-
-    public Solution() {
-        numberCharsMap = new HashMap<>();
-        numberCharsMap.put(2, List.of('a', 'b', 'c'));
-        numberCharsMap.put(3, List.of('d', 'e', 'f'));
-        numberCharsMap.put(4, List.of('g', 'h', 'i'));
-        numberCharsMap.put(5, List.of('j', 'k', 'l'));
-        numberCharsMap.put(6, List.of('m', 'n', 'o'));
-        numberCharsMap.put(7, List.of('p', 'q', 'r', 's'));
-        numberCharsMap.put(8, List.of('t', 'u', 'v'));
-        numberCharsMap.put(9, List.of('w', 'x', 'y', 'z'));
-    }
+    private String[] mappings;
 
     public List<String> letterCombinations(String digits) {
+        mappings = new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
         List<String> result = new ArrayList<>();
         if (!digits.isEmpty()) {
-            backtracking(result, 0, "", digits);
+            backtracking(result, 0, new StringBuilder(), digits);
         }
         return result;
     }
 
-    private void backtracking(List<String> result, int index, String currentString, String digits) {
+    private void backtracking(List<String> result, int index, StringBuilder sb, String digits) {
         if (index == digits.length()) {
-            result.add(currentString);
+            result.add(sb.toString());
             return;
         }
-        for (char c : numberCharsMap.get(digits.charAt(index) - '0')) {
-            backtracking(result, index + 1, currentString + c, digits);
+        for (char c : mappings[digits.charAt(index) - '0'].toCharArray()) {
+            sb.append(c);
+            backtracking(result, index + 1, sb, digits);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
