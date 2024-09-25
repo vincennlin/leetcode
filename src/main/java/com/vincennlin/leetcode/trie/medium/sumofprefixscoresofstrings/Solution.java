e13e53c15e443b80a6e70c13614f24104ac41121
vincennlin <vincentagwa@gmail.com>
2024-09-25 08:05:29 +0800

    Trie - Sum of Prefix Scores of Strings

---
+++ src/main/java/com/vincennlin/leetcode/trie/medium/sumofprefixscoresofstrings/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.trie.medium.sumofprefixscoresofstrings;
+// 2416
+class Solution {
+    public int[] sumPrefixScores(String[] words) {
+        return new int[]{};
+    }
+}
