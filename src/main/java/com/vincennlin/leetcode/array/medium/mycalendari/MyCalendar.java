ec4b73911451b7e238217f14441b165d2bcfacc3
vincennlin <vincentagwa@gmail.com>
2024-09-26 10:07:50 +0800

    Array - My Calendar I - ans - bad

--- src/main/java/com/vincennlin/leetcode/array/medium/mycalendari/MyCalendar.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/mycalendari/MyCalendar.java
@@ -1,12 +1,28 @@
 package com.vincennlin.leetcode.array.medium.mycalendari;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 729
 class MyCalendar {
 
-    public MyCalendar() {
+    List<int[]> calendar;
 
+    public MyCalendar() {
+        calendar = new ArrayList<>();
     }
 
     public boolean book(int start, int end) {
-        return false;
+        if (calendar.isEmpty()) {
+            calendar.add(new int[]{start, end});
+        } else {
+            for (int[] event : calendar) {
+                if (start < event[1] && end > event[0]) {
+                    return false;
+                }
+            }
+            calendar.add(new int[]{start, end});
+        }
+        return true;
     }
 }
