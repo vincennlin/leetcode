0aec373eb6777de03a9c480a9e6665fb414c7818
vincennlin <vincentagwa@gmail.com>
2024-09-26 09:41:53 +0800

    Array - My Calendar I

---
+++ src/main/java/com/vincennlin/leetcode/array/medium/mycalendari/MyCalendar.java
@@ -0,0 +1,12 @@
+package com.vincennlin.leetcode.array.medium.mycalendari;
+// 729
+class MyCalendar {
+
+    public MyCalendar() {
+
+    }
+
+    public boolean book(int start, int end) {
+        return false;
+    }
+}
