8c309f8dfb1e788cb608c5d07d9a428bc8a80ab1
vincennlin <vincentagwa@gmail.com>
2024-09-26 10:18:47 +0800

    Array - My Calendar I - ans

--- src/main/java/com/vincennlin/leetcode/array/medium/mycalendari/MyCalendar.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/mycalendari/MyCalendar.java
@@ -1,28 +1,27 @@
 package com.vincennlin.leetcode.array.medium.mycalendari;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 // 729
 class MyCalendar {
 
-    List<int[]> calendar;
+    TreeMap<Integer, Integer> calendar;
 
     public MyCalendar() {
-        calendar = new ArrayList<>();
+        calendar = new TreeMap<>();
     }
 
     public boolean book(int start, int end) {
-        if (calendar.isEmpty()) {
-            calendar.add(new int[]{start, end});
+        Map.Entry<Integer, Integer> prev = calendar.floorEntry(start);
+        Map.Entry<Integer, Integer> next = calendar.ceilingEntry(start);
+
+        if ((prev == null || prev.getValue() <= start) &&
+                ((next == null || next.getKey() >= end))) {
+            calendar.put(start, end);
+            return true;
         } else {
-            for (int[] event : calendar) {
-                if (start < event[1] && end > event[0]) {
-                    return false;
-                }
-            }
-            calendar.add(new int[]{start, end});
+            return false;
         }
-        return true;
     }
 }
