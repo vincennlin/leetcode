282c16a9befc6fab0dacfeb2d4a1eb15f23a0429
vincennlin <vincentagwa@gmail.com>
2024-09-27 17:03:01 +0800

    Array - My Calendar II - ans

--- src/main/java/com/vincennlin/leetcode/array/medium/mycalendarii/MyCalendarTwo.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/mycalendarii/MyCalendarTwo.java
@@ -1,12 +1,40 @@
 package com.vincennlin.leetcode.array.medium.mycalendarii;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 731
 class MyCalendarTwo {
 
-    public MyCalendarTwo() {
+    private List<int[]> calendar;
+    private List<int[]> overlap;
 
+    public MyCalendarTwo() {
+        this.calendar = new ArrayList<>();
+        this.overlap = new ArrayList<>();
     }
 
     public boolean book(int start, int end) {
-        return false;
+        for (int[] overlapEvent : overlap) {
+            int overlapStart = overlapEvent[0];
+            int overlapEnd = overlapEvent[1];
+            if (start < overlapEnd && end > overlapStart) {
+                return false;
+            }
+        }
+
+        for (int[] event : calendar) {
+            int calStart = event[0];
+            int calEnd = event[1];
+            if (start < calEnd && end > calStart) {
+                int newOverLapStart = Math.max(start, calStart);
+                int newOverLapEnd = Math.min(end, calEnd);
+                overlap.add(new int[]{newOverLapStart, newOverLapEnd});
+            }
+        }
+
+        calendar.add(new int[]{start, end});
+
+        return true;
     }
 }
