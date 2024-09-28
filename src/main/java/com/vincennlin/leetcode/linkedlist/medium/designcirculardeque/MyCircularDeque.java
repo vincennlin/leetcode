6454823dc5eb50e6c7dfa54995d21c52654bce36
vincennlin <vincentagwa@gmail.com>
2024-09-28 10:00:21 +0800

    Linked List - Design Circular Deque - my & ans

--- src/main/java/com/vincennlin/leetcode/linkedlist/medium/designcirculardeque/MyCircularDeque.java
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/designcirculardeque/MyCircularDeque.java
@@ -2,39 +2,96 @@ package com.vincennlin.leetcode.linkedlist.medium.designcirculardeque;
 // 641
 class MyCircularDeque {
 
-    public MyCircularDeque(int k) {
+    private int size;
+    private final int limit;
+    private Node head;
+    private Node tail;
 
+    public MyCircularDeque(int k) {
+        this.size = 0;
+        this.limit = k;
     }
 
     public boolean insertFront(int value) {
-        return false;
+        if (isFull()) return false;
+        Node node = new Node(value);
+        if (isEmpty()) {
+            tail = node;
+        } else {
+            node.next = head;
+            head.prev = node;
+        }
+        head = node;
+        size++;
+        return true;
     }
 
     public boolean insertLast(int value) {
-        return false;
+        if (isFull()) return false;
+        Node node = new Node(value);
+        if (isEmpty()) {
+            head = node;
+        } else {
+            node.prev = tail;
+            tail.next = node;
+        }
+        tail = node;
+        size++;
+        return true;
     }
 
     public boolean deleteFront() {
-        return false;
+        if (isEmpty()) return false;
+        if (size == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+        size--;
+        return true;
     }
 
     public boolean deleteLast() {
-        return false;
+        if (isEmpty()) return false;
+        if (size == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+        size--;
+        return true;
     }
 
     public int getFront() {
-        return 0;
+        if (isEmpty()) return -1;
+        return head.value;
     }
 
     public int getRear() {
-        return 0;
+        if (isEmpty()) return -1;
+        return tail.value;
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public boolean isFull() {
-        return false;
+        return size == limit;
+    }
+}
+
+class Node {
+
+    int value;
+    Node prev;
+    Node next;
+
+    Node (int value) {
+        this.value = value;
     }
 }
