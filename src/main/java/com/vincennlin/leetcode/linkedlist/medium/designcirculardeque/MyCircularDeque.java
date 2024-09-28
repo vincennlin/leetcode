b8fd7f37472bc769ba802985d6e67ad7da7bf300
vincennlin <vincentagwa@gmail.com>
2024-09-28 09:46:59 +0800

    Linked List - Design Circular Deque

---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/medium/designcirculardeque/MyCircularDeque.java
@@ -0,0 +1,40 @@
+package com.vincennlin.leetcode.linkedlist.medium.designcirculardeque;
+// 641
+class MyCircularDeque {
+
+    public MyCircularDeque(int k) {
+
+    }
+
+    public boolean insertFront(int value) {
+        return false;
+    }
+
+    public boolean insertLast(int value) {
+        return false;
+    }
+
+    public boolean deleteFront() {
+        return false;
+    }
+
+    public boolean deleteLast() {
+        return false;
+    }
+
+    public int getFront() {
+        return 0;
+    }
+
+    public int getRear() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+}
