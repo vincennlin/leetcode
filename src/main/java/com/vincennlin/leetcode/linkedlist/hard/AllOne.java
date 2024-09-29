9f61cb17950d302a7dc0afcf9194230277956d9e
vincennlin <vincentagwa@gmail.com>
2024-09-29 23:59:48 +0800

    Linked List - All O`one Data Structure

---
+++ src/main/java/com/vincennlin/leetcode/linkedlist/hard/AllOne.java
@@ -0,0 +1,24 @@
+package com.vincennlin.leetcode.linkedlist.hard;
+// 432
+class AllOne {
+
+    public AllOne() {
+
+    }
+
+    public void inc(String key) {
+
+    }
+
+    public void dec(String key) {
+
+    }
+
+    public String getMaxKey() {
+        return "";
+    }
+
+    public String getMinKey() {
+        return "";
+    }
+}
