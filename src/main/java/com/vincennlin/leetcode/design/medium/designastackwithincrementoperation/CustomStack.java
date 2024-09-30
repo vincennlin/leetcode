d151c8733b58ffc8dc3d7edbfe5f95c73e573784
vincennlin <vincentagwa@gmail.com>
2024-09-30 09:22:51 +0800

    Design - Design a Stack With Increment Operation

---
+++ src/main/java/com/vincennlin/leetcode/design/medium/designastackwithincrementoperation/CustomStack.java
@@ -0,0 +1,20 @@
+package com.vincennlin.leetcode.design.medium.designastackwithincrementoperation;
+// 1381
+class CustomStack {
+
+    public CustomStack(int maxSize) {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+        return 0;
+    }
+
+    public void increment(int k, int val) {
+
+    }
+}
