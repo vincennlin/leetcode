963541bc8ae884fbccbd4de084a5acfe362b1d9e
vincennlin <vincentagwa@gmail.com>
2024-09-30 09:31:08 +0800

    Design - Design a Stack With Increment Operation - ans

--- src/main/java/com/vincennlin/leetcode/design/medium/designastackwithincrementoperation/CustomStack.java
+++ src/main/java/com/vincennlin/leetcode/design/medium/designastackwithincrementoperation/CustomStack.java
@@ -1,35 +1,31 @@
 package com.vincennlin.leetcode.design.medium.designastackwithincrementoperation;
-
-import java.util.ArrayList;
-import java.util.List;
-
 // 1381
 class CustomStack {
 
-    List<Integer> stack;
-    int maxSize;
+    int[] stack;
+    int index;
 
     public CustomStack(int maxSize) {
-        this.stack = new ArrayList<>();
-        this.maxSize = maxSize;
+        stack = new int[maxSize];
+        index = 0;
     }
 
     public void push(int x) {
-        if (stack.size() < maxSize) {
-            stack.add(x);
+        if (index < stack.length) {
+            stack[index++] = x;
         }
     }
 
     public int pop() {
-        if (stack.isEmpty()) {
+        if (index < 1) {
             return -1;
         }
-        return stack.remove(stack.size() - 1);
+        return stack[--index];
     }
 
     public void increment(int k, int val) {
-        for (int i = 0; i < Math.min(stack.size(), k); i++) {
-            stack.set(i, stack.get(i) + val);
+        for (int i = 0; i < Math.min(stack.length, k); i++) {
+            stack[i] += val;
         }
     }
 }
