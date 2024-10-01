4f579365d3e4a4f99ece378b56c87aad51c90f0c
vincennlin <vincentagwa@gmail.com>
2024-10-01 10:05:27 +0800

    Array - Check If Array Paris Are Divisible by k

---
+++ src/main/java/com/vincennlin/leetcode/array/medium/checkifarraypairsaredivisiblebyk/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.array.medium.checkifarraypairsaredivisiblebyk;
+// 1497
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        return false;
+    }
+}
