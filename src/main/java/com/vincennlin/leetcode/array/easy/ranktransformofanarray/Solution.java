b29550297f91d9a346ac2771068ae6ce20f0336d
vincennlin <vincentagwa@gmail.com>
2024-10-02 09:44:33 +0800

    Array - Rank Transform of an Array

---
+++ src/main/java/com/vincennlin/leetcode/array/easy/ranktransformofanarray/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.array.easy.ranktransformofanarray;
+// 1331
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        return new int[]{};
+    }
+}
