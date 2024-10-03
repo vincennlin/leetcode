6373a18a7f59dafcdf9acaade099a66397c212f7
vincennlin <vincentagwa@gmail.com>
2024-10-03 09:41:43 +0800

    Prefix Sum - Make Sum Divisible by P - ans

--- src/main/java/com/vincennlin/leetcode/prefixsum/medium/makesumdivisblebyp/Solution.java
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/makesumdivisblebyp/Solution.java
@@ -1,7 +1,38 @@
 package com.vincennlin.leetcode.prefixsum.medium.makesumdivisblebyp;
+
+import java.util.HashMap;
+import java.util.Map;
+
 // 1590
 class Solution {
     public int minSubarray(int[] nums, int p) {
-        return 0;
+        int m = nums.length;
+        int target = 0;
+        for (int num : nums) {
+            target = (target + num) % p;
+        }
+
+        if (target == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> prefixModIndex = new HashMap<>();
+        prefixModIndex.put(0, -1);
+
+        int currentMod = 0;
+        int minLength = m;
+
+        for (int i = 0; i < m; i++) {
+            currentMod = (currentMod + nums[i]) % p;
+
+            int neededMod = (currentMod - target + p) % p;
+            if (prefixModIndex.containsKey(neededMod)) {
+                minLength = Math.min(minLength, i - prefixModIndex.get(neededMod));
+            }
+
+            prefixModIndex.put(currentMod, i);
+        }
+
+        return minLength == m ? -1 : minLength;
     }
 }
