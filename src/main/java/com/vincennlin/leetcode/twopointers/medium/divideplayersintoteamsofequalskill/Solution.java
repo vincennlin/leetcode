a67437fa3387ab4dd1c13fe0ccea1f214954b748
vincennlin <vincentagwa@gmail.com>
2024-10-04 08:52:40 +0800

    Two Pointers - Divide Players Into Teams of Equal Skill - my & ans

--- src/main/java/com/vincennlin/leetcode/twopointers/medium/divideplayersintoteamsofequalskill/Solution.java
+++ src/main/java/com/vincennlin/leetcode/twopointers/medium/divideplayersintoteamsofequalskill/Solution.java
@@ -1,7 +1,22 @@
 package com.vincennlin.leetcode.twopointers.medium.divideplayersintoteamsofequalskill;
+
+import java.util.Arrays;
+
 // 2491
 class Solution {
     public long dividePlayers(int[] skill) {
-        return 0;
+        int m = skill.length;
+
+        Arrays.sort(skill);
+
+        long chemistry = 0;
+        int skillSum = skill[0] + skill[m - 1];
+
+        for (int left = 0, right = m - 1; left < right; left++, right--) {
+            if (skill[left] + skill[right] != skillSum) return -1;
+            chemistry += (long) skill[left] * skill[right];
+        }
+
+        return chemistry;
     }
 }
