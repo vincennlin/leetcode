974480059f6d7633aa3247848192911773bc1ca2
vincennlin <vincentagwa@gmail.com>
2024-10-05 09:54:34 +0800

    Hash Table - Permutation in String

---
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/permutationinstring/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.hashtable.medium.permutationinstring;
+// 567
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        return false;
+    }
+}
