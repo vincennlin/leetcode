a4571e07b3a7b05fdb2cd39782ac14fdb63fda38
vincennlin <vincentagwa@gmail.com>
2024-10-06 13:54:26 +0800

    Two Pointers - Sentence Similarity III

---
+++ src/main/java/com/vincennlin/leetcode/twopointers/medium/sentencesimilarityiii/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.twopointers.medium.sentencesimilarityiii;
+// 1813
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        return false;
+    }
+}
