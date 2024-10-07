d8fa8ccb127f61b2a54999632f422c41d51f4cb4
vincennlin <vincentagwa@gmail.com>
2024-10-07 10:32:59 +0800

    Stack - Minimum String Length After Removing Substrings - my

--- src/main/java/com/vincennlin/leetcode/stack/medium/minimumstringlengthafterremovingsubstrings/Solution.java
+++ src/main/java/com/vincennlin/leetcode/stack/medium/minimumstringlengthafterremovingsubstrings/Solution.java
@@ -1,7 +1,35 @@
 package com.vincennlin.leetcode.stack.medium.minimumstringlengthafterremovingsubstrings;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 // 2696
 class Solution {
     public int minLength(String s) {
-        return 0;
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            deque.add(c);
+        }
+
+        return recursive(deque);
+    }
+
+    private int recursive(Deque<Character> deque) {
+        if (deque.isEmpty()) return 0;
+        Deque<Character> tempDeque = new ArrayDeque<>();
+        tempDeque.add(deque.pollFirst());
+
+        while (!deque.isEmpty()) {
+            if ((tempDeque.peekLast() == 'A' && deque.peekFirst() == 'B') || (tempDeque.peekLast() == 'C' && deque.peekFirst() == 'D')) {
+                tempDeque.pollLast();
+                deque.pollFirst();
+                tempDeque.addAll(deque);
+                return recursive(tempDeque);
+            } else {
+                tempDeque.add(deque.pollFirst());
+            }
+        }
+
+        return tempDeque.size();
     }
 }
