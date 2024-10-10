68c0f9c0c96afcd890e4f804299b3293025e187d
vincennlin <vincentagwa@gmail.com>
2024-10-10 09:38:08 +0800

    Monotonic Stack - Maximum Width Ramp - ans

--- src/main/java/com/vincennlin/leetcode/monotonicstack/medium/maximumwidthramp/Solution.java
+++ src/main/java/com/vincennlin/leetcode/monotonicstack/medium/maximumwidthramp/Solution.java
@@ -1,7 +1,30 @@
 package com.vincennlin.leetcode.monotonicstack.medium.maximumwidthramp;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 // 962
 class Solution {
     public int maxWidthRamp(int[] nums) {
-        return 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (stack.isEmpty() || nums[stack.peek()] > num) {
+                stack.push(i);
+            }
+            if (num == 0) break;
+        }
+
+        int maxWidth = 0;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int num = nums[i];
+            while (!stack.isEmpty() && num >= nums[stack.peek()]) {
+                maxWidth = Math.max(maxWidth, i - stack.pop());
+            }
+        }
+
+        return maxWidth;
     }
 }
