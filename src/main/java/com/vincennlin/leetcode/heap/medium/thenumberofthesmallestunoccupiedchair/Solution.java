e250ff16438c683e60c606312b5f8521edfedf05
vincennlin <vincentagwa@gmail.com>
2024-10-11 09:14:15 +0800

    Heap - The NUmber of the Smallest Unoccupied Chair - ans

--- src/main/java/com/vincennlin/leetcode/heap/medium/thenumberofthesmallestunoccupiedchair/Solution.java
+++ src/main/java/com/vincennlin/leetcode/heap/medium/thenumberofthesmallestunoccupiedchair/Solution.java
@@ -5,39 +5,29 @@ import java.util.*;
 // 1942
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-        int n = times.length;
-        List<int[]> events = new ArrayList<>();
+        int targetStart = times[targetFriend][0];
+        Arrays.sort(times, (a, b) -> a[0] - b[0]);
 
-        for (int i = 0; i < n; i++) {
-            int[] time = times[i];
-            events.add(new int[]{time[0], i});
-            events.add(new int[]{time[1], ~i}); // bitwise NOT
-        }
-        events.sort((o1, o2) -> o1[0] - o2[0]);
+        Queue<Integer> chairs = new PriorityQueue<>();
 
-        Queue<Integer> availableChairs = new PriorityQueue<>();
-        Queue<int[]> occupiedChairs = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-
-        for (int i = 0; i < n; i++) {
-            availableChairs.add(i);
+        for (int i = 0; i < times.length; i++) {
+            chairs.add(i);
         }
 
-        for (int[] event : events) {
-            int time = event[0];
-            int friendIndex = event[1];
+        Queue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
-            while (!occupiedChairs.isEmpty() && occupiedChairs.peek()[0] <= time) {
-                availableChairs.add(occupiedChairs.poll()[1]);
+        for (int i = 0; i < times.length; i++) {
+            while (!minHeap.isEmpty() && minHeap.peek()[0] <= times[i][0]) {
+                chairs.add(minHeap.poll()[1]);
             }
-            if (friendIndex >= 0) {
-                int chair = availableChairs.poll();
-                if (friendIndex == targetFriend) {
-                    return chair;
-                }
-                occupiedChairs.add(new int[]{times[friendIndex][1], chair});
+
+            if (times[i][0] == targetStart) {
+                return chairs.peek();
             }
+
+            minHeap.offer(new int[]{times[i][1], chairs.poll()});
         }
 
-        return -1;
+        return chairs.peek();
     }
 }
