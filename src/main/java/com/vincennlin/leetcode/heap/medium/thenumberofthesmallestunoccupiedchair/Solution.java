5f6d15ca1cd4d261e95b489100a32a674990cb96
vincennlin <vincentagwa@gmail.com>
2024-10-11 08:35:11 +0800

    Heap - The NUmber of the Smallest Unoccupied Chair

---
+++ src/main/java/com/vincennlin/leetcode/heap/medium/thenumberofthesmallestunoccupiedchair/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.heap.medium.thenumberofthesmallestunoccupiedchair;
+// 1942
+class Solution {
+    public int smallestChair(int[][] times, int targetFriend) {
+        return 0;
+    }
+}
