4f6316f3cad2ded431a4ebdacd36a1a8f4bd26d9
vincennlin <vincentagwa@gmail.com>
2024-10-12 09:16:48 +0800

    Heap - Divide Intervals Into Minimum Number of Groups

---
+++ src/main/java/com/vincennlin/leetcode/heap/medium/divideintervalsintominimumnumberofgroups/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.heap.medium.divideintervalsintominimumnumberofgroups;
+// 2406
+class Solution {
+    public int minGroups(int[][] intervals) {
+        return 0;
+    }
+}
