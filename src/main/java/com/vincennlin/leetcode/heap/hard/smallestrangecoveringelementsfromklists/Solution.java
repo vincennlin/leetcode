21b0b9ce43562fa8d2d026e3307e5802a445ebd8
vincennlin <vincentagwa@gmail.com>
2024-10-13 09:32:50 +0800

    Heap - Smallest Range Covering Elements from K Lists

---
+++ src/main/java/com/vincennlin/leetcode/heap/hard/smallestrangecoveringelementsfromklists/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.heap.hard.smallestrangecoveringelementsfromklists;
+
+import java.util.List;
+
+// 632
+class Solution {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        return new int[]{};
+    }
+}
