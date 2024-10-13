397d12393bc35f1a0b691c1bd045c4b948c5bcf4
vincennlin <vincentagwa@gmail.com>
2024-10-13 09:25:24 +0800

    Heap - Divide Intervals Into Minimum Number of Groups - ans

--- src/main/java/com/vincennlin/leetcode/heap/medium/divideintervalsintominimumnumberofgroups/Solution.java
+++ src/main/java/com/vincennlin/leetcode/heap/medium/divideintervalsintominimumnumberofgroups/Solution.java
@@ -9,16 +9,13 @@ class Solution {
     public int minGroups(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
 
-        Queue<Integer> endHeap = new PriorityQueue<>((o1, o2) -> o1 - o2);
+        Queue<Integer> endHeap = new PriorityQueue<>();
 
         for (int[] interval : intervals) {
-            int start = interval[0];
-            int end = interval[1];
-
-            if (!endHeap.isEmpty() && endHeap.peek() < start) {
+            if (!endHeap.isEmpty() && endHeap.peek() < interval[0]) {
                 endHeap.poll();
             }
-            endHeap.add(end);
+            endHeap.add(interval[1]);
         }
 
         return endHeap.size();
