0c6c93a2a38e3ec6eb407f967311080f4443a970
vincennlin <vincentagwa@gmail.com>
2024-10-14 09:12:56 +0800

    Heap - Maximum Score After Applying K Operations

---
+++ src/main/java/com/vincennlin/leetcode/heap/medium/maximumscoreafterapplyingkoperations/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.heap.medium.maximumscoreafterapplyingkoperations;
+// 2530
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+        return 0L;
+    }
+}
