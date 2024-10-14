0e01bc7e553cbe765d1db5b3604c8144d6ca5fda
vincennlin <vincentagwa@gmail.com>
2024-10-14 09:22:06 +0800

    Heap - Maximum Score After Applying K Operations - my & ans

--- src/main/java/com/vincennlin/leetcode/heap/medium/maximumscoreafterapplyingkoperations/Solution.java
+++ src/main/java/com/vincennlin/leetcode/heap/medium/maximumscoreafterapplyingkoperations/Solution.java
@@ -1,7 +1,25 @@
 package com.vincennlin.leetcode.heap.medium.maximumscoreafterapplyingkoperations;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 // 2530
 class Solution {
     public long maxKelements(int[] nums, int k) {
-        return 0L;
+        Queue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int num : nums) {
+            maxHeap.add(num);
+        }
+
+        long score = 0;
+
+        for (int i = 0; i < k; i++) {
+            int increase = maxHeap.poll();
+            score += increase;
+            maxHeap.add((int) Math.ceil(((double) increase) / 3));
+        }
+
+        return score;
     }
 }
