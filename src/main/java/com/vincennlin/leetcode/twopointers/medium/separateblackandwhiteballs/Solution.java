36f3a3aaadfec1752de5f7392d6a805d1d146de1
vincennlin <vincentagwa@gmail.com>
2024-10-15 08:44:39 +0800

    Two Pointers - Separate Black and White Balls - ans - counter

--- src/main/java/com/vincennlin/leetcode/twopointers/medium/separateblackandwhiteballs/Solution.java
+++ src/main/java/com/vincennlin/leetcode/twopointers/medium/separateblackandwhiteballs/Solution.java
@@ -2,6 +2,17 @@ package com.vincennlin.leetcode.twopointers.medium.separateblackandwhiteballs;
 // 2938
 class Solution {
     public long minimumSteps(String s) {
-        return 0;
+        long totalSwaps = 0;
+        int blackBallCount = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '0') {
+                totalSwaps += blackBallCount;
+            } else {
+                blackBallCount++;
+            }
+        }
+
+        return totalSwaps;
     }
 }
