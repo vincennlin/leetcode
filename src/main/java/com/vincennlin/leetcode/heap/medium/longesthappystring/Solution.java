0027d4a1e37243b2cfc777bd4e9b8426dbebb09e
vincennlin <vincentagwa@gmail.com>
2024-10-16 13:50:58 +0800

    Heap - Longest Happy String

---
+++ src/main/java/com/vincennlin/leetcode/heap/medium/longesthappystring/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.heap.medium.longesthappystring;
+// 1405
+class Solution {
+    public String longestDiverseString(int a, int b, int c) {
+        return "";
+    }
+}
