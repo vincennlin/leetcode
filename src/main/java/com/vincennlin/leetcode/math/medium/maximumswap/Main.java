e7fa68295214a8b9596fd62278551578d025fdd2
vincennlin <vincentagwa@gmail.com>
2024-10-17 09:25:33 +0800

    Math - Maximum Swap

---
+++ src/main/java/com/vincennlin/leetcode/math/medium/maximumswap/Main.java
@@ -0,0 +1,33 @@
+package com.vincennlin.leetcode.math.medium.maximumswap;
+/*
+You are given an integer num. You can swap two digits at most once to get the maximum valued number.
+
+Return the maximum valued number you can get.
+
+
+
+Example 1:
+
+Input: num = 2736
+Output: 7236
+Explanation: Swap the number 2 and the number 7.
+Example 2:
+
+Input: num = 9973
+Output: 9973
+Explanation: No swap.
+
+
+Constraints:
+
+0 <= num <= 108
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.maximumSwap(2736)); // 7236
+
+        System.out.println(solution.maximumSwap(9973)); // 9973
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/math/medium/maximumswap/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.math.medium.maximumswap;
+// 670
+class Solution {
+    public int maximumSwap(int num) {
+        return 0;
+    }
+}
