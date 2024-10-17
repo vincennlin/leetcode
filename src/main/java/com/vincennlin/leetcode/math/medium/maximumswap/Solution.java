8cfe26dc025dc18249e2a319dbe327a19df02236
vincennlin <vincentagwa@gmail.com>
2024-10-17 10:03:24 +0800

    Math - Maximum Swap - ans

--- src/main/java/com/vincennlin/leetcode/math/medium/maximumswap/Solution.java
+++ src/main/java/com/vincennlin/leetcode/math/medium/maximumswap/Solution.java
@@ -1,47 +1,25 @@
 package com.vincennlin.leetcode.math.medium.maximumswap;
 
-import java.util.Arrays;
-
 // 670
 class Solution {
     public int maximumSwap(int num) {
+        char[] digits = String.valueOf(num).toCharArray();
         int[] lastIndex = new int[10];
-        Arrays.fill(lastIndex, -1);
 
-        int temp = num;
-        int index = 0;
-        while (temp > 0) {
-            int digits = (int) Math.log10(temp);
-            int divisor = (int) Math.pow(10, digits);
-            int highestDigit = temp / divisor;
-            lastIndex[highestDigit] = index;
-            index++;
-            temp = temp % (divisor * highestDigit);
+        for (int i = 0; i < digits.length; i++) {
+            lastIndex[digits[i] - '0'] = i;
         }
 
-        int digits = (int) Math.log10(num);
-        int highestDigit = num / ((int) Math.pow(10, digits));
+        for (int i = 0; i < digits.length; i++)    {
+            for (int d = 9; d > digits[i] - '0'; d--) {
+                if (lastIndex[d] > i) {
+                    char temp = digits[i];
+                    digits[i] = digits[lastIndex[d]];
+                    digits[lastIndex[d]] = temp;
 
-        for (int i = 9; i > highestDigit; i--) {
-            if (lastIndex[i] == -1) {
-                continue;
-            }
-
-            StringBuilder tempSb = new StringBuilder(String.valueOf(num));
-
-            int replacement = 0;
-            for (; replacement < digits; replacement++) {
-                if (tempSb.charAt(replacement) - '0' >= i) {
-                    continue;
-                } else {
-                    break;
+                    return Integer.parseInt(new String(digits));
                 }
             }
-
-            tempSb.replace(lastIndex[i], lastIndex[i] + 1, String.valueOf(highestDigit));
-            tempSb.replace(replacement, replacement + 1, String.valueOf(i));
-
-            return Integer.parseInt(tempSb.toString());
         }
 
         return num;
