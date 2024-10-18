31c6138b87085bdb3a2f28cde09ad9ea8ff48a99
vincennlin <vincentagwa@gmail.com>
2024-10-18 12:21:56 +0800

    Bit - Count Number of Maximum Bitwise-OR Subsets - ans

--- src/main/java/com/vincennlin/leetcode/bit/medium/countnumberofmaximumbitwiseorsubsets/Main.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/countnumberofmaximumbitwiseorsubsets/Main.java
@@ -1,12 +1,5 @@
 package com.vincennlin.leetcode.bit.medium.countnumberofmaximumbitwiseorsubsets;
 /*
-2044. Count Number of Maximum Bitwise-OR Subsets
-Medium
-
-Topics
-Companies
-
-Hint
 Given an integer array nums, find the maximum possible bitwise OR of a subset of nums and return the number of different non-empty subsets with the maximum bitwise OR.
 
 An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b. Two subsets are considered different if the indices of the elements chosen are different.
--- src/main/java/com/vincennlin/leetcode/bit/medium/countnumberofmaximumbitwiseorsubsets/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/countnumberofmaximumbitwiseorsubsets/Solution.java
@@ -1,7 +1,29 @@
 package com.vincennlin.leetcode.bit.medium.countnumberofmaximumbitwiseorsubsets;
 // 2044
 class Solution {
+    private int[] nums;
+    private int count;
+    private int maxOr;
+
     public int countMaxOrSubsets(int[] nums) {
-        return 0;
+        this.nums = nums;
+        this.count = 0;
+        this.maxOr = 0;
+        for (int num : nums) {
+            maxOr |= num;
+        }
+        dfs(0, 0);
+        return count;
+    }
+
+    private void dfs(int index, int currentOr) {
+        if (index == nums.length) {
+            if (currentOr == maxOr) {
+                count++;
+            }
+            return;
+        }
+        dfs(index + 1, currentOr | nums[index]);
+        dfs(index + 1, currentOr);
     }
 }
