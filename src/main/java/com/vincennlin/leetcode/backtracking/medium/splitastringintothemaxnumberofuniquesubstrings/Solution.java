37c3c36fd183b6496b9751191a10f60283126576
vincennlin <vincentagwa@gmail.com>
2024-10-21 10:08:41 +0800

    Backtracking - Split a String Into the Max NUmber of Unique Substrings

---
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/splitastringintothemaxnumberofuniquesubstrings/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.backtracking.medium.splitastringintothemaxnumberofuniquesubstrings;
+// 1593
+class Solution {
+    public int maxUniqueSplit(String s) {
+        return 0;
+    }
+}
