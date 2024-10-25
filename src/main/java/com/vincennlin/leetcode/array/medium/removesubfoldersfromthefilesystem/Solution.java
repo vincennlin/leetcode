4493f29d65eb3f7e321c0d28e8f4ecf9a436087f
vincennlin <vincentagwa@gmail.com>
2024-10-25 08:35:01 +0800

    Array - Remove Sub-Folders from the Filesystem - my & ans

--- src/main/java/com/vincennlin/leetcode/array/medium/removesubfoldersfromthefilesystem/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/removesubfoldersfromthefilesystem/Solution.java
@@ -1,10 +1,24 @@
 package com.vincennlin.leetcode.array.medium.removesubfoldersfromthefilesystem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // 1233
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        return null;
+        Arrays.sort(folder);
+        String prev = folder[0];
+        List<String> result = new ArrayList<>();
+        result.add(prev);
+
+        for (int i = 1; i < folder.length; i++) {
+            if (!folder[i].startsWith(prev + '/')) {
+                result.add(folder[i]);
+                prev = folder[i];
+            }
+        }
+
+        return result;
     }
 }
