a67edd97ae762350776de041669fd71e80c59a46
vincennlin <vincentagwa@gmail.com>
2024-10-27 10:43:02 +0800

    Multidimensional DP - Count Square Submatrices with All Ones - ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/countsquaresubmatriceswithallones/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/countsquaresubmatriceswithallones/Solution.java
@@ -1,7 +1,24 @@
 package com.vincennlin.leetcode.dynamicprogramming.multidimensional.medium.countsquaresubmatriceswithallones;
+
+import java.util.Arrays;
+
 // 1277
 class Solution {
     public int countSquares(int[][] matrix) {
-        return 0;
+        int row = matrix.length;;
+        int col = matrix[0].length;
+        int[][] dp = new int[row + 1][col + 1];
+        int ans = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 1) {
+                    dp[i + 1][j + 1] = Math.min(dp[i][j], Math.min(dp[i + 1][j], dp[i][j + 1])) + 1;
+                    ans += dp[i + 1][j + 1];
+                }
+            }
+        }
+
+        return ans;
     }
 }
