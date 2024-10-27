371701661935b308230a6634ada1d7aaa66eb0c9
vincennlin <vincentagwa@gmail.com>
2024-10-27 09:56:38 +0800

    Multidimensional DP - Count Square Submatrices with All Ones

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/countsquaresubmatriceswithallones/Main.java
@@ -0,0 +1,60 @@
+package com.vincennlin.leetcode.dynamicprogramming.multidimensional.medium.countsquaresubmatriceswithallones;
+/*
+Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+
+
+Example 1:
+
+Input: matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+Output: 15
+Explanation:
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+Example 2:
+
+Input: matrix =
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+]
+Output: 7
+Explanation:
+There are 6 squares of side 1.
+There is 1 square of side 2.
+Total number of squares = 6 + 1 = 7.
+
+
+Constraints:
+
+1 <= arr.length <= 300
+1 <= arr[0].length <= 300
+0 <= arr[i][j] <= 1
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[][] matrix1 = {
+            {0,1,1,1},
+            {1,1,1,1},
+            {0,1,1,1}
+        };
+        System.out.println(solution.countSquares(matrix1)); // 15
+
+        int[][] matrix2 = {
+            {1,0,1},
+            {1,1,0},
+            {1,1,0}
+        };
+        System.out.println(solution.countSquares(matrix2)); // 7
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/countsquaresubmatriceswithallones/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dynamicprogramming.multidimensional.medium.countsquaresubmatriceswithallones;
+// 1277
+class Solution {
+    public int countSquares(int[][] matrix) {
+        return 0;
+    }
+}
