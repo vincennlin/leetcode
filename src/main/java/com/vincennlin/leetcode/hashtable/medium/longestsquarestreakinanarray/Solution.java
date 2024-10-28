5e9da5014914bb86f70d584b2858a8d34e7579c2
vincennlin <vincentagwa@gmail.com>
2024-10-28 09:40:22 +0800

    Hash Table - Longest Square Streak in an Array

---
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/longestsquarestreakinanarray/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.hashtable.medium.longestsquarestreakinanarray;
+// 2501
+class Solution {
+    public int longestSquareStreak(int[] nums) {
+        return 0;
+    }
+}
