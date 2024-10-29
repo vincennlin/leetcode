ab6a8836d42bc9c6d2443b2219cedcdbee800904
vincennlin <vincentagwa@gmail.com>
2024-10-29 08:40:59 +0800

    Multidimensional DP - Maximum Number of Moves in a Grid

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/medium/maximumnumberofmovesinagrid/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dynamicprogramming.multidimensional.medium.maximumnumberofmovesinagrid;
+// 2684
+class Solution {
+    public int maxMoves(int[][] grid) {
+        return 0;
+    }
+}
