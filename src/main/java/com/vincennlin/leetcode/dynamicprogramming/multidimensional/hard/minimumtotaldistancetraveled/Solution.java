99c77abea37f4cfb55790f33b03bea80a6ccbefe
vincennlin <vincentagwa@gmail.com>
2024-10-31 21:48:54 +0800

    Multidimensional DP - Minimum Total Distance Traveled - ans - not understood

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/hard/minimumtotaldistancetraveled/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/hard/minimumtotaldistancetraveled/Solution.java
@@ -1,10 +1,62 @@
 package com.vincennlin.leetcode.dynamicprogramming.multidimensional.hard.minimumtotaldistancetraveled;
 
-import java.util.List;
+import java.util.*;
 
 // 2463
+// not understood
 class Solution {
     public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
-        return 0;
+        Collections.sort(robot);
+        Arrays.sort(factory, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int m = robot.size();
+        int n = factory.length;
+
+        long[][] dp = new long[m + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
+            dp[i][n] = Long.MAX_VALUE;
+        }
+
+        for (int j = n - 1; j >= 0; j--) {
+            long prefix = 0;
+            Deque<Pair> qq = new ArrayDeque<>();
+            qq.offer(new Pair(m, 0L));
+
+            for (int i = m - 1; i >= 0; i--) {
+                prefix += Math.abs(robot.get(i) - factory[j][0]);
+
+                while (!qq.isEmpty() && qq.peekFirst().getKey() > i + factory[j][1]) {
+                    qq.pollFirst();
+                }
+
+                while (!qq.isEmpty() && qq.peekLast().getValue() >= dp[i][j + 1] - prefix) {
+                    qq.pollLast();
+                }
+
+                qq.offerLast(new Pair(i, dp[i][j + 1] - prefix));
+                dp[i][j] = qq.peekFirst().getValue() + prefix;
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    private static class Pair {
+        private Integer key;
+        private Long value;
+
+        public Pair(Integer key, Long value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Integer getKey() {
+            return key;
+        }
+
+        public Long getValue() {
+            return value;
+        }
     }
 }
