ce45528464ef735411e95aec9e491f221cc50df3
vincennlin <vincentagwa@gmail.com>
2024-10-31 21:30:39 +0800

    Multidimensional DP - Minimum Total Distance Traveled

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/hard/minimumtotaldistancetraveled/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.dynamicprogramming.multidimensional.hard.minimumtotaldistancetraveled;
+
+import java.util.List;
+
+// 2463
+class Solution {
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        return 0;
+    }
+}
