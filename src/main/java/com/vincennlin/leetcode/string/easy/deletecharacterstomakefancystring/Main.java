631a540b17d71492e9866b1de5197f8806a3e82d
vincennlin <vincentagwa@gmail.com>
2024-11-01 12:26:37 +0800

    String - Delete Characters to Make Fancy String

---
+++ src/main/java/com/vincennlin/leetcode/string/easy/deletecharacterstomakefancystring/Main.java
@@ -0,0 +1,48 @@
+package com.vincennlin.leetcode.string.easy.deletecharacterstomakefancystring;
+/*
+A fancy string is a string where no three consecutive characters are equal.
+
+Given a string s, delete the minimum possible number of characters from s to make it fancy.
+
+Return the final string after the deletion. It can be shown that the answer will always be unique.
+
+
+
+Example 1:
+
+Input: s = "leeetcode"
+Output: "leetcode"
+Explanation:
+Remove an 'e' from the first group of 'e's to create "leetcode".
+No three consecutive characters are equal, so return "leetcode".
+Example 2:
+
+Input: s = "aaabaaaa"
+Output: "aabaa"
+Explanation:
+Remove an 'a' from the first group of 'a's to create "aabaaaa".
+Remove two 'a's from the second group of 'a's to create "aabaa".
+No three consecutive characters are equal, so return "aabaa".
+Example 3:
+
+Input: s = "aab"
+Output: "aab"
+Explanation: No three consecutive characters are equal, so return "aab".
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists only of lowercase English letters.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.makeFancyString("leeetcode")); // "leetcode"
+
+        System.out.println(solution.makeFancyString("aaabaaaa")); // "aabaa"
+
+        System.out.println(solution.makeFancyString("aab")); // "aab"
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/string/easy/deletecharacterstomakefancystring/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.string.easy.deletecharacterstomakefancystring;
+// 1957
+class Solution {
+    public String makeFancyString(String s) {
+        return "";
+    }
+}
