08c758ce2628ef2081815c97534669f9f3e9ab46
vincennlin <vincentagwa@gmail.com>
2024-11-02 09:41:00 +0800

    String - Circular Sentence - my & ans

--- src/main/java/com/vincennlin/leetcode/string/easy/circularsentence/Solution.java
+++ src/main/java/com/vincennlin/leetcode/string/easy/circularsentence/Solution.java
@@ -2,6 +2,13 @@ package com.vincennlin.leetcode.string.easy.circularsentence;
 // 2490
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        return false;
+        for (int i = 1; i < sentence.length() - 1; i++) {
+            char c = sentence.charAt(i);
+            if (c == ' ' && sentence.charAt(i - 1) != sentence.charAt(i + 1)) {
+                return false;
+            }
+        }
+
+        return sentence.charAt(0) == sentence.charAt(sentence.length() - 1);
     }
 }
