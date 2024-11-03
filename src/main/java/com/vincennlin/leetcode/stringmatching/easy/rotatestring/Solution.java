97c989292939660f8d1c2cc38b9b1a3e9dd10742
vincennlin <vincentagwa@gmail.com>
2024-11-03 10:53:44 +0800

    String Matching - Rotate String

---
+++ src/main/java/com/vincennlin/leetcode/stringmatching/easy/rotatestring/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.stringmatching.easy.rotatestring;
+// 796
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        return false;
+    }
+}
