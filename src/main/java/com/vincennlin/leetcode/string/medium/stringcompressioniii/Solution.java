d6379183d6e487d48e4041468bf056354249ab1c
vincennlin <vincentagwa@gmail.com>
2024-11-04 09:55:34 +0800

    String - String Compression III - my

--- src/main/java/com/vincennlin/leetcode/string/medium/stringcompressioniii/Solution.java
+++ src/main/java/com/vincennlin/leetcode/string/medium/stringcompressioniii/Solution.java
@@ -2,6 +2,20 @@ package com.vincennlin.leetcode.string.medium.stringcompressioniii;
 // 3163
 class Solution {
     public String compressedString(String word) {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        char lastChar = word.charAt(0);
+        int count = 1;
+
+        for (int i = 1; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (lastChar != c || count >= 9) {
+                sb.append(count).append(lastChar);
+                count = 0;
+            }
+            lastChar = c;
+            count++;
+        }
+
+        return sb.append(count).append(lastChar).toString();
     }
 }
