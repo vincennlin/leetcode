ec6ced232c89cefb8d916c15ab23076e108cd9ba
vincennlin <vincentagwa@gmail.com>
2024-11-06 13:36:02 +0800

    Bit Manipulation - Find if Array Can Be Sorted - ans

--- src/main/java/com/vincennlin/leetcode/bit/medium/findifarraycanbesorted/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/findifarraycanbesorted/Solution.java
@@ -2,6 +2,23 @@ package com.vincennlin.leetcode.bit.medium.findifarraycanbesorted;
 // 3011
 class Solution {
     public boolean canSortArray(int[] nums) {
-        return false;
+        byte prevBit = 0;
+        int prevMax = 0, curMin = 0, curMax = 0;
+
+        for (int num : nums) {
+            byte curBit = (byte) Integer.bitCount(num);
+
+            if (prevBit == curBit) {
+                curMin = Math.min(curMin, num);
+                curMax = Math.max(curMax, num);
+            } else if (curMin < prevMax) {
+                return false;
+            } else {
+                prevMax = curMax;
+                curMin = curMax = num;
+                prevBit = curBit;
+            }
+        }
+        return curMin > prevMax;
     }
 }
