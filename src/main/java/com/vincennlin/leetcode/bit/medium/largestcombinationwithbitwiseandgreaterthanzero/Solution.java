1a48182618039c4a1e920302933c43cc25971104
vincennlin <vincentagwa@gmail.com>
2024-11-07 10:52:23 +0800

    Bit Manipulation - Largest Combination With Bitwise AND Greater Than Zero - my & ans

--- src/main/java/com/vincennlin/leetcode/bit/medium/largestcombinationwithbitwiseandgreaterthanzero/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/largestcombinationwithbitwiseandgreaterthanzero/Solution.java
@@ -2,6 +2,21 @@ package com.vincennlin.leetcode.bit.medium.largestcombinationwithbitwiseandgreat
 // 2275
 class Solution {
     public int largestCombination(int[] candidates) {
-        return 0;
+        int[] bits = new int[24];
+        int max = 0;
+
+        for (int num : candidates) {
+            int pos = 0;
+            while (num > 0) {
+                if ((num & 1) == 1) {
+                    bits[pos] += 1;
+                    max = Math.max(max, bits[pos]);
+                }
+                pos++;
+                num >>= 1;
+            }
+        }
+
+        return max;
     }
 }
