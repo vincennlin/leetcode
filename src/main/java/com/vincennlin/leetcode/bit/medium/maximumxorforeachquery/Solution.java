899ae0285be352d8167fb4bcf576edec294f27be
vincennlin <vincentagwa@gmail.com>
2024-11-08 11:09:43 +0800

    Bit Manipulation - Maximum XOR for Each Query

---
+++ src/main/java/com/vincennlin/leetcode/bit/medium/maximumxorforeachquery/Solution.java
@@ -0,0 +1,9 @@
+package com.vincennlin.leetcode.bit.medium.maximumxorforeachquery;
+/*
+// 1829
+ */
+class Solution {
+    public int[] getMaximumXor(int[] nums, int maximumBit) {
+        return null;
+    }
+}
