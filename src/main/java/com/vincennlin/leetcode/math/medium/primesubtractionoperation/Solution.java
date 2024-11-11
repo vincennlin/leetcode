5cc838e2c0f873fd9149d29be931dd26f97692ed
vincennlin <vincentagwa@gmail.com>
2024-11-11 12:12:11 +0800

    Math - Prime Subtraction Operation

---
+++ src/main/java/com/vincennlin/leetcode/math/medium/primesubtractionoperation/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.math.medium.primesubtractionoperation;
+// 2601
+class Solution {
+    public boolean primeSubOperation(int[] nums) {
+        return false;
+    }
+}
