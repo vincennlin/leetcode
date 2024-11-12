dcbaa50ffab69a10913ff819e5ed2f5876394f2f
vincennlin <vincentagwa@gmail.com>
2024-11-12 11:50:08 +0800

    Binary Search - Most Beautiful Item for Each Query

---
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/mostbeautifulitemforeachquery/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.binarysearch.medium.mostbeautifulitemforeachquery;
+// 2070
+class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        return null;
+    }
+}
