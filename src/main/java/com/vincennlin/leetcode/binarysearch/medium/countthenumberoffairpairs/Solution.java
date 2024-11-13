856296cfc580ac25253797a0dc9cd6950dd2d9f7
vincennlin <vincentagwa@gmail.com>
2024-11-13 13:35:33 +0800

    Binary Search - Count the Number of Fair Pairs

---
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/countthenumberoffairpairs/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.binarysearch.medium.countthenumberoffairpairs;
+// 2563
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        return 0;
+    }
+}
