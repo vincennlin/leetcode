7113cc60a73f1deed662255886b185a00d2381da
vincennlin <vincentagwa@gmail.com>
2024-11-14 08:54:39 +0800

    Binary Search - Minimized Maximum of Products Distributed to Any Store

---
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/minimizedmaximumofproductsdistributedtoanystore/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.binarysearch.medium.minimizedmaximumofproductsdistributedtoanystore;
+// 2064
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        return 0;
+    }
+}
