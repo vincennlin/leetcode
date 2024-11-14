8c7bb283dbe4f83015e02c2fbd15254cd6aa734c
vincennlin <vincentagwa@gmail.com>
2024-11-14 09:12:57 +0800

    Binary Search - Minimized Maximum of Products Distributed to Any Store - ans

--- src/main/java/com/vincennlin/leetcode/binarysearch/medium/minimizedmaximumofproductsdistributedtoanystore/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/minimizedmaximumofproductsdistributedtoanystore/Solution.java
@@ -1,7 +1,33 @@
 package com.vincennlin.leetcode.binarysearch.medium.minimizedmaximumofproductsdistributedtoanystore;
+
+import java.util.Arrays;
+
 // 2064
 class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
-        return 0;
+        int left = 1;
+        int right = Arrays.stream(quantities).max().getAsInt();
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canDistribute(quantities, n, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private boolean canDistribute(int[] quantities, int n, int maxProductsPerStore) {
+        int storesNeeded = 0;
+        for (int quantity : quantities) {
+            storesNeeded += (quantity + maxProductsPerStore - 1) / maxProductsPerStore;
+            if (storesNeeded > n) {
+                return false;
+            }
+        }
+        return true;
     }
 }
