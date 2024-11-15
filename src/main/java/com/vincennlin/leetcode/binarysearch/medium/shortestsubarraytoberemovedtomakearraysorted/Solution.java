9bc702ce2173906643694f75521f928475efe079
vincennlin <vincentagwa@gmail.com>
2024-11-15 11:12:54 +0800

    Binary Search - Shortest Subarray to be Removed to Make Array Sorted - ans

--- src/main/java/com/vincennlin/leetcode/binarysearch/medium/shortestsubarraytoberemovedtomakearraysorted/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/shortestsubarraytoberemovedtomakearraysorted/Solution.java
@@ -2,8 +2,33 @@ package com.vincennlin.leetcode.binarysearch.medium.shortestsubarraytoberemovedt
 // 1574
 class Solution {
     public int findLengthOfShortestSubarray(int[] arr) {
-        return 0;
+        int n = arr.length;
+        int start = 0, end = n - 1;
+
+        while (start < n - 1 && arr[start] <= arr[start + 1]) {
+            start++;
+        }
+
+        if (start == n - 1) {
+            return 0;
+        }
+
+        while (end > 0 && arr[end] >= arr[end - 1]) {
+            end--;
+        }
+
+        int result = Math.min(n - start - 1, end);
+
+        int left = 0, right = end;
+        while (left <= start && right < n) {
+            if (arr[left] <= arr[right]) {
+                result = Math.min(result, right - left - 1);
+                left++;
+            } else {
+                right++;
+            }
+        }
+
+        return result;
     }
-
-
 }
