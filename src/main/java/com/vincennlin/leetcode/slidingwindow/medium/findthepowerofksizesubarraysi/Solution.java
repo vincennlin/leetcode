602c6a093d7fcbabb76a9c1869b9ce45e2ab7028
vincennlin <vincentagwa@gmail.com>
2024-11-16 23:16:39 +0800

    Sliding Window - Find the Power of K-Size Subarrays I - ans

--- src/main/java/com/vincennlin/leetcode/slidingwindow/medium/findthepowerofksizesubarraysi/Solution.java
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/findthepowerofksizesubarraysi/Solution.java
@@ -2,6 +2,26 @@ package com.vincennlin.leetcode.slidingwindow.medium.findthepowerofksizesubarray
 // 3254
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        return null;
+        int n = nums.length;
+        int[] results = new int[n - k + 1];
+
+        for (int i = 0; i <= n - k; i++) {
+            if (isConsecutiveAndSorted(nums, i, i + k - 1)) {
+                results[i] = nums[i + k - 1];
+            } else {
+                results[i] = -1;
+            }
+        }
+
+        return results;
+    }
+
+    private boolean isConsecutiveAndSorted(int[] nums, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (nums[i + 1] != nums[i] + 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
