f2624029bf048f4d12959170cad8a419cce4a5e9
vincennlin <vincentagwa@gmail.com>
2024-11-16 23:13:14 +0800

    Sliding Window - Find the Power of K-Size Subarrays I

---
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/findthepowerofksizesubarraysi/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.slidingwindow.medium.findthepowerofksizesubarraysi;
+// 3254
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        return null;
+    }
+}
