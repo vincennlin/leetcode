6cc801e183e536552e351e7df6c00b7910ee856e
vincennlin <vincentagwa@gmail.com>
2024-11-17 22:32:59 +0800

    Heap - Shortest Subarray with Sum at Least K - ans - bad

--- src/main/java/com/vincennlin/leetcode/heap/hard/shortestsubarraywithsumatleastk/Solution.java
+++ src/main/java/com/vincennlin/leetcode/heap/hard/shortestsubarraywithsumatleastk/Solution.java
@@ -1,7 +1,50 @@
 package com.vincennlin.leetcode.heap.hard.shortestsubarraywithsumatleastk;
+
+import java.util.PriorityQueue;
+
 // 862
 class Solution {
     public int shortestSubarray(int[] nums, int k) {
-        return 0;
+        int n = nums.length;
+        int shortestSubarrayLength = Integer.MAX_VALUE;
+        long cumulativeSum = 0;
+
+        PriorityQueue<Pair> prefixSumHeap = new PriorityQueue<>((a, b) -> Long.compare(a.getKey(), b.getKey()));
+
+        for (int i = 0; i < n; i++) {
+            cumulativeSum += nums[i];
+
+            if (cumulativeSum >= k) {
+                shortestSubarrayLength = Math.min(shortestSubarrayLength, i + 1);
+            }
+
+            while (!prefixSumHeap.isEmpty() && cumulativeSum - prefixSumHeap.peek().getKey() >= k) {
+                shortestSubarrayLength = Math.min(shortestSubarrayLength, i - prefixSumHeap.poll().getValue());
+            }
+
+            prefixSumHeap.offer(new Pair(cumulativeSum, i));
+        }
+
+        return shortestSubarrayLength == Integer.MAX_VALUE
+                ? -1
+                : shortestSubarrayLength;
+    }
+}
+
+class Pair {
+    long key;
+    int value;
+
+    Pair (long key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    long getKey() {
+        return key;
+    }
+
+    int getValue() {
+        return value;
     }
 }
