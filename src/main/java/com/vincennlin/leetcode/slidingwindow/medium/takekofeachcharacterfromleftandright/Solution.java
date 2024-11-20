e8532b2d679c7c902794263e6d943ba8092d8e24
vincennlin <vincentagwa@gmail.com>
2024-11-20 18:04:27 +0800

    Sliding Window - Take K of Each Character From Left and Right - ans

--- src/main/java/com/vincennlin/leetcode/slidingwindow/medium/takekofeachcharacterfromleftandright/Solution.java
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/takekofeachcharacterfromleftandright/Solution.java
@@ -2,6 +2,34 @@ package com.vincennlin.leetcode.slidingwindow.medium.takekofeachcharacterfromlef
 // 2516
 class Solution {
     public int takeCharacters(String s, int k) {
-        return 0;
+        int[] count = new int[3];
+        int n = s.length();
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (count[i] < k) {
+                return -1;
+            }
+        }
+
+        int[] window = new int[3];
+        int left = 0, maxWindow = 0;
+
+        for (int right = 0; right < n; right++) {
+            window[s.charAt(right) - 'a']++;
+
+            while (left <= right &&
+                            (count[0] - window[0] < k || count[1] - window[1] < k || count[2] - window[2] < k)) {
+                window[s.charAt(left) - 'a']--;
+                left++;
+            }
+
+            maxWindow = Math.max(maxWindow, right - left + 1);
+        }
+
+        return n - maxWindow;
     }
 }
