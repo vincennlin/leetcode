fe09757c39b533d155532d12d0781cc781f03e40
vincennlin <vincentagwa@gmail.com>
2024-11-20 17:44:26 +0800

    Sliding Window - Take K of Each Character From Left and Right

---
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/takekofeachcharacterfromleftandright/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.slidingwindow.medium.takekofeachcharacterfromleftandright;
+// 2516
+class Solution {
+    public int takeCharacters(String s, int k) {
+        return 0;
+    }
+}
