8af2b29f2ac869334c52e96c34cd6bcd4feb8c53
vincennlin <vincentagwa@gmail.com>
2024-11-21 12:02:45 +0800

    Matrix - Count Unguarded Cells in the Grid - ans

--- src/main/java/com/vincennlin/leetcode/matrix/medium/countunguardedcellsinthegrid/Solution.java
+++ src/main/java/com/vincennlin/leetcode/matrix/medium/countunguardedcellsinthegrid/Solution.java
@@ -1,37 +1,44 @@
 package com.vincennlin.leetcode.matrix.medium.countunguardedcellsinthegrid;
 // 2257
 class Solution {
-    private int result;
-
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] directions = new int[][]{{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-        char[][] grid = new char[m][n];
-        result = m * n;
+        boolean[][] grid = new boolean[m][n];
+        boolean[][] guarded = new boolean[m][n];
 
         for (int[] wall : walls) {
-            grid[wall[0]][wall[1]] = '*';
-            result--;
+            grid[wall[0]][wall[1]] = true;
         }
 
         for (int[] guard : guards) {
-            grid[guard[0]][guard[1]] = '*';
-            result--;
+            grid[guard[0]][guard[1]] = true;
+        }
+
+        int[][] directions = new int[][]{{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+
+        for (int[] guard : guards) {
             for (int[] direction : directions) {
-                guardGrid(grid, direction, guard[0] + direction[0], guard[1] + direction[1], m, n);
+                int x = guard[0];
+                int y = guard[1];
+                while (true) {
+                    x += direction[0];
+                    y += direction[1];
+                    if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y]) {
+                        break;
+                    }
+                    guarded[x][y] = true;
+                }
             }
         }
 
-        return result;
-    }
+        int unguardedCount = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!grid[i][j] && !guarded[i][j]) {
+                    unguardedCount++;
+                }
+            }
+        }
 
-    private void guardGrid(char[][] grid, int[] direction, int i, int j, int m, int n) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '*') {
-            return;
-        }
-        if (grid[i][j] !='-' && grid[i][j] != '*') {
-            result--;
-        }
-        grid[i][j] = '-';
-        guardGrid(grid, direction, i + direction[0], j + direction[1], m, n);
+        return unguardedCount;
     }
 }
