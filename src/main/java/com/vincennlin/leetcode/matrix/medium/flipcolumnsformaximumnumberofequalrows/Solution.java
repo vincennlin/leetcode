644956a76a80103b54b192a91e2a551b2f36823b
vincennlin <vincentagwa@gmail.com>
2024-11-22 12:01:15 +0800

    Matrix - Flip Columns For Maximum Number of Equal Rows - ans

--- src/main/java/com/vincennlin/leetcode/matrix/medium/flipcolumnsformaximumnumberofequalrows/Solution.java
+++ src/main/java/com/vincennlin/leetcode/matrix/medium/flipcolumnsformaximumnumberofequalrows/Solution.java
@@ -1,7 +1,22 @@
 package com.vincennlin.leetcode.matrix.medium.flipcolumnsformaximumnumberofequalrows;
+
+import java.util.HashMap;
+import java.util.Map;
+
 // 1072
 class Solution {
     public int maxEqualRowsAfterFlips(int[][] matrix) {
-        return 0;
+        Map<String, Integer> rowCountMap = new HashMap<>();
+
+        for (int[] row : matrix) {
+            StringBuilder sb = new StringBuilder();
+            for (int num : row) {
+                sb.append(num == row[0] ? 'T' : 'F');
+            }
+            String rowPattern = sb.toString();
+            rowCountMap.put(rowPattern, rowCountMap.getOrDefault(rowPattern, 0) + 1);
+        }
+
+        return rowCountMap.values().stream().max(Integer::compareTo).get();
     }
 }
