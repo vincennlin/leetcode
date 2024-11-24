269e2814d57d2fcaeafc751633322fd39979d5de
vincennlin <vincentagwa@gmail.com>
2024-11-24 15:07:41 +0800

    Matrix - Maximum Matrix Sum - my & ans

--- src/main/java/com/vincennlin/leetcode/matrix/medium/maximummatrixsum/Solution.java
+++ src/main/java/com/vincennlin/leetcode/matrix/medium/maximummatrixsum/Solution.java
@@ -2,6 +2,20 @@ package com.vincennlin.leetcode.matrix.medium.maximummatrixsum;
 // 1975
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        return 0;
+        long result = 0;
+        int minAbsValue = Integer.MAX_VALUE;
+        boolean isNegativeCountEven = true;
+
+        for (int[] row : matrix) {
+            for (int num : row) {
+                if (num < 0) {
+                    isNegativeCountEven = !isNegativeCountEven;
+                }
+                minAbsValue = Math.min(minAbsValue, Math.abs(num));
+                result += Math.abs(num);
+            }
+        }
+
+        return result - (isNegativeCountEven ? 0 : minAbsValue + minAbsValue);
     }
 }
