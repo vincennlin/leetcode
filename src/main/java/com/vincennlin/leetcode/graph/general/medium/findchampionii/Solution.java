92297a33124041449245ee36c60c0e90a2f516fa
vincennlin <vincentagwa@gmail.com>
2024-11-26 19:10:04 +0800

    Graph - Find Champion II - my & ans

--- src/main/java/com/vincennlin/leetcode/graph/general/medium/findchampionii/Solution.java
+++ src/main/java/com/vincennlin/leetcode/graph/general/medium/findchampionii/Solution.java
@@ -2,6 +2,21 @@ package com.vincennlin.leetcode.graph.general.medium.findchampionii;
 // 2924
 class Solution {
     public int findChampion(int n, int[][] edges) {
-        return 0;
+        int[] inDegrees = new int[n];
+        for (int[] edge : edges) {
+            inDegrees[edge[1]]++;
+        }
+
+        int champion = -1;
+        for (int i = 0; i < n; i++) {
+            if (inDegrees[i] == 0) {
+                if (champion != -1) {
+                    return -1;
+                }
+                champion = i;
+            }
+        }
+
+        return champion;
     }
 }
