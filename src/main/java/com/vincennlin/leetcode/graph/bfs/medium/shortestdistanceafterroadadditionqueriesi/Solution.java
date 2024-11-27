fe47480a6c4af932b19585655f209c7f2978a0e2
vincennlin <vincentagwa@gmail.com>
2024-11-27 12:58:30 +0800

    Graph - Shortest Distance After Road Addition Queries I

---
+++ src/main/java/com/vincennlin/leetcode/graph/bfs/medium/shortestdistanceafterroadadditionqueriesi/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.graph.bfs.medium.shortestdistanceafterroadadditionqueriesi;
+// 3243
+class Solution {
+    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+        return new int[]{};
+    }
+}
