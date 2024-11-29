96cfb2724b1c8e075e0ff3a2e8bb1ad184670f7a
vincennlin <vincentagwa@gmail.com>
2024-11-29 12:43:18 +0800

    Shortest Path - Minimum Time to Visit a Cell In a Grid - ans

--- src/main/java/com/vincennlin/leetcode/shortestpath/hard/minimumtimetovisitacellinagrid/Solution.java
+++ src/main/java/com/vincennlin/leetcode/shortestpath/hard/minimumtimetovisitacellinagrid/Solution.java
@@ -1,30 +1,45 @@
 package com.vincennlin.leetcode.shortestpath.hard.minimumtimetovisitacellinagrid;
 
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 // 2577
 class Solution {
+    private int m, n;
+    private boolean[][] isVisited;
+
     public int minimumTime(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
+        if (grid[0][1] > 1 && grid[1][0] > 1) {
+            return -1;
+        }
+
+        m = grid.length;
+        n = grid[0].length;
+
         Queue<Node> pq = new PriorityQueue<>((a, b) -> a.currentTime - b.currentTime);
         pq.add(new Node(0, 0, 0));
 
         int[][] directions = new int[][]{{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
 
-        int[][] time = new int[m][n];
-        for (int[] row : time) {
-            Arrays.fill(row, Integer.MAX_VALUE);
-        }
-        time[0][0] = 0;
+        isVisited = new boolean[m][n];
 
         while (!pq.isEmpty()) {
             Node node = pq.poll();
+
+            if (node.row == m - 1 && node.col == n - 1) {
+                return node.currentTime;
+            }
+
+            if (isVisited[node.row][node.col]) {
+                continue;
+            }
+
+            isVisited[node.row][node.col] = true;
+
             for (int[] direction : directions) {
                 int newRow = node.row + direction[0];
                 int newCol = node.col + direction[1];
-                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
+                if (isValid(newRow, newCol)) {
                     int newTime;
                     if (node.currentTime >= grid[newRow][newCol]) {
                         newTime = node.currentTime + 1;
@@ -35,19 +50,16 @@ class Solution {
                             newTime = grid[newRow][newCol] + 1;
                         }
                     }
-                    // 要判斷新的時間是否小於之前走過該格的時間，不然 pq 會一直有走過的格子
-                    if ((node.row != 0 || node.col != 0 || newTime == 1) && newTime < time[newRow][newCol]) {
-                        time[newRow][newCol] = newTime;
-                        if (newRow == m - 1 && newCol == n - 1) {
-                            return newTime;
-                        }
-                        pq.add(new Node(newTime, newRow, newCol));
-                    }
+                    pq.add(new Node(newTime, newRow, newCol));
                 }
             }
         }
 
-        return time[m - 1][n - 1] != Integer.MAX_VALUE ? time[m - 1][n - 1] : -1;
+        return -1;
+    }
+
+    private boolean isValid(int newRow, int newCol) {
+        return newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !isVisited[newRow][newCol];
     }
 }
 
