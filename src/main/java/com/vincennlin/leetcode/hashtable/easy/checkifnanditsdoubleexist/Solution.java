c5f4b79aef6f23fd6c8a449458d46fdb5bc028f4
vincennlin <vincentagwa@gmail.com>
2024-12-01 11:14:22 +0800

    Hash Table - Check If N and Its Double Exist - my & ans

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/checkifnanditsdoubleexist/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/checkifnanditsdoubleexist/Solution.java
@@ -1,7 +1,18 @@
 package com.vincennlin.leetcode.hashtable.easy.checkifnanditsdoubleexist;
+
+import java.util.HashSet;
+import java.util.Set;
+
 // 1346
 class Solution {
     public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
+                return true;
+            }
+            set.add(num);
+        }
         return false;
     }
 }
