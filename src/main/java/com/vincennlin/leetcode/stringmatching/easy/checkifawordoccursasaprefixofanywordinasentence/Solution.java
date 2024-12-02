de0748913ae3c83cf3ffd8c526d5f777535f5785
vincennlin <vincentagwa@gmail.com>
2024-12-02 10:39:17 +0800

    String Matching - Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ src/main/java/com/vincennlin/leetcode/stringmatching/easy/checkifawordoccursasaprefixofanywordinasentence/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.stringmatching.easy.checkifawordoccursasaprefixofanywordinasentence;
+// 1455
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        return 0;
+    }
+}
