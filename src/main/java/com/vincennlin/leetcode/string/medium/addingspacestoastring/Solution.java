3e9668bc468cc7559a29547066976c4364436014
vincennlin <vincentagwa@gmail.com>
2024-12-03 09:57:07 +0800

    String - Adding Spaces to a String - my

--- src/main/java/com/vincennlin/leetcode/string/medium/addingspacestoastring/Solution.java
+++ src/main/java/com/vincennlin/leetcode/string/medium/addingspacestoastring/Solution.java
@@ -2,6 +2,15 @@ package com.vincennlin.leetcode.string.medium.addingspacestoastring;
 // 2109
 class Solution {
     public String addSpaces(String s, int[] spaces) {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        int spaceIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (spaceIndex < spaces.length && spaces[spaceIndex] == i) {
+                sb.append(" ");
+                spaceIndex++;
+            }
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
     }
 }
