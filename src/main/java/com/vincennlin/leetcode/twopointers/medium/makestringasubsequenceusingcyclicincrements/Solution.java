b569172da8d2be215fc56a0ea327b52f1ff5616f
vincennlin <vincentagwa@gmail.com>
2024-12-04 13:17:02 +0800

    Two Pointers - Make String a Subsequence Using Cyclic Increments - my & ans

--- src/main/java/com/vincennlin/leetcode/twopointers/medium/makestringasubsequenceusingcyclicincrements/Solution.java
+++ src/main/java/com/vincennlin/leetcode/twopointers/medium/makestringasubsequenceusingcyclicincrements/Solution.java
@@ -2,6 +2,12 @@ package com.vincennlin.leetcode.twopointers.medium.makestringasubsequenceusingcy
 // 2825
 class Solution {
     public boolean canMakeSubsequence(String str1, String str2) {
-        return false;
+        int i = 0, j = 0;
+        for (; i < str1.length() && j < str2.length(); i++) {
+            if ((str2.charAt(j) - str1.charAt(i) + 26) % 26 <= 1) {
+                j++;
+            }
+        }
+        return j == str2.length();
     }
 }
