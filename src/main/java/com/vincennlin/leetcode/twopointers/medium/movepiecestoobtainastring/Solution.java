64c0f3d4bdb1c49ab667f4cb76d202b44ed16106
vincennlin <vincentagwa@gmail.com>
2024-12-05 10:44:40 +0800

    Two Pointers - Move Pieces to Obtain a String

---
+++ src/main/java/com/vincennlin/leetcode/twopointers/medium/movepiecestoobtainastring/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.twopointers.medium.movepiecestoobtainastring;
+// 2337
+class Solution {
+    public boolean canChange(String start, String target) {
+        return false;
+    }
+}
