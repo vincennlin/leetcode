3f94e8e843e9658e7f6109ddcada80dac70cab1a
vincennlin <vincentagwa@gmail.com>
2024-12-06 19:22:51 +0800

    Greedy - Maximum Number of Integers to Choose From a Range I - my & ans

--- src/main/java/com/vincennlin/leetcode/greedy/medium/maximumnumberofintegerstochoosefromarangei/Solution.java
+++ src/main/java/com/vincennlin/leetcode/greedy/medium/maximumnumberofintegerstochoosefromarangei/Solution.java
@@ -1,7 +1,29 @@
 package com.vincennlin.leetcode.greedy.medium.maximumnumberofintegerstochoosefromarangei;
+
+import java.util.HashSet;
+import java.util.Set;
+
 // 2554
 class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
-        return 0;
+        Set<Integer> bannedSet =  new HashSet<>();
+        for (int bannedNum : banned) {
+            bannedSet.add(bannedNum);
+        }
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (!bannedSet.contains(i)) {
+                if (maxSum - i >= 0) {
+                    maxSum -= i;
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return count;
     }
 }
