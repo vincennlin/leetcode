b70ed669d83cd5e3d5e2acfa8d25697108c789c9
vincennlin <vincentagwa@gmail.com>
2024-12-07 12:04:24 +0800

    Binary Search - Minimum Limit of Balls in a Bag

---
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/minimumlimitofballsinabag/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.binarysearch.medium.minimumlimitofballsinabag;
+// 1760
+class Solution {
+    public int minimumSize(int[] nums, int maxOperations) {
+        return 0;
+    }
+}
