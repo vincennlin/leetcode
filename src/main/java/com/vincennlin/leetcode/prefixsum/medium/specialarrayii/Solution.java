ca56efc659cb71bf72645ddeadeadb392faa4aca
vincennlin <vincentagwa@gmail.com>
2024-12-09 15:39:38 +0800

    Prefix Sum - Special Array II

---
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/specialarrayii/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.prefixsum.medium.specialarrayii;
+// 3152
+class Solution {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        return null;
+    }
+}
