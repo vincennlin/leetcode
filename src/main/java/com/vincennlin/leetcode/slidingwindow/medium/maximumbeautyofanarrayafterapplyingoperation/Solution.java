e61aa3648163132d4ec5e1fd0a1266c5fbc436c3
vincennlin <vincentagwa@gmail.com>
2024-12-11 12:54:37 +0800

    Sliding Window - Maximum Beauty of an Array After Applying Operation

---
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/maximumbeautyofanarrayafterapplyingoperation/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.slidingwindow.medium.maximumbeautyofanarrayafterapplyingoperation;
+// 2779
+class Solution {
+    public int maximumBeauty(int[] nums, int k) {
+        return 0;
+    }
+}
