aa15c883762c0b74df7e3aaa1eb10682c78e4dc0
vincennlin <vincentagwa@gmail.com>
2024-12-12 10:05:54 +0800

    Heap - Take Gifts From the Richest Pile - my & ans

--- src/main/java/com/vincennlin/leetcode/heap/easy/takegiftsfromtherichestpile/Solution.java
+++ src/main/java/com/vincennlin/leetcode/heap/easy/takegiftsfromtherichestpile/Solution.java
@@ -1,7 +1,27 @@
 package com.vincennlin.leetcode.heap.easy.takegiftsfromtherichestpile;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 // 2558
 class Solution {
     public long pickGifts(int[] gifts, int k) {
-        return 0;
+        Queue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int gift : gifts) {
+            heap.add(gift);
+        }
+
+        for (int i = 0; i < k; i++) {
+            int maxGift = heap.poll();
+            heap.add((int) Math.sqrt(maxGift));
+        }
+
+        long result = 0;
+        for (int pile : heap) {
+            result += pile;
+        }
+
+        return result;
     }
 }
