dd4ff783e4075f070cd6bae79b51259f963c11b0
vincennlin <vincentagwa@gmail.com>
2024-12-13 14:11:52 +0800

    Heap - Find Score of an Array After Marking All Elements - ans

--- src/main/java/com/vincennlin/leetcode/heap/medium/findscoreofanarrayaftermarkingallelements/Solution.java
+++ src/main/java/com/vincennlin/leetcode/heap/medium/findscoreofanarrayaftermarkingallelements/Solution.java
@@ -1,7 +1,37 @@
 package com.vincennlin.leetcode.heap.medium.findscoreofanarrayaftermarkingallelements;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 // 2593
 class Solution {
     public long findScore(int[] nums) {
-        return 0;
+        boolean[] marked = new boolean[nums.length];
+        long ans = 0;
+
+        Queue<int[]> heap = new PriorityQueue<>((a, b) ->
+                a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+
+        for (int i = 0; i < nums.length; i++) {
+            heap.add(new int[]{nums[i], i});
+        }
+
+        while (!heap.isEmpty()) {
+            int[] element = heap.poll();
+            if (!marked[element[1]]) {
+                int num = element[0];
+                int index = element[1];
+                ans += num;
+                marked[index] = true;
+                if (index > 0) {
+                    marked[index - 1] = true;
+                }
+                if (index < nums.length - 1) {
+                    marked[index + 1] = true;
+                }
+            }
+        }
+
+        return ans;
     }
 }
