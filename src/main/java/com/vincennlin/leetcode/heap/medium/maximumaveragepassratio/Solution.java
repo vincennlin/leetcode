dcd2129fd28eeff88d48b95d49afc739cbb841a8
vincennlin <vincentagwa@gmail.com>
2024-12-15 19:44:56 +0800

    Heap - Maximum Average Pass Ratio

---
+++ src/main/java/com/vincennlin/leetcode/heap/medium/maximumaveragepassratio/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.heap.medium.maximumaveragepassratio;
+// 1792
+class Solution {
+    public double maxAverageRatio(int[][] classes, int extraStudents) {
+        return 0;
+    }
+}
