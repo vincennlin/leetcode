f7898a3c41b07b09ce248a1031fac5e0ad384eb0
vincennlin <vincentagwa@gmail.com>
2024-12-16 17:41:02 +0800

    Heap - Final Array State After K Multiplication Operations I

---
+++ src/main/java/com/vincennlin/leetcode/heap/easy/finalarraystateafterkmultiplicationoperationsi/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.heap.easy.finalarraystateafterkmultiplicationoperationsi;
+// 3264
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        return null;
+    }
+}
