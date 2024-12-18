a005552a3b583ca9b19278c33ab2832197795b2f
vincennlin <vincentagwa@gmail.com>
2024-12-18 13:27:01 +0800

    Stack - Final Prices With a Special Discount in a Shop - my

--- src/main/java/com/vincennlin/leetcode/stack/easy/finalpriceswithaspecialdiscountinashop/Solution.java
+++ src/main/java/com/vincennlin/leetcode/stack/easy/finalpriceswithaspecialdiscountinashop/Solution.java
@@ -2,6 +2,14 @@ package com.vincennlin.leetcode.stack.easy.finalpriceswithaspecialdiscountinasho
 // 1475
 class Solution {
     public int[] finalPrices(int[] prices) {
-        return null;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] <= prices[i]) {
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
     }
 }
