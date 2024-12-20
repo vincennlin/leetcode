dbb31aaeaab390ffcef2fa6729360855db8f7422
vincennlin <vincentagwa@gmail.com>
2024-12-20 17:15:21 +0800

    Binary Tree - Reverse Odd Levels of Binary Tree - my - wrong

--- src/main/java/com/vincennlin/leetcode/binarytree/dfs/medium/reverseoddlevelsofbinarytree/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytree/dfs/medium/reverseoddlevelsofbinarytree/Solution.java
@@ -2,6 +2,20 @@ package com.vincennlin.leetcode.binarytree.dfs.medium.reverseoddlevelsofbinarytr
 // 2415
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        return null;
+        dfs(root, false);
+        return root;
+    }
+
+    private void dfs(TreeNode root, boolean isNodeOddLevel) {
+        if (root == null) {
+            return;
+        }
+        if (!isNodeOddLevel && root.left != null && root.right != null) {
+            int temp = root.left.val;
+            root.left.val = root.right.val;
+            root.right.val = temp;
+        }
+        dfs(root.left, !isNodeOddLevel);
+        dfs(root.right, !isNodeOddLevel);
     }
 }
