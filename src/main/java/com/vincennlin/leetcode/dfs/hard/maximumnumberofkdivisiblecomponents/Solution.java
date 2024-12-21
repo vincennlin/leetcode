6211dc044560e7ac9381041c6fc549c2bd91c4f2
vincennlin <vincentagwa@gmail.com>
2024-12-21 09:30:58 +0800

    DFS - Maximum Number of K-Divisible Components

---
+++ src/main/java/com/vincennlin/leetcode/dfs/hard/maximumnumberofkdivisiblecomponents/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dfs.hard.maximumnumberofkdivisiblecomponents;
+// 2872
+class Solution {
+    public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+        return 0;
+    }
+}
