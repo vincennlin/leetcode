0c59b2afa88aaf1067488ba8f83bfc09edf25854
vincennlin <vincentagwa@gmail.com>
2024-12-22 13:44:01 +0800

    Monotonic Stack - Find Building Where Alice and Bob Can Meet

---
+++ src/main/java/com/vincennlin/leetcode/monotonicstack/hard/findbuildingwherealiceandbobcanmeet/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.monotonicstack.hard.findbuildingwherealiceandbobcanmeet;
+// 2940
+class Solution {
+    public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
+        return null;
+    }
+}
