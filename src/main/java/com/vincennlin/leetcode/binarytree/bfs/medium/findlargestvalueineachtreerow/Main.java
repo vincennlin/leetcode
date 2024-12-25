c97a50435e44f6a707f516f9378ba1b8101c23bf
vincennlin <vincentagwa@gmail.com>
2024-12-25 10:06:35 +0800

    Binary Tree - Find Largest Value in Each Tree Row

---
+++ src/main/java/com/vincennlin/leetcode/binarytree/bfs/medium/findlargestvalueineachtreerow/Main.java
@@ -0,0 +1,47 @@
+package com.vincennlin.leetcode.binarytree.bfs.medium.findlargestvalueineachtreerow;
+
+import java.util.List;
+
+/*
+Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+
+
+
+Example 1:
+
+
+Input: root = [1,3,2,5,3,null,9]
+Output: [1,3,9]
+Example 2:
+
+Input: root = [1,2,3]
+Output: [1,3]
+
+
+Constraints:
+
+The number of nodes in the tree will be in the range [0, 104].
+-231 <= Node.val <= 231 - 1
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(3);
+        root1.right = new TreeNode(2);
+        root1.left.left = new TreeNode(5);
+        root1.left.right = new TreeNode(3);
+        root1.right.right = new TreeNode(9);
+
+        List<Integer> result1 = solution.largestValues(root1);
+        System.out.println(result1); // [1,3,9]
+
+        TreeNode root2 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(3);
+
+        List<Integer> result2 = solution.largestValues(root2);
+        System.out.println(result2); // [1,3]
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/binarytree/bfs/medium/findlargestvalueineachtreerow/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.binarytree.bfs.medium.findlargestvalueineachtreerow;
+
+import java.util.List;
+
+// 515
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        return null;
+    }
+}
