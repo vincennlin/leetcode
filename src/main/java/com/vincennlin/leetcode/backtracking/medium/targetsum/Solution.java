dc075a97f79dc5175d27de18cb40f0a44756b319
vincennlin <vincentagwa@gmail.com>
2024-12-26 09:40:40 +0700

    Backtracking - Target Sum - my

--- src/main/java/com/vincennlin/leetcode/backtracking/medium/targetsum/Solution.java
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/targetsum/Solution.java
@@ -2,6 +2,16 @@ package com.vincennlin.leetcode.backtracking.medium.targetsum;
 // 494
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return 0;
+        return dfs(nums, target, 0, 0, true) + dfs(nums, target, 0, 0, false);
+    }
+
+    private int dfs(int[] nums, int target, int i, int val, boolean isPlus) {
+        val += isPlus ? nums[i] : -nums[i];
+
+        if (i == nums.length - 1) {
+            return val == target ? 1 : 0;
+        }
+
+        return dfs(nums, target, i + 1, val, isPlus) + dfs(nums, target, i + 1, val, !isPlus);
     }
 }
