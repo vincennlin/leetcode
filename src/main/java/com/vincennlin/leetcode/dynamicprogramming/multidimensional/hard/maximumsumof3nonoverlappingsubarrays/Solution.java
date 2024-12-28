824bcdf2f3673ea64709970f7fb56024c9dff67e
vincennlin <vincentagwa@gmail.com>
2024-12-28 16:36:06 +0700

    Multidimensional DP - Maximum Sum of 3 Non-Overlapping Subarrays

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/multidimensional/hard/maximumsumof3nonoverlappingsubarrays/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dynamicprogramming.multidimensional.hard.maximumsumof3nonoverlappingsubarrays;
+// 689
+class Solution {
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        return null;
+    }
+}
