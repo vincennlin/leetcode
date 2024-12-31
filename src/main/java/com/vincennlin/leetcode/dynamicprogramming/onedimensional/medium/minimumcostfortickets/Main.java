74ca8bfa971ab5daa92b82ef84b6e823bf89d54f
vincennlin <vincentagwa@gmail.com>
2024-12-31 11:26:46 +0800

    One Dimensional DP - Minimum Cost For Tickets - my - wrong

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/medium/minimumcostfortickets/Main.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/medium/minimumcostfortickets/Main.java
@@ -48,5 +48,8 @@ public class Main {
         System.out.println(solution.mincostTickets(new int[] {1, 4, 6, 7, 8, 20}, new int[] {2, 7, 15})); // 11
 
         System.out.println(solution.mincostTickets(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31}, new int[] {2, 7, 15})); // 17
+
+        // days = [1,4,6,7,8,20], costs = [7,2,15]
+        System.out.println(solution.mincostTickets(new int[] {1, 4, 6, 7, 8, 20}, new int[] {7, 2, 15})); // 6
     }
 }
--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/medium/minimumcostfortickets/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/medium/minimumcostfortickets/Solution.java
@@ -2,6 +2,28 @@ package com.vincennlin.leetcode.dynamicprogramming.onedimensional.medium.minimum
 // 983
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        return 0;
+        int maxDay = days[days.length - 1];
+        int[] dp = new int[maxDay + 1];
+        dp[0] = 0;
+
+        for (int i = 0, day = 1; i < days.length && day <= maxDay; day++) {
+            if (days[i] == day) {
+                dp[day] = dp[day - 1] + costs[0];
+
+                if (day >= 7) {
+                    dp[day] = Math.min(dp[day], dp[day - 7] + costs[1]);
+                }
+
+                if (day >= 30) {
+                    dp[day] = Math.min(dp[day], dp[day - 30] + costs[2]);
+                }
+
+                i++;
+            } else {
+                dp[day] = dp[day - 1];
+            }
+        }
+
+        return dp[maxDay];
     }
 }
