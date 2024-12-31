8e62d918b348dd46018ea19446fc89716060882c
vincennlin <vincentagwa@gmail.com>
2024-12-31 11:26:57 +0800

    One Dimensional DP - Minimum Cost For Tickets - ans

--- src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/medium/minimumcostfortickets/Solution.java
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/medium/minimumcostfortickets/Solution.java
@@ -4,23 +4,16 @@ class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int maxDay = days[days.length - 1];
         int[] dp = new int[maxDay + 1];
-        dp[0] = 0;
 
-        for (int i = 0, day = 1; i < days.length && day <= maxDay; day++) {
-            if (days[i] == day) {
-                dp[day] = dp[day - 1] + costs[0];
-
-                if (day >= 7) {
-                    dp[day] = Math.min(dp[day], dp[day - 7] + costs[1]);
-                }
-
-                if (day >= 30) {
-                    dp[day] = Math.min(dp[day], dp[day - 30] + costs[2]);
-                }
+        for (int i = 0, day = 1; day <= maxDay; day++) {
+            if (day < days[i]) {
+                dp[day] = dp[day - 1];
+            } else {
+                dp[day] = Math.min(dp[day - 1] + costs[0],
+                        Math.min(dp[Math.max(0, day - 7)] + costs[1],
+                                dp[Math.max(0, day - 30)] + costs[2]));
 
                 i++;
-            } else {
-                dp[day] = dp[day - 1];
             }
         }
 
