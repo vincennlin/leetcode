f3a53d70b93eb8df2f29c98b2ef12ed8e9ced3bf
vincennlin <vincentagwa@gmail.com>
2024-12-31 10:51:12 +0800

    One Dimensional DP - Minimum Cost For Tickets

---
+++ src/main/java/com/vincennlin/leetcode/dynamicprogramming/onedimensional/medium/minimumcostfortickets/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.dynamicprogramming.onedimensional.medium.minimumcostfortickets;
+// 983
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        return 0;
+    }
+}
