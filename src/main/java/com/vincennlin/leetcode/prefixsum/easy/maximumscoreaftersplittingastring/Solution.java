cfea134a57797ec471c3da5444b276dbdba87377
vincennlin <vincentagwa@gmail.com>
2025-01-01 11:23:48 +0800

    Prefix Sum - Maximum Score After Splitting a String - my - wrong

--- src/main/java/com/vincennlin/leetcode/prefixsum/easy/maximumscoreaftersplittingastring/Solution.java
+++ src/main/java/com/vincennlin/leetcode/prefixsum/easy/maximumscoreaftersplittingastring/Solution.java
@@ -2,6 +2,20 @@ package com.vincennlin.leetcode.prefixsum.easy.maximumscoreaftersplittingastring
 // 1422
 class Solution {
     public int maxScore(String s) {
-        return 0;
+        int[] prefixOnes = new int[s.length()];
+        prefixOnes[0] = s.charAt(0) == 1 ? 1 : 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            prefixOnes[i] = prefixOnes[i - 1] + (s.charAt(i) == '1' ? 1 : 0);
+        }
+
+        int ans = 0, zeroCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            zeroCount += (s.charAt(i) == '0' ? 1 : 0);
+            ans = Math.max(ans, zeroCount + (prefixOnes[prefixOnes.length - 1] - prefixOnes[i]));
+        }
+
+        return ans;
     }
 }
