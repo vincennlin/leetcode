eb681199780020bdecbcb9dd24858d23847348ef
vincennlin <vincentagwa@gmail.com>
2025-01-02 11:49:34 +0800

    Prefix Sum - Count Vowel Strings in Ranges

---
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/countvowelstringsinranges/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.prefixsum.medium.countvowelstringsinranges;
+// 2559
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        return new int[0];
+    }
+}
