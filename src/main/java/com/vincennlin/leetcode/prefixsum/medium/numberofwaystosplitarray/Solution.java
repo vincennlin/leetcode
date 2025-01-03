2a1de904555edd45f9a0bef4063aa3d4b2785dd7
vincennlin <vincentagwa@gmail.com>
2025-01-03 11:39:41 +0800

    Prefix Sum - Number of Ways to Split Array - my

--- src/main/java/com/vincennlin/leetcode/prefixsum/medium/numberofwaystosplitarray/Solution.java
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/numberofwaystosplitarray/Solution.java
@@ -2,6 +2,22 @@ package com.vincennlin.leetcode.prefixsum.medium.numberofwaystosplitarray;
 // 2270
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        return 0;
+        long[] prefixSum = new long[nums.length];
+        prefixSum[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        long sum = prefixSum[prefixSum.length - 1];
+        int result = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (prefixSum[i] >= sum - prefixSum[i]) {
+                result++;
+            }
+        }
+
+        return result;
     }
 }
