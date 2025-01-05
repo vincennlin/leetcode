eb118b43471f7ed5a70bb47a54a163bf3b301349
vincennlin <vincentagwa@gmail.com>
2025-01-05 13:41:05 +0800

    Prefix Sum - Shifting Letters II - ans

--- src/main/java/com/vincennlin/leetcode/prefixsum/medium/shiftinglettersii/Solution.java
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/shiftinglettersii/Solution.java
@@ -2,20 +2,27 @@ package com.vincennlin.leetcode.prefixsum.medium.shiftinglettersii;
 // 2381
 class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
-        int[] shiftedSums = new int[s.length()];
+        int n = s.length();
+        int[] diffArray = new int[n + 1]; // 差分陣列
 
         for (int[] shift : shifts) {
-            for (int i = shift[0]; i <= shift[1]; i++) {
-                shiftedSums[i] += shift[2] == 1 ? 1 : -1;
-            }
+            int start = shift[0];
+            int end = shift[1];
+            int direction = shift[2];
+
+            diffArray[start] += (direction == 1 ? 1 : -1);
+            diffArray[end + 1] -= (direction == 1 ? 1 : -1);
+        }
+
+        for (int i = 1; i < n; i++) {
+            diffArray[i] += diffArray[i - 1];
         }
 
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < shiftedSums.length; i++) {
-            shiftedSums[i] = (shiftedSums[i] % 26 + 26);
-            int shiftedIndex = (s.charAt(i) - 'a' + shiftedSums[i]) % 26;
-            sb.append((char) ('a' + shiftedIndex));
+        for (int i = 0; i < n; i++) {
+            int shift = (diffArray[i] % 26 + 26) % 26;
+            sb.append((char) ('a' + (s.charAt(i) - 'a' + shift) % 26));
         }
 
         return sb.toString();
