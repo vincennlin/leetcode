9314c36678742a468cdc92977354eb4752495c2b
vincennlin <vincentagwa@gmail.com>
2025-01-05 12:59:49 +0800

    Prefix Sum - Shifting Letters II

---
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/shiftinglettersii/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.prefixsum.medium.shiftinglettersii;
+// 2381
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        return null;
+    }
+}
