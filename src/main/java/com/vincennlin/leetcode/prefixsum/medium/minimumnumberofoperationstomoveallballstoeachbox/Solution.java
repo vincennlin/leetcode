92efdd79fb771e1f2764d18c234e3cea7da6cf92
vincennlin <vincentagwa@gmail.com>
2025-01-07 00:59:55 +0800

    Prefix Sum - Minimum Number of Operations to Move All Balls to Each Box

---
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/minimumnumberofoperationstomoveallballstoeachbox/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.prefixsum.medium.minimumnumberofoperationstomoveallballstoeachbox;
+// 1769
+class Solution {
+    public int[] minOperations(String boxes) {
+        return null;
+    }
+}
