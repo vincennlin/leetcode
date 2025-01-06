5945b0815c63eb858980da7564a0cfe74688624c
vincennlin <vincentagwa@gmail.com>
2025-01-07 01:25:32 +0800

    Prefix Sum - Minimum Number of Operations to Move All Balls to Each Box - my & ans

--- src/main/java/com/vincennlin/leetcode/prefixsum/medium/minimumnumberofoperationstomoveallballstoeachbox/Solution.java
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/minimumnumberofoperationstomoveallballstoeachbox/Solution.java
@@ -2,6 +2,33 @@ package com.vincennlin.leetcode.prefixsum.medium.minimumnumberofoperationstomove
 // 1769
 class Solution {
     public int[] minOperations(String boxes) {
-        return null;
+        int n = boxes.length();
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int ballCount = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (boxes.charAt(i - 1) == '1') {
+                ballCount++;
+            }
+            left[i] = left[i - 1] + ballCount;
+        }
+
+        ballCount = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (boxes.charAt(i + 1) == '1') {
+                ballCount++;
+            }
+            right[i] = right[i + 1] + ballCount;
+        }
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = left[i] + right[i];
+        }
+
+        return result;
     }
 }
