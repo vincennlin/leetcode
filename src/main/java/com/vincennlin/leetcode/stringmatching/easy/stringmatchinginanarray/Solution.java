35c895c7ef12c755ec83f5af771b005a0270bc6a
vincennlin <vincentagwa@gmail.com>
2025-01-07 13:10:17 +0800

    String Matching - String Matching in an Array

---
+++ src/main/java/com/vincennlin/leetcode/stringmatching/easy/stringmatchinginanarray/Solution.java
@@ -0,0 +1,10 @@
+package com.vincennlin.leetcode.stringmatching.easy.stringmatchinginanarray;
+
+import java.util.List;
+
+// 1408
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        return null;
+    }
+}
