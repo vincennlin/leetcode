b881e0dc6f75de480ee94a5ff9ca72db6f6e23d8
vincennlin <vincentagwa@gmail.com>
2025-01-08 23:07:06 +0800

    Design - Insert Delete GetRandom O(1)

---
+++ src/main/java/com/vincennlin/leetcode/design/medium/insertdeletegetrandomo1/RandomizedSet.java
@@ -0,0 +1,21 @@
+package com.vincennlin.leetcode.design.medium.insertdeletegetrandomo1;
+// 380
+class RandomizedSet {
+
+    public RandomizedSet() {
+
+    }
+
+    public boolean insert(int val) {
+        return false;
+    }
+
+    public boolean remove(int val) {
+        return false;
+    }
+
+    public int getRandom() {
+        return 0;
+    }
+}
+
