e1b08e1bcacec9e46655732a9af6b8f3709b19b0
vincennlin <vincentagwa@gmail.com>
2025-01-08 13:14:45 +0800

    String Matching - Count Prefix and Suffix Pairs I - my & ans

--- src/main/java/com/vincennlin/leetcode/stringmatching/easy/countprefixandsuffixpairsi/Solution.java
+++ src/main/java/com/vincennlin/leetcode/stringmatching/easy/countprefixandsuffixpairsi/Solution.java
@@ -2,6 +2,21 @@ package com.vincennlin.leetcode.stringmatching.easy.countprefixandsuffixpairsi;
 // 3042
 class Solution {
     public int countPrefixSuffixPairs(String[] words) {
-        return 0;
+        int n = words.length;
+        int ans = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean isPrefixAndSuffix(String str1, String str2) {
+        return str2.startsWith(str1) && str2.endsWith(str1);
     }
 }
