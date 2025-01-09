5e9953aa0d2cbb36dad294cfec51e116214b41a7
vincennlin <vincentagwa@gmail.com>
2025-01-09 13:12:12 +0800

    String Matching - Counting Words With a Given Prefix

---
+++ src/main/java/com/vincennlin/leetcode/string/easy/countingwordswithagivenprefix/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.string.easy.countingwordswithagivenprefix;
+// 2185
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        return 0;
+    }
+}
