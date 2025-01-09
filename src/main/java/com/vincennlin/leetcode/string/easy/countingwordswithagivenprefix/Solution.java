e840ea790654ebddc06e50ce45ce15264ec018ba
vincennlin <vincentagwa@gmail.com>
2025-01-09 13:24:14 +0800

    String Matching - Counting Words With a Given Prefix - Trie

--- src/main/java/com/vincennlin/leetcode/string/easy/countingwordswithagivenprefix/Solution.java
+++ src/main/java/com/vincennlin/leetcode/string/easy/countingwordswithagivenprefix/Solution.java
@@ -1,7 +1,59 @@
 package com.vincennlin.leetcode.string.easy.countingwordswithagivenprefix;
 // 2185
+// Trie
 class Solution {
+    class Node {
+        Node[] children;
+        int count;
+
+        Node() {
+            this.children = new Node[26];
+            this.count = 0;
+        }
+    }
+
     public int prefixCount(String[] words, String pref) {
-        return 0;
+        Node root = new Node();
+
+        for (String word : words) {
+            insert(root, word);
+        }
+
+        return search(root, pref);
+    }
+
+    private void insert(Node root, String word) {
+        for (char c : word.toCharArray()) {
+            if (root.children[c - 'a'] == null) {
+                root.children[c - 'a'] = new Node();
+            }
+            root = root.children[c - 'a'];
+            root.count++;
+        }
+    }
+
+    private int search(Node root, String prefix) {
+        for (char c : prefix.toCharArray()) {
+            if (root.children[c - 'a'] == null) {
+                return 0;
+            }
+            root = root.children[c - 'a'];
+        }
+        return root.count;
     }
 }
+
+// Brute Force
+//class Solution {
+//    public int prefixCount(String[] words, String pref) {
+//        int ans = 0;
+//
+//        for (String word : words) {
+//            if (word.startsWith(pref)) {
+//                ans++;
+//            }
+//        }
+//
+//        return ans;
+//    }
+//}
