942d664a142189c01d943bbd8929e5782980748d
vincennlin <vincentagwa@gmail.com>
2025-01-11 12:36:17 +0800

    String - Construct K Palindrome Strings - my

--- src/main/java/com/vincennlin/leetcode/string/medium/constructkpalindromestrings/Solution.java
+++ src/main/java/com/vincennlin/leetcode/string/medium/constructkpalindromestrings/Solution.java
@@ -1,7 +1,27 @@
 package com.vincennlin.leetcode.string.medium.constructkpalindromestrings;
+
 // 1400
 class Solution {
     public boolean canConstruct(String s, int k) {
-        return false;
+        if (s.length() < k) {
+            return false;
+        }
+        if (s.length() == k) {
+            return true;
+        }
+
+        boolean[] isOdd = new boolean[26];
+        int oddCount = 0;
+
+        for (char c : s.toCharArray()) {
+            if (isOdd[c - 'a']) {
+                oddCount--;
+            } else {
+                oddCount++;
+            }
+            isOdd[c - 'a'] = !isOdd[c - 'a'];
+        }
+
+        return oddCount <= k;
     }
 }
