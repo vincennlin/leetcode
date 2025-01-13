a2366529907e18af364e5a099218fcca0bc8e29d
vincennlin <vincentagwa@gmail.com>
2025-01-13 14:32:58 +0800

    String - Minimum Length of String After Operations - ans

--- src/main/java/com/vincennlin/leetcode/string/medium/minimumlengthofstringafteroperations/Solution.java
+++ src/main/java/com/vincennlin/leetcode/string/medium/minimumlengthofstringafteroperations/Solution.java
@@ -2,6 +2,25 @@ package com.vincennlin.leetcode.string.medium.minimumlengthofstringafteroperatio
 // 3223
 class Solution {
     public int minimumLength(String s) {
-        return 0;
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int ans = 0;
+
+        for (int f : freq) {
+            if (f == 0) {
+                continue;
+            }
+            if (f % 2 == 1) {
+                ans += 1;
+            } else {
+                ans += 2;
+            }
+        }
+
+        return ans;
     }
 }
