b9b0e25828aca7a637995e6fe7bcea8d7b0f02d5
vincennlin <vincentagwa@gmail.com>
2025-01-15 02:41:30 +0800

    Array - Find the Prefix Common Array of Two Arrays - my & ans

--- src/main/java/com/vincennlin/leetcode/array/medium/findtheprefixcommonarrayoftwoarrays/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/medium/findtheprefixcommonarrayoftwoarrays/Solution.java
@@ -2,6 +2,22 @@ package com.vincennlin.leetcode.array.medium.findtheprefixcommonarrayoftwoarrays
 // 2657
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        return null;
+        int n = A.length;
+        int[] freq = new int[n];
+
+        int[] result = new int[n];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (++freq[A[i] - 1] > 1) {
+                ans++;
+            }
+            if (++freq[B[i] - 1] > 1) {
+                ans++;
+            }
+            result[i] = ans;
+        }
+
+        return result;
     }
 }
