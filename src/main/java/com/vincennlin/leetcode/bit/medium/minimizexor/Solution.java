e032ed1b715533c279e868f40d381c370687bfe0
vincennlin <vincentagwa@gmail.com>
2025-01-15 16:29:07 +0800

    Bit - Minimize XOR - ans

--- src/main/java/com/vincennlin/leetcode/bit/medium/minimizexor/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/minimizexor/Solution.java
@@ -1,7 +1,29 @@
 package com.vincennlin.leetcode.bit.medium.minimizexor;
 // 2429
-class Solution {
+public class Solution {
     public int minimizeXor(int num1, int num2) {
-        return 0;
+        int result = 0;
+
+        int targetSetBitsCount = Integer.bitCount(num2);
+        int setBitsCount = 0;
+        int currentBit = 31;
+
+        while (setBitsCount < targetSetBitsCount) {
+            if (isSet(num1, currentBit) || (targetSetBitsCount - setBitsCount > currentBit)) {
+                result = setBit(result, currentBit);
+                setBitsCount++;
+            }
+            currentBit--;
+        }
+
+        return result;
+    }
+
+    private boolean isSet(int x, int bit) {
+        return (x & (1 << bit)) != 0;
+    }
+
+    private int setBit(int x, int bit) {
+        return x | (1 << bit);
     }
 }
