85495faa45c21601f3cabb081e0cb95a5e612999
vincennlin <vincentagwa@gmail.com>
2025-01-16 14:56:24 +0800

    Bit - Bitwise XOR of All Pairings - my & ans

--- src/main/java/com/vincennlin/leetcode/bit/medium/bitwisexorofallpairings/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bit/medium/bitwisexorofallpairings/Solution.java
@@ -2,6 +2,24 @@ package com.vincennlin.leetcode.bit.medium.bitwisexorofallpairings;
 // 2425
 class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
-        return 0;
+        int m = nums1.length, n = nums2.length;
+
+        if (m % 2 == 0 && n % 2 == 0) {
+            return 0;
+        } else if (m % 2 == 0) {
+            return getArrayXor(nums1);
+        } else if (n % 2 == 0) {
+            return getArrayXor(nums2);
+        } else {
+            return getArrayXor(nums1) ^ getArrayXor(nums2);
+        }
+    }
+
+    private int getArrayXor(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
     }
 }
