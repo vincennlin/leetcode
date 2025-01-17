4ee8211ae6450de4b5643dcf06a0dab75bf80cea
vincennlin <vincentagwa@gmail.com>
2025-01-17 09:02:07 +0800

    Bit - Neighboring Bitwise XOR

---
+++ src/main/java/com/vincennlin/leetcode/bit/medium/neighboringbitwisexor/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.bit.medium.neighboringbitwisexor;
+// 2683
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        return false;
+    }
+}
