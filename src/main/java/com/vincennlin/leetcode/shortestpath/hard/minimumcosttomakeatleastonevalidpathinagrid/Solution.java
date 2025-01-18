0beda915736bf3a22be81a2d18b974128f23f8a1
vincennlin <vincentagwa@gmail.com>
2025-01-18 15:09:40 +0800

    Shortest Path - Minimum Cost to Make at Least One Valid Path in a Grid

---
+++ src/main/java/com/vincennlin/leetcode/shortestpath/hard/minimumcosttomakeatleastonevalidpathinagrid/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.shortestpath.hard.minimumcosttomakeatleastonevalidpathinagrid;
+// 1368
+class Solution {
+    public int minCost(int[][] grid) {
+        return 0;
+    }
+}
