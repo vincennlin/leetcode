90ec5fe900a56c60ef2c16e4e40271dc759d5c21
vincennlin <vincentagwa@gmail.com>
2025-01-20 16:13:22 +0800

    Hash Table - First Completely Painted Row or Column

---
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/firstcompletelypaintedroworcolumn/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.hashtable.medium.firstcompletelypaintedroworcolumn;
+// 2661
+class Solution {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        return 0;
+    }
+}
