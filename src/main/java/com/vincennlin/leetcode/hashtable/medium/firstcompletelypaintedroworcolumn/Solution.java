fe5ef09a760d346ac9ef0578b3b694b09e73975c
vincennlin <vincentagwa@gmail.com>
2025-01-20 16:23:04 +0800

    Hash Table - First Completely Painted Row or Column - my & ans

--- src/main/java/com/vincennlin/leetcode/hashtable/medium/firstcompletelypaintedroworcolumn/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/firstcompletelypaintedroworcolumn/Solution.java
@@ -1,17 +1,13 @@
 package com.vincennlin.leetcode.hashtable.medium.firstcompletelypaintedroworcolumn;
-
-import java.util.HashMap;
-import java.util.Map;
-
 // 2661
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
         int m = mat.length, n = mat[0].length;
-        Map<Integer, int[]> map = new HashMap<>();
+        int[][] map = new int[m * n][2];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                map.put(mat[i][j], new int[]{i, j});
+                map[mat[i][j] - 1] = new int[]{i, j};
             }
         }
 
@@ -19,7 +15,7 @@ class Solution {
 
         for (int i = 0; i < arr.length; i++) {
             int num = arr[i];
-            int row = map.get(num)[0], col = map.get(num)[1];
+            int row = map[num - 1][0], col = map[num - 1][1];
             if (++rows[row] == n || ++cols[col] == m) {
                 return i;
             }
