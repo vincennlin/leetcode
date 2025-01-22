400a6f5c8e2e5acc42f030eb2a9b41297166017b
vincennlin <vincentagwa@gmail.com>
2025-01-22 14:58:28 +0800

    BFS - Map of Highest Peak - my & ans

--- src/main/java/com/vincennlin/leetcode/bfs/medium/mapofhighestpeak/Solution.java
+++ src/main/java/com/vincennlin/leetcode/bfs/medium/mapofhighestpeak/Solution.java
@@ -1,7 +1,49 @@
 package com.vincennlin.leetcode.bfs.medium.mapofhighestpeak;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 // 1765
 class Solution {
+    int[][] directions = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
     public int[][] highestPeak(int[][] isWater) {
-        return null;
+        int m = isWater.length, n = isWater[0].length;
+        boolean[][] isVisited = new boolean[m][n];
+        int[][] map = new int[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isWater[i][j] == 1) {
+                    queue.add(new int[]{i, j, 0});
+                    isVisited[i][j] = true;
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int[] element = queue.poll();
+            int i = element[0];
+            int j = element[1];
+            int height = element[2];
+
+            map[i][j] = height;
+
+            for (int[] direction : directions) {
+                if (isValidCell(m, n, i + direction[0], j + direction[1])
+                        && !isVisited[i + direction[0]][j + direction[1]]) {
+                    queue.add(new int[]{i + direction[0], j + direction[1], height + 1});
+                    isVisited[i + direction[0]][j + direction[1]] = true;
+                }
+            }
+
+        }
+
+        return map;
+    }
+
+    private boolean isValidCell(int m, int n, int i, int j) {
+        return i >= 0 && i < m && j >= 0 && j < n;
     }
 }
