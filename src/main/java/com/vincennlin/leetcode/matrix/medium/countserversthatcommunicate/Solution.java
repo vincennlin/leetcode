c2f073bad942a1427dd4e5b54b3652cd905aab68
vincennlin <vincentagwa@gmail.com>
2025-01-23 13:46:30 +0800

    Matrix - Count Servers that Communicate - my & ans

--- src/main/java/com/vincennlin/leetcode/matrix/medium/countserversthatcommunicate/Solution.java
+++ src/main/java/com/vincennlin/leetcode/matrix/medium/countserversthatcommunicate/Solution.java
@@ -2,6 +2,29 @@ package com.vincennlin.leetcode.matrix.medium.countserversthatcommunicate;
 // 1267
 class Solution {
     public int countServers(int[][] grid) {
-        return 0;
+        int m = grid.length, n = grid[0].length;
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    rows[i]++;
+                    cols[j]++;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && (rows[i] > 1 || cols[j] > 1)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
