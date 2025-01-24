b72da3c53f116aa04b690e49c98119758548c4b9
vincennlin <vincentagwa@gmail.com>
2025-01-24 08:14:14 +0800

    Graph - Find Eventual Safe States

---
+++ src/main/java/com/vincennlin/leetcode/graph/dfs/medium/findeventualsafestates/Solution.java
@@ -0,0 +1,9 @@
+package com.vincennlin.leetcode.graph.dfs.medium.findeventualsafestates;
+
+import java.util.List;
+// 802
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        return null;
+    }
+}
