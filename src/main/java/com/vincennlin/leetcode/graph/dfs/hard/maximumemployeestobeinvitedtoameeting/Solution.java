0639f0ac3333541e42e1399f63cd272a27222558
vincennlin <vincentagwa@gmail.com>
2025-01-26 14:35:11 +0800

    Graph - Maximum Employees to Be Invited to a Meeting

---
+++ src/main/java/com/vincennlin/leetcode/graph/dfs/hard/maximumemployeestobeinvitedtoameeting/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.graph.dfs.hard.maximumemployeestobeinvitedtoameeting;
+// 2127
+class Solution {
+    public int maximumInvitations(int[] favorite) {
+        return 0;
+    }
+}
