f749be68d1468d86444e9df9321bb01225430af7
vincennlin <vincentagwa@gmail.com>
2025-01-27 14:15:31 +0800

    Graph - Course Schedule IV

---
+++ src/main/java/com/vincennlin/leetcode/graph/bfs/medium/coursescheduleiv/Solution.java
@@ -0,0 +1,9 @@
+package com.vincennlin.leetcode.graph.bfs.medium.coursescheduleiv;
+
+import java.util.List;
+//  1462
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        return null;
+    }
+}
