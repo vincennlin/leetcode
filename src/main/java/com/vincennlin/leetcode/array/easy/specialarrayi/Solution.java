ea0aaf8fbd3c08d9ac8b07510c53b562d92b18d9
vincennlin <vincentagwa@gmail.com>
2025-02-01 16:14:59 +0800

    Array - Special Array I - my & ans

--- src/main/java/com/vincennlin/leetcode/array/easy/specialarrayi/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/easy/specialarrayi/Solution.java
@@ -2,6 +2,12 @@ package com.vincennlin.leetcode.array.easy.specialarrayi;
 // 3151
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        return false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] % 2 == nums[i - 1] % 2) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
