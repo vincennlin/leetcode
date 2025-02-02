91833abe8e88cd12e5264d4529ab0036b435caaa
vincennlin <vincentagwa@gmail.com>
2025-02-02 15:12:15 +0800

    Array - Check if Array Is Sorted and Rotated

---
+++ src/main/java/com/vincennlin/leetcode/array/easy/checkifarrayissortedandrotated/Main.java
@@ -0,0 +1,45 @@
+package com.vincennlin.leetcode.array.easy.checkifarrayissortedandrotated;
+/*
+Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+
+
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+Example 2:
+
+Input: nums = [2,1,3,4]
+Output: false
+Explanation: There is no sorted array once rotated that can make nums.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: true
+Explanation: [1,2,3] is the original sorted array.
+You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.check(new int[] {3, 4, 5, 1, 2})); // true
+
+        System.out.println(solution.check(new int[] {2, 1, 3, 4})); // false
+
+        System.out.println(solution.check(new int[] {1, 2, 3})); // true
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/array/easy/checkifarrayissortedandrotated/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.array.easy.checkifarrayissortedandrotated;
+// 1752
+class Solution {
+    public boolean check(int[] nums) {
+        return false;
+    }
+}
