361aa16ce5270847c46a67bd4acf4248894bf442
vincennlin <vincentagwa@gmail.com>
2025-02-02 15:15:49 +0800

    Array - Check if Array Is Sorted and Rotated - my & ans

--- src/main/java/com/vincennlin/leetcode/array/easy/checkifarrayissortedandrotated/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/easy/checkifarrayissortedandrotated/Solution.java
@@ -2,6 +2,20 @@ package com.vincennlin.leetcode.array.easy.checkifarrayissortedandrotated;
 // 1752
 class Solution {
     public boolean check(int[] nums) {
-        return false;
+        if (nums.length <= 2) {
+            return true;
+        }
+
+        int rotatePoint = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > nums[i]) {
+                if (++rotatePoint > 1) {
+                    return false;
+                }
+            }
+        }
+
+        return rotatePoint == 0 || nums[0] >= nums[nums.length - 1];
     }
 }
