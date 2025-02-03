2125c62378ec25b9f9090a156f74b9032a07872d
vincennlin <vincentagwa@gmail.com>
2025-02-03 21:58:22 +0800

    Array - Longest Strictly Increasing or Strictly Decreasing Subarray

---
+++ src/main/java/com/vincennlin/leetcode/array/easy/longeststrictlyincreasingorstrictlydecreasingsubarray/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.array.easy.longeststrictlyincreasingorstrictlydecreasingsubarray;
+// 3105
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        return 0;
+    }
+}
