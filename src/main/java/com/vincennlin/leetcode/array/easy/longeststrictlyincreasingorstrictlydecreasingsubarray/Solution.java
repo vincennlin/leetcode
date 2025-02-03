bed8fe34f0d6bff3e377f045907faa146ccd408e
vincennlin <vincentagwa@gmail.com>
2025-02-03 22:05:24 +0800

    Array - Longest Strictly Increasing or Strictly Decreasing Subarray - ans

--- src/main/java/com/vincennlin/leetcode/array/easy/longeststrictlyincreasingorstrictlydecreasingsubarray/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/easy/longeststrictlyincreasingorstrictlydecreasingsubarray/Solution.java
@@ -2,6 +2,25 @@ package com.vincennlin.leetcode.array.easy.longeststrictlyincreasingorstrictlyde
 // 3105
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
-        return 0;
+        int incLength = 1;
+        int decLength = 1;
+        int maxLength = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                incLength++;
+                decLength = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                decLength++;
+                incLength = 1;
+            } else {
+                incLength = 1;
+                decLength = 1;
+            }
+
+            maxLength = Math.max(maxLength, Math.max(incLength, decLength));
+        }
+
+        return maxLength;
     }
 }
