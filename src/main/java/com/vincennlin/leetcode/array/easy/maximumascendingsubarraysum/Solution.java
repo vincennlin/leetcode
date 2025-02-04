d846e1d1f36c02daae7d3db8f4a8752e8e4e7864
vincennlin <vincentagwa@gmail.com>
2025-02-04 13:07:44 +0800

    Array - Maximum Ascending Subarray Sum - my & ans

--- src/main/java/com/vincennlin/leetcode/array/easy/maximumascendingsubarraysum/Solution.java
+++ src/main/java/com/vincennlin/leetcode/array/easy/maximumascendingsubarraysum/Solution.java
@@ -2,6 +2,18 @@ package com.vincennlin.leetcode.array.easy.maximumascendingsubarraysum;
 // 1800
 class Solution {
     public int maxAscendingSum(int[] nums) {
-        return 0;
+        int maxSum = nums[0];
+        int curSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                curSum += nums[i];
+            } else {
+                curSum = nums[i];
+            }
+            maxSum = Math.max(maxSum, curSum);
+        }
+
+        return maxSum;
     }
 }
