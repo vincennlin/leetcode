5ff7d1d6f0391256d5703697fb454f29357f93eb
vincennlin <vincentagwa@gmail.com>
2025-02-05 13:13:03 +0800

    String - Check if One String Swap Can Make Strings Equal - my & ans

--- src/main/java/com/vincennlin/leetcode/string/easy/checkifonestringswapcanmakestringsequal/Solution.java
+++ src/main/java/com/vincennlin/leetcode/string/easy/checkifonestringswapcanmakestringsequal/Solution.java
@@ -2,6 +2,27 @@ package com.vincennlin.leetcode.string.easy.checkifonestringswapcanmakestringseq
 // 1790
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        return false;
+        int[] freq = new int[26];
+        int diff = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+            if (c1 != c2) {
+                if (++diff > 2) {
+                    return false;
+                }
+                freq[c1 - 'a']++;
+                freq[c2 - 'a']--;
+            }
+        }
+
+        for (int f : freq) {
+            if (f != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
