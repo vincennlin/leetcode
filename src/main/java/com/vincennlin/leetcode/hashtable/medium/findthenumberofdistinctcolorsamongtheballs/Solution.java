cbb50be125595d72b382bede4831ca5cb1e602dc
vincennlin <vincentagwa@gmail.com>
2025-02-07 13:25:34 +0800

    Hash Table - Find the Number of Distinct Colors Among the Balls

---
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/findthenumberofdistinctcolorsamongtheballs/Solution.java
@@ -0,0 +1,8 @@
+package com.vincennlin.leetcode.hashtable.medium.findthenumberofdistinctcolorsamongtheballs;
+// 3160
+class Solution {
+    public int[] queryResults(int limit, int[][] queries) {
+        int[] result = new int[]{};
+        return result;
+    }
+}
