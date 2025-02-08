50ce00caf511293e9146afa2f36a6e2887aa19fe
vincennlin <vincentagwa@gmail.com>
2025-02-08 15:19:42 +0800

    Design - Design a Number Container System

---
+++ src/main/java/com/vincennlin/leetcode/design/medium/designanumbercontainersystem/NumberContainers.java
@@ -0,0 +1,16 @@
+package com.vincennlin.leetcode.design.medium.designanumbercontainersystem;
+// 2349
+class NumberContainers {
+
+    public NumberContainers() {
+
+    }
+
+    public void change(int index, int number) {
+
+    }
+
+    public int find(int number) {
+        return 0;
+    }
+}
