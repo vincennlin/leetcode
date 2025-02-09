10b870dd0df8bb5901fb9a0fa33cd84a6e9ce7f0
vincennlin <vincentagwa@gmail.com>
2025-02-09 12:42:54 +0800

    Hash Table - Count Number of Bad Pairs - my & ans

--- src/main/java/com/vincennlin/leetcode/hashtable/medium/countnumberofbadpairs/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/medium/countnumberofbadpairs/Solution.java
@@ -1,7 +1,25 @@
 package com.vincennlin.leetcode.hashtable.medium.countnumberofbadpairs;
+
+import java.util.HashMap;
+import java.util.Map;
+
 // 2364
 class Solution {
     public long countBadPairs(int[] nums) {
-        return 0L;
+        Map<Integer, Integer> numCountMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = i - nums[i];
+            numCountMap.put(num, numCountMap.getOrDefault(num, 0) + 1);
+        }
+
+        long result = (long) nums.length * (nums.length - 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = i - nums[i];
+            result -= (numCountMap.get(num) - 1);
+        }
+
+        return result / 2;
     }
 }
