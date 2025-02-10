7f974b0c0496ea489895e5a6001f0d265e3c6877
vincennlin <vincentagwa@gmail.com>
2025-02-10 14:12:46 +0800

    Stack - Clear Digits - my & ans

--- src/main/java/com/vincennlin/leetcode/stack/easy/cleardigits/Solution.java
+++ src/main/java/com/vincennlin/leetcode/stack/easy/cleardigits/Solution.java
@@ -1,7 +1,27 @@
 package com.vincennlin.leetcode.stack.easy.cleardigits;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
 // 3174
 class Solution {
     public String clearDigits(String s) {
-        return null;
+        Deque<Character> stack = new LinkedList<>();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c) && !stack.isEmpty()) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pollLast());
+        }
+
+        return sb.toString();
     }
 }
