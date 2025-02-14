5e2609dfd098f0f9cb05eb811dbf021e0e2bdd4f
vincennlin <vincentagwa@gmail.com>
2025-02-14 19:33:10 +0800

    Design - Product of the Last K Numbers - my & ans

--- src/main/java/com/vincennlin/leetcode/design/medium/productofthelastknumbers/ProductOfNumbers.java
+++ src/main/java/com/vincennlin/leetcode/design/medium/productofthelastknumbers/ProductOfNumbers.java
@@ -1,16 +1,39 @@
 package com.vincennlin.leetcode.design.medium.productofthelastknumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 1352
 class ProductOfNumbers {
 
-    public ProductOfNumbers() {
+    List<Integer> prefixSum;
+    int allProduct;
 
+    public ProductOfNumbers() {
+        prefixSum = new ArrayList<>();
+        allProduct = 0;
     }
 
     public void add(int num) {
-
+        if (num == 0) {
+            prefixSum.clear();
+            allProduct = 0;
+        } else if (prefixSum.isEmpty()){
+            prefixSum.add(num);
+            allProduct = num;
+        } else {
+            prefixSum.add(num * prefixSum.get(prefixSum.size() - 1));
+            allProduct *= num;
+        }
     }
 
     public int getProduct(int k) {
-        return 0;
+        if (k > prefixSum.size()) {
+            return 0;
+        } else if (k == prefixSum.size()){
+            return allProduct;
+        } else {
+            return allProduct / prefixSum.get(prefixSum.size() - 1 - k);
+        }
     }
 }
