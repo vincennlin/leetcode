0cf7f68682c57aed7fb9045a3bd5037b1dd34f40
vincennlin <vincentagwa@gmail.com>
2025-02-14 15:03:07 +0800

    Design - Product of the Last K Numbers

---
+++ src/main/java/com/vincennlin/leetcode/design/medium/productofthelastknumbers/ProductOfNumbers.java
@@ -0,0 +1,16 @@
+package com.vincennlin.leetcode.design.medium.productofthelastknumbers;
+// 1352
+class ProductOfNumbers {
+
+    public ProductOfNumbers() {
+
+    }
+
+    public void add(int num) {
+
+    }
+
+    public int getProduct(int k) {
+        return 0;
+    }
+}
