c99651b7c447b97c0c355221c1383f0ba0f6ec7f
vincennlin <vincentagwa@gmail.com>
2025-02-15 15:47:37 +0800

    Backtracking - Find the Punishment Number of an Integer - ans

--- src/main/java/com/vincennlin/leetcode/backtracking/medium/findthepunishmentnumberofaninteger/Solution.java
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/findthepunishmentnumberofaninteger/Solution.java
@@ -2,6 +2,41 @@ package com.vincennlin.leetcode.backtracking.medium.findthepunishmentnumberofani
 // 2698
 class Solution {
     public int punishmentNumber(int n) {
-        return 0;
+        int punishmentNum = 0;
+
+        for (int currentNum = 1; currentNum <= n; currentNum++) {
+            int squareNum = currentNum * currentNum;
+
+            if (canPartition(Integer.toString(squareNum), currentNum)) {
+                punishmentNum += squareNum;
+            }
+        }
+
+        return punishmentNum;
+    }
+
+    private boolean canPartition(String stringNum, int target) {
+
+        if (stringNum.isEmpty() && target == 0) {
+            return true;
+        }
+
+        if (target < 0) {
+            return false;
+        }
+
+
+        for (int i = 0; i < stringNum.length(); i++) {
+            String left = stringNum.substring(0, i + 1);
+            String right = stringNum.substring(i + 1);
+
+            int leftNum = Integer.parseInt(left);
+
+            if (canPartition(right, target - leftNum)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
