78bd7f4d1cdeb4ce8e0846b4cc4f8c7df91002fc
vincennlin <vincentagwa@gmail.com>
2025-02-15 15:29:56 +0800

    Backtracking - Find the Punishment Number of an Integer

---
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/findthepunishmentnumberofaninteger/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.backtracking.medium.findthepunishmentnumberofaninteger;
+// 2698
+class Solution {
+    public int punishmentNumber(int n) {
+        return 0;
+    }
+}
