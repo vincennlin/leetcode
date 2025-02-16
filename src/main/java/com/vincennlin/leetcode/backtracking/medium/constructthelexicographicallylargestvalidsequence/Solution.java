89058ac5b328776d396037ff839d47763ddc5642
vincennlin <vincentagwa@gmail.com>
2025-02-16 12:35:49 +0800

    Backtracking - Construct the Lexicographically Largest Valid Sequence

---
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/constructthelexicographicallylargestvalidsequence/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.backtracking.medium.constructthelexicographicallylargestvalidsequence;
+// 1718
+class Solution {
+    public int[] constructDistancedSequence(int n) {
+        return null;
+    }
+}
