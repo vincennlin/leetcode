618b738ff353505c67b293e6bb70c600de5441fc
vincennlin <vincentagwa@gmail.com>
2025-02-17 10:51:14 +0700

    Backtracking - Letter Tile Possibilities - ans

--- src/main/java/com/vincennlin/leetcode/backtracking/medium/lettertilepossibilities/Solution.java
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/lettertilepossibilities/Solution.java
@@ -1,42 +1,30 @@
 package com.vincennlin.leetcode.backtracking.medium.lettertilepossibilities;
-
-import java.util.HashMap;
-import java.util.Map;
-
 // 1079
 class Solution {
-    int result;
-
     public int numTilePossibilities(String tiles) {
-        Map<Character, Integer> letterFreqMap = new HashMap<>();
-        result = 0;
+        int[] freq = new int[26];
 
         for (char c : tiles.toCharArray()) {
-            letterFreqMap.put(c, letterFreqMap.getOrDefault(c, 0) + 1);
+            freq[c - 'A']++;
         }
 
-        backtracking(letterFreqMap, true, 0, tiles.length());
-
-        return result;
+        return backtracking(freq);
     }
 
-    private void backtracking(Map<Character, Integer> letterFreqMap, boolean isEmpty, int length, int targetLength) {
-        if (length == targetLength) {
-            if (!isEmpty) {
-                result++;
+    private int backtracking(int[] freq) {
+        int total = 0;
+
+        for (int charIndex = 0; charIndex < 26; charIndex++) {
+            if (freq[charIndex] == 0) {
+                continue;
             }
-            return;
+
+            total++;
+            freq[charIndex]--;
+            total += backtracking(freq);
+            freq[charIndex]++;
         }
 
-        for (char c : letterFreqMap.keySet()) {
-            int freq = letterFreqMap.get(c);
-            if (freq > 0) {
-                letterFreqMap.put(c, freq - 1);
-                backtracking(letterFreqMap, false, length + 1, targetLength);
-                letterFreqMap.put(c, freq);
-            }
-        }
-
-        backtracking(letterFreqMap, isEmpty, length + 1, targetLength);
+        return total;
     }
 }
