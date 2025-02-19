249a98d261f5d09f31559fdea6ac32391fd822c9
vincennlin <vincentagwa@gmail.com>
2025-02-19 10:17:47 +0700

    Backtracking - The k-th Lexicographical String of All Happy Strings of Length n

---
+++ src/main/java/com/vincennlin/leetcode/backtracking/medium/thekthlexicographicalstringofallhappystringsoflengthn/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.backtracking.medium.thekthlexicographicalstringofallhappystringsoflengthn;
+// 1415
+class Solution {
+    public String getHappyString(int n, int k) {
+        return "";
+    }
+}
