85766b7f07e80d70cf7272fb54d6ef444ce6e85b
vincennlin <vincentagwa@gmail.com>
2025-02-23 01:44:43 +0700

    Binary Tree - Recover a Tree From Preorder Traversal - unfinished

--- src/main/java/com/vincennlin/leetcode/binarytree/dfs/hard/recoveratreefrompreordertraversal/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarytree/dfs/hard/recoveratreefrompreordertraversal/Solution.java
@@ -1,7 +1,57 @@
 package com.vincennlin.leetcode.binarytree.dfs.hard.recoveratreefrompreordertraversal;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 // 1028
 class Solution {
     public TreeNode recoverFromPreorder(String traversal) {
-        return null;
+        TreeNode root = new TreeNode();
+        Queue<Character> charQueue = new LinkedList<>();
+
+        for (char c : traversal.toCharArray()) {
+            charQueue.add(c);
+        }
+
+        dfs(charQueue, root, 0);
+
+        return root;
+    }
+
+    private void dfs(Queue<Character> charQueue, TreeNode root, int depth) {
+        if (charQueue.isEmpty() || root == null) {
+            return;
+        }
+
+        int val = getValFromQueue(charQueue);
+        int nextDepth = getDepthFromQueue(charQueue, false);
+
+        if (nextDepth > depth) {
+            getDepthFromQueue(charQueue, true);
+            TreeNode leftChild = new TreeNode();
+            root.left = leftChild;
+            dfs(charQueue, leftChild, nextDepth);
+        }
+    }
+
+    private int getValFromQueue(Queue<Character> charQueue) {
+        int val = 0;
+
+        while (!charQueue.isEmpty() && Character.isDigit(charQueue.peek())) {
+            val = 10 * val + charQueue.poll();
+        }
+
+        return val;
+    }
+
+    private int getDepthFromQueue(Queue<Character> charQueue, boolean isRemove) {
+        int depth = 0;
+
+        while (!charQueue.isEmpty() && charQueue.peek() == '-') {
+            depth++;
+            charQueue.poll();
+        }
+
+        return depth;
     }
 }
