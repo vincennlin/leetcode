2acaaee58d43f088b88799f7b86f5b2aacc82864
vincennlin <vincentagwa@gmail.com>
2025-02-24 12:02:11 +0800

    Graph - Most Profitable Path in a Tree

---
+++ src/main/java/com/vincennlin/leetcode/graph/dfs/medium/mostprofitablepathinatree/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.graph.dfs.medium.mostprofitablepathinatree;
+// 2467
+class Solution {
+    public int mostProfitablePath(int[][] edges, int bob, int[] amount) {
+        return 0;
+    }
+}
