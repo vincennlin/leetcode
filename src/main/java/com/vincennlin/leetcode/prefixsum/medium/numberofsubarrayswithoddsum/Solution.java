4dfb5f88f9f78ede90a1d909482b5544dc486841
vincennlin <vincentagwa@gmail.com>
2025-02-25 14:48:08 +0800

    Prefix Sum - Number of Sub-arrays With Odd Sum - ans

--- src/main/java/com/vincennlin/leetcode/prefixsum/medium/numberofsubarrayswithoddsum/Solution.java
+++ src/main/java/com/vincennlin/leetcode/prefixsum/medium/numberofsubarrayswithoddsum/Solution.java
@@ -2,6 +2,28 @@ package com.vincennlin.leetcode.prefixsum.medium.numberofsubarrayswithoddsum;
 // 1524
 class Solution {
     public int numOfSubarrays(int[] arr) {
-        return 0;
+        final int MOD = 1000000007;
+
+        int oddSumCount = 0;
+        int evenSumCount = 1;
+
+        int result = 0;
+        long prefixSum = 0;
+
+        for (int num : arr) {
+            prefixSum += num;
+
+            if (prefixSum % 2 == 0) {
+                result += oddSumCount;
+                evenSumCount++;
+            } else {
+                result += evenSumCount;
+                oddSumCount++;
+            }
+
+            result %= MOD;
+        }
+
+        return result;
     }
 }
