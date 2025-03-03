8f35688567a79c31026bd5ae9230af602076ac5c
vincennlin <vincentagwa@gmail.com>
2025-03-03 13:24:06 +0800

    Two Pointers - Partition Array According to Given Pivot - my

--- src/main/java/com/vincennlin/leetcode/twopointers/medium/partitionarrayaccordingtogivenpivot/Solution.java
+++ src/main/java/com/vincennlin/leetcode/twopointers/medium/partitionarrayaccordingtogivenpivot/Solution.java
@@ -1,7 +1,37 @@
 package com.vincennlin.leetcode.twopointers.medium.partitionarrayaccordingtogivenpivot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 2161
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        return null;
+        int n = nums.length;
+        List<Integer> smaller = new ArrayList<>();
+        List<Integer> larger = new ArrayList<>();
+
+        for (int num : nums) {
+            if (num < pivot) {
+                smaller.add(num);
+            } else if (num > pivot) {
+                larger.add(num);
+            }
+        }
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < smaller.size(); i++) {
+            result[i] = smaller.get(i);
+        }
+
+        for (int i = 0; i < n - smaller.size() - larger.size(); i++) {
+            result[smaller.size() + i] = pivot;
+        }
+
+        for (int i = 0; i < larger.size(); i++) {
+            result[n - larger.size() + i] = larger.get(i);
+        }
+
+        return result;
     }
 }
