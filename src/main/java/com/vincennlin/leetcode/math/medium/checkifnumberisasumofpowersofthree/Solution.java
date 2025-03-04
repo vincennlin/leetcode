4c37b1d93e8da77c80ebe0bf3e76e8f6d7a47be9
vincennlin <vincentagwa@gmail.com>
2025-03-04 16:23:12 +0800

    Math - Check if Number is a Sum of Powers of Three - my & ans

--- src/main/java/com/vincennlin/leetcode/math/medium/checkifnumberisasumofpowersofthree/Solution.java
+++ src/main/java/com/vincennlin/leetcode/math/medium/checkifnumberisasumofpowersofthree/Solution.java
@@ -2,6 +2,13 @@ package com.vincennlin.leetcode.math.medium.checkifnumberisasumofpowersofthree;
 // 1780
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        return false;
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+
+        return true;
     }
 }
