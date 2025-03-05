7e74f92e18b1b6aaeb46ae43043f555f18448dd7
vincennlin <vincentagwa@gmail.com>
2025-03-05 12:45:49 +0800

    Math - Count Total Number of Colored Cells - ans

--- src/main/java/com/vincennlin/leetcode/math/medium/counttotalnumberofcoloredcells/Solution.java
+++ src/main/java/com/vincennlin/leetcode/math/medium/counttotalnumberofcoloredcells/Solution.java
@@ -2,6 +2,6 @@ package com.vincennlin.leetcode.math.medium.counttotalnumberofcoloredcells;
 // 2579
 class Solution {
     public long coloredCells(int n) {
-        return 0L;
+        return ((long) n * (long) n) + ((long) (n - 1) * (long) (n - 1));
     }
 }
