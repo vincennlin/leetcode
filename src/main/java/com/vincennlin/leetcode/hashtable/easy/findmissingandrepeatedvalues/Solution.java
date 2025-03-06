42a6cc7ef4baf7165f465ce57fb434512f6e6e51
vincennlin <vincentagwa@gmail.com>
2025-03-06 12:00:48 +0800

    Hash Table - Find Missing and Repeated Values - my

--- src/main/java/com/vincennlin/leetcode/hashtable/easy/findmissingandrepeatedvalues/Solution.java
+++ src/main/java/com/vincennlin/leetcode/hashtable/easy/findmissingandrepeatedvalues/Solution.java
@@ -1,7 +1,28 @@
 package com.vincennlin.leetcode.hashtable.easy.findmissingandrepeatedvalues;
+
+import java.util.HashSet;
+import java.util.Set;
+
 // 2965
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        return null;
+        int[] result = new int[2];
+        Set<Integer> set = new HashSet<>();
+        int n = grid.length;
+        int sum = ((n * n + 1) * n * n) / 2;
+
+        for (int[] row : grid) {
+            for (int num : row) {
+                if (set.contains(num)) {
+                    result[0] = num;
+                } else {
+                    set.add(num);
+                    sum -= num;
+                }
+            }
+        }
+        result[1] = sum;
+
+        return result;
     }
 }
