e2f3dd46ebd8698fb10db61f01ee68b49a197ddd
vincennlin <vincentagwa@gmail.com>
2025-03-07 18:45:50 +0800

    Number Theory - Closest Prime Numbers in Range - ans

--- src/main/java/com/vincennlin/leetcode/numbertheory/medium/closestprimenumbersinrange/Solution.java
+++ src/main/java/com/vincennlin/leetcode/numbertheory/medium/closestprimenumbersinrange/Solution.java
@@ -1,53 +1,59 @@
 package com.vincennlin.leetcode.numbertheory.medium.closestprimenumbersinrange;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 // 2523
 class Solution {
-    public int[] closestPrimes(int left, int right) {
-        Set<Integer> composites = getComposites(right);
-        List<Integer> primes = new ArrayList<>();
 
-        for (int num = Math.max(left, 2); num <= right; num++) {
-            if (!composites.contains(num)) {
-                primes.add(num);
+    public int[] closestPrimes(int left, int right) {
+        boolean[] notPrimeArray = getNotPrimeArray(right);
+
+        List<Integer> primeNumbers = new ArrayList<>();
+        for (int num = left; num <= right; num++) {
+            if (!notPrimeArray[num]) {
+                primeNumbers.add(num);
             }
         }
 
         int[] result = new int[]{-1, -1};
 
-        if (primes.size() < 2) {
+        if (primeNumbers.size() < 2) {
             return result;
         }
 
-        int minDiff = Integer.MAX_VALUE;
+        int minDifference = Integer.MAX_VALUE;
 
-        for (int i = 1; i < primes.size(); i++) {
-            int diff = primes.get(i) - primes.get(i - 1);
-            if (diff < minDiff) {
-                minDiff = diff;
-                result[0] = primes.get(i - 1);
-                result[1] = primes.get(i);
+        for (int index = 1; index < primeNumbers.size(); index++) {
+            int difference =
+                    primeNumbers.get(index) - primeNumbers.get(index - 1);
+            if (difference < minDifference) {
+                minDifference = difference;
+                result[0] = primeNumbers.get(index - 1);
+                result[1] = primeNumbers.get(index);
             }
         }
 
         return result;
     }
 
-    private Set<Integer> getComposites(int range) {
-        Set<Integer> composites = new HashSet<>();
+    private boolean[] getNotPrimeArray(int upperLimit) {
+        boolean[] isNotPrime = new boolean[upperLimit + 1];
 
-        for (int num = 2; num * num <= range; num++) {
-            if (!composites.contains(num)) {
-                for (int multiple = num * num; multiple <= range; multiple += num) {
-                    composites.add(multiple);
+        isNotPrime[0] = true;
+        isNotPrime[1] = true;
+
+        for (int number = 2; number * number <= upperLimit; number++) {
+            if (!isNotPrime[number]) {
+                for (
+                        int multiple = number * number;
+                        multiple <= upperLimit;
+                        multiple += number
+                ) {
+                    isNotPrime[multiple] = true;
                 }
             }
         }
-
-        return composites;
+        return isNotPrime;
     }
 }
