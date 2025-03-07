c8e1a16294145673f964791e6bdf2674d5eeed1e
vincennlin <vincentagwa@gmail.com>
2025-03-07 18:04:02 +0800

    Number Theory - Closest Prime Numbers in Range

---
+++ src/main/java/com/vincennlin/leetcode/numbertheory/medium/closestprimenumbersinrange/Main.java
@@ -0,0 +1,43 @@
+package com.vincennlin.leetcode.numbertheory.medium.closestprimenumbersinrange;
+
+import java.util.Arrays;
+
+/*
+Given two positive integers left and right, find the two integers num1 and num2 such that:
+
+left <= num1 < num2 <= right .
+Both num1 and num2 are prime numbers.
+num2 - num1 is the minimum amongst all other pairs satisfying the above conditions.
+Return the positive integer array ans = [num1, num2]. If there are multiple pairs satisfying these conditions, return the one with the smallest num1 value. If no such numbers exist, return [-1, -1].
+
+
+
+Example 1:
+
+Input: left = 10, right = 19
+Output: [11,13]
+Explanation: The prime numbers between 10 and 19 are 11, 13, 17, and 19.
+The closest gap between any pair is 2, which can be achieved by [11,13] or [17,19].
+Since 11 is smaller than 17, we return the first pair.
+Example 2:
+
+Input: left = 4, right = 6
+Output: [-1,-1]
+Explanation: There exists only one prime number in the given range, so the conditions cannot be satisfied.
+
+
+Constraints:
+
+1 <= left <= right <= 106
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] result1 = solution.closestPrimes(10, 19);
+        System.out.println(Arrays.toString(result1)); // [11, 13]
+
+        int[] result2 = solution.closestPrimes(4, 6);
+        System.out.println(Arrays.toString(result2)); // [-1, -1]
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/numbertheory/medium/closestprimenumbersinrange/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.numbertheory.medium.closestprimenumbersinrange;
+// 2523
+class Solution {
+    public int[] closestPrimes(int left, int right) {
+        return null;
+    }
+}
