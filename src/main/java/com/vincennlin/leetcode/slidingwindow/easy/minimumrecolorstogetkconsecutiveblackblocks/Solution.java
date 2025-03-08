95c383ea121d8c803ac4913a6c65e624e2ed5c84
vincennlin <vincentagwa@gmail.com>
2025-03-08 22:18:29 +0800

    Sliding Window - Minimum Recolors to Get K Consecutive Black Blocks

---
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/easy/minimumrecolorstogetkconsecutiveblackblocks/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.slidingwindow.easy.minimumrecolorstogetkconsecutiveblackblocks;
+// 2379
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        return 0;
+    }
+}
