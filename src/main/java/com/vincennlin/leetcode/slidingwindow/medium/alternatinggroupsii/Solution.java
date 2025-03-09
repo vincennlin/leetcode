f0a6d85dc504f9e1cc6878613b53ce929ed90ee3
vincennlin <vincentagwa@gmail.com>
2025-03-09 23:54:09 +0800

    Sliding Window - Alternating Groups II - ans

--- src/main/java/com/vincennlin/leetcode/slidingwindow/medium/alternatinggroupsii/Solution.java
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/alternatinggroupsii/Solution.java
@@ -1,7 +1,36 @@
 package com.vincennlin.leetcode.slidingwindow.medium.alternatinggroupsii;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 3208
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
-        return 0;
+        List<Integer> colorList = new ArrayList<>();
+
+        for (int color : colors) {
+            colorList.add(color);
+        }
+
+        for (int i = 0; i < k; i++) {
+            colorList.add(colors[i]);
+        }
+
+        int groups = 0;
+        int size = 0;
+
+        for (int i = 1; i < colorList.size(); i++) {
+            if (colorList.get(i) != colorList.get(i - 1)) {
+                size++;
+                if (size == k) {
+                    groups++;
+                    size--;
+                }
+            } else {
+                size = 1;
+            }
+        }
+
+        return groups;
     }
 }
