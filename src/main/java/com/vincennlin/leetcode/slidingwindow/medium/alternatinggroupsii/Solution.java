4a9a09b69817675c55ef6c3dd934ec7cdb72b1fd
vincennlin <vincentagwa@gmail.com>
2025-03-09 23:40:56 +0800

    Sliding Window - Alternating Groups II

---
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/alternatinggroupsii/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.slidingwindow.medium.alternatinggroupsii;
+// 3208
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        return 0;
+    }
+}
