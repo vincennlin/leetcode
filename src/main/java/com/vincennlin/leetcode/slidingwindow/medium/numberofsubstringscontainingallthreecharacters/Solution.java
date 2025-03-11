e2b1298e9529f117c774365f4096305c418cd972
vincennlin <vincentagwa@gmail.com>
2025-03-11 13:16:34 +0800

    Sliding Window - Number of Substrings Containing All Three Characters

---
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/numberofsubstringscontainingallthreecharacters/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.slidingwindow.medium.numberofsubstringscontainingallthreecharacters;
+// 1358
+class Solution {
+    public int numberOfSubstrings(String s) {
+        return 0;
+    }
+}
