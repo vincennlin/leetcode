1384df97bca9e0fa424829e1fdc1e9d0e4e2e4a8
vincennlin <vincentagwa@gmail.com>
2025-03-11 13:31:27 +0800

    Sliding Window - Number of Substrings Containing All Three Characters - ans

--- src/main/java/com/vincennlin/leetcode/slidingwindow/medium/numberofsubstringscontainingallthreecharacters/Solution.java
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/numberofsubstringscontainingallthreecharacters/Solution.java
@@ -1,34 +1,14 @@
 package com.vincennlin.leetcode.slidingwindow.medium.numberofsubstringscontainingallthreecharacters;
-
-import java.util.HashMap;
-import java.util.Map;
-
 // 1358
 class Solution {
     public int numberOfSubstrings(String s) {
         int result = 0;
+        int[] lastPos = {-1, -1, -1};
 
-        Map<Character, Integer> charFreqMap = new HashMap<>();
-        int start = 0, end = 0;
+        for (int pos = 0; pos < s.length(); pos++) {
+            lastPos[s.charAt(pos) - 'a'] = pos;
 
-        while (end < s.length()) {
-            char endChar = s.charAt(end);
-
-            charFreqMap.put(endChar, charFreqMap.getOrDefault(endChar, 0) + 1);
-
-            while (charFreqMap.size() == 3) {
-                result += s.length() - end;
-                char startChar = s.charAt(start);
-
-                charFreqMap.put(startChar, charFreqMap.get(startChar) - 1);
-                if (charFreqMap.get(startChar) == 0) {
-                    charFreqMap.remove(startChar);
-                }
-
-                start++;
-            }
-
-            end++;
+            result += 1 + Math.min(Math.min(lastPos[0], lastPos[1]), lastPos[2]);
         }
 
         return result;
