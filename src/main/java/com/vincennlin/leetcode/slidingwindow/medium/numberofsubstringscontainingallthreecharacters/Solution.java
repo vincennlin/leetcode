cd25994dfa150bdb9614de448a62e7d1d1d2d50a
vincennlin <vincentagwa@gmail.com>
2025-03-11 13:25:55 +0800

    Sliding Window - Number of Substrings Containing All Three Characters - my - bad

--- src/main/java/com/vincennlin/leetcode/slidingwindow/medium/numberofsubstringscontainingallthreecharacters/Solution.java
+++ src/main/java/com/vincennlin/leetcode/slidingwindow/medium/numberofsubstringscontainingallthreecharacters/Solution.java
@@ -1,7 +1,36 @@
 package com.vincennlin.leetcode.slidingwindow.medium.numberofsubstringscontainingallthreecharacters;
+
+import java.util.HashMap;
+import java.util.Map;
+
 // 1358
 class Solution {
     public int numberOfSubstrings(String s) {
-        return 0;
+        int result = 0;
+
+        Map<Character, Integer> charFreqMap = new HashMap<>();
+        int start = 0, end = 0;
+
+        while (end < s.length()) {
+            char endChar = s.charAt(end);
+
+            charFreqMap.put(endChar, charFreqMap.getOrDefault(endChar, 0) + 1);
+
+            while (charFreqMap.size() == 3) {
+                result += s.length() - end;
+                char startChar = s.charAt(start);
+
+                charFreqMap.put(startChar, charFreqMap.get(startChar) - 1);
+                if (charFreqMap.get(startChar) == 0) {
+                    charFreqMap.remove(startChar);
+                }
+
+                start++;
+            }
+
+            end++;
+        }
+
+        return result;
     }
 }
