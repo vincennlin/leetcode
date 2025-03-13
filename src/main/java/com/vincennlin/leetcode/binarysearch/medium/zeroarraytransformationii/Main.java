2d5dd2b74883e76366a41779fc37dd2aba329f27
vincennlin <vincentagwa@gmail.com>
2025-03-13 09:04:28 +0800

    Binary Search - Zero Array Transformation II

---
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/zeroarraytransformationii/Main.java
@@ -0,0 +1,62 @@
+package com.vincennlin.leetcode.binarysearch.medium.zeroarraytransformationii;
+/*
+You are given an integer array nums of length n and a 2D array queries where queries[i] = [li, ri, vali].
+
+Each queries[i] represents the following action on nums:
+
+Decrement the value at each index in the range [li, ri] in nums by at most vali.
+The amount by which each value is decremented can be chosen independently for each index.
+A Zero Array is an array with all its elements equal to 0.
+
+Return the minimum possible non-negative value of k, such that after processing the first k queries in sequence, nums becomes a Zero Array. If no such k exists, return -1.
+
+
+
+Example 1:
+
+Input: nums = [2,0,2], queries = [[0,2,1],[0,2,1],[1,1,3]]
+
+Output: 2
+
+Explanation:
+
+For i = 0 (l = 0, r = 2, val = 1):
+Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
+The array will become [1, 0, 1].
+For i = 1 (l = 0, r = 2, val = 1):
+Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
+The array will become [0, 0, 0], which is a Zero Array. Therefore, the minimum value of k is 2.
+Example 2:
+
+Input: nums = [4,3,2,1], queries = [[1,3,2],[0,2,1]]
+
+Output: -1
+
+Explanation:
+
+For i = 0 (l = 1, r = 3, val = 2):
+Decrement values at indices [1, 2, 3] by [2, 2, 1] respectively.
+The array will become [4, 1, 0, 0].
+For i = 1 (l = 0, r = 2, val = 1):
+Decrement values at indices [0, 1, 2] by [1, 1, 0] respectively.
+The array will become [3, 0, 0, 0], which is not a Zero Array.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 5 * 105
+1 <= queries.length <= 105
+queries[i].length == 3
+0 <= li <= ri < nums.length
+1 <= vali <= 5
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.minZeroArray(new int[]{2, 0, 2}, new int[][]{{0, 2, 1}, {0, 2, 1}, {1, 1, 3}})); // 2
+
+        System.out.println(solution.minZeroArray(new int[]{4, 3, 2, 1}, new int[][]{{1, 3, 2}, {0, 2, 1}})); // -1
+    }
+}
---
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/zeroarraytransformationii/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.binarysearch.medium.zeroarraytransformationii;
+// 3356
+class Solution {
+    public int minZeroArray(int[] nums, int[][] queries) {
+        return 0;
+    }
+}
