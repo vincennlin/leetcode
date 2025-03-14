7ab6942ad1c1252f51e2fedd017fc36eb5eba808
vincennlin <vincentagwa@gmail.com>
2025-03-14 11:37:20 +0800

    Binary Search - Maximum Candies Allocated to K Children - my

--- src/main/java/com/vincennlin/leetcode/binarysearch/medium/maximumcandiesallocatedtokchildren/Solution.java
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/maximumcandiesallocatedtokchildren/Solution.java
@@ -2,6 +2,32 @@ package com.vincennlin.leetcode.binarysearch.medium.maximumcandiesallocatedtokch
 // 2226
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-        return 0;
+        int left = 0, right = 10000000;
+        int mid;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (canAllocate(candies, k, mid)) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return Math.max(right, 0);
+    }
+
+    private boolean canAllocate(int[] candies, long k, int size) {
+        if (size == 0) {
+            return false;
+        }
+
+        long piles = 0;
+
+        for (int candy : candies) {
+            piles += candy / size;
+        }
+
+        return piles >= k;
     }
 }
