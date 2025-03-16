b78bed9fb825bb835dca85c931c0a4c7d83c99dd
vincennlin <vincentagwa@gmail.com>
2025-03-16 11:43:03 +0800

    Binary Search - Minimum Time to Repair Cars

---
+++ src/main/java/com/vincennlin/leetcode/binarysearch/medium/minimumtimetorepaircars/Solution.java
@@ -0,0 +1,7 @@
+package com.vincennlin.leetcode.binarysearch.medium.minimumtimetorepaircars;
+// 2594
+class Solution {
+    public long repairCars(int[] ranks, int cars) {
+        return 0L;
+    }
+}
